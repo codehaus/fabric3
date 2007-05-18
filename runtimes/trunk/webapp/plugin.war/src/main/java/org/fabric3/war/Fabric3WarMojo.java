3e52d85f8592b1c1cf95b0fe4a14cbca478d80d6
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-05-18 14:29:58 +0000

    modify war plugin to run in the generate-reosurce phase adding artifacts to the target webapp modify extension behaviour to temporarily just add the artifacts to the boot directory this should be replaced with a mechanism compatible with the contribution service

--- runtimes/trunk/webapp/plugin.war/src/main/java/org/fabric3/war/Fabric3WarMojo.java
+++ runtimes/trunk/webapp/plugin.war/src/main/java/org/fabric3/war/Fabric3WarMojo.java
@@ -18,25 +18,14 @@
  */
 package org.fabric3.war;
 
-import java.beans.XMLEncoder;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.Properties;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
-import java.util.zip.ZipException;
+import java.util.Set;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.metadata.ArtifactMetadataRetrievalException;
@@ -49,9 +38,11 @@ import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
 
 /**
- * Build the Fabric3 war file by adding the Fabric3 dependencies.
+ * Add fabric3 runtime dependencies to a webapp.
  * 
  * Performs the following tasks.
  * 
@@ -60,13 +51,10 @@ import org.apache.maven.plugin.MojoExecutionException;
  *   <li>By default boot libraries are transitively resolved from webapp-host</li>
  *   <li>The version of boot libraries can be specified using configuration/runTimeVersion element</li>
  *   <li>Boot libraries can be overridden using the configuration/bootLibs element in the plugin</li>
- *   <li>Adds the extension artifacts specified using configuration/extensions to WEB-INF/fabric3/extensions</li>
- *   <li>If configuration/loadExtensionsDependency is set to true extension dependencies are transitivel loaded</li>
- *   <li>Extension dependencies are loaded into WEB-INF/fabric3/repository directory in a Maven repo format</li>
- *   <li>Extension dependency metadata is written to WEB-INF/fabric3/repository/dependency.metadata file</li>
+ *   <li>Adds the extension artifacts specified using configuration/extensions to WEB-INF/fabric3/boot</li>
  * </ul>
  * @goal fabric3-war
- * @phase package
+ * @phase generate-resources
  * 
  * @version $Rev$ $Date$
  * 
@@ -76,22 +64,20 @@ public class Fabric3WarMojo extends AbstractMojo {
     /**
      * Fabric3 path.
      */
-    private static final String FABRIC3_PATH = "WEB-INF/fabric3/";
+    private static final String FABRIC3_PATH = "WEB-INF/fabric3";
 
     /**
      * Fabric3 boot path.
      */
-    private static final String BOOT_PATH = FABRIC3_PATH + "boot/";
+    private static final String BOOT_PATH = FABRIC3_PATH + "/boot";
 
     /**
-     * Fabric3 extension path.
+     * The directory where the webapp is built.
+     *
+     * @parameter expression="${project.build.directory}/${project.build.finalName}"
+     * @required
      */
-    private static final String EXTENSION_PATH = FABRIC3_PATH + "extensions/";
-
-    /**
-     * Fabric3 repository path.
-     */
-    private static final String REPOSITORY_PATH = FABRIC3_PATH + "repository/";
+     public File webappDirectory;
 
     /**
      * Artifact metadata source.
@@ -139,46 +125,16 @@ public class Fabric3WarMojo extends AbstractMojo {
     /**
      * The directory for the generated WAR.
      * 
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
-    public String outputDirectory;
-
-    /**
-     * The directory for the generated WAR.
-     * 
      * @parameter
      */
     public Dependency[] bootLibs;
 
     /**
-     * The directory for the generated WAR.
-     * 
+     * Set of extension artifacts that should be deployed to the runtime.
+     *
      * @parameter
      */
-    public Dependency[] extensions = new Dependency[0];
-
-    /**
-     * The directory for the generated WAR.
-     * 
-     * @parameter
-     */
-    public Dependency[] dependencies = new Dependency[0];
-
-    /**
-     * The name of the generated WAR.
-     * 
-     * @parameter expression="${project.build.finalName}"
-     * @required
-     */
-    public String warName;
-
-    /**
-     * A flag to indicate whether extension dependencies should be resolved transitively.
-     * 
-     * @parameter
-     */
-    public boolean loadExtensionDependencies;
+    public Dependency[] extensions;
 
     /**
      * The default version of the runtime to use.
@@ -186,29 +142,11 @@ public class Fabric3WarMojo extends AbstractMojo {
      * @parameter
      */
     public String runTimeVersion;
-    
-    /**
-     * WEB-INF jar files.
-     */
-    public Set<String> packagedLibs = new HashSet<String>();
-
-    /**
-     * Transitive dependencies for extensions.
-     */
-    public Map<String, Set<String>> transDepenedencyMap = new HashMap<String, Set<String>>();
 
     /**
      * Executes the MOJO.
      */
     public void execute() throws MojoExecutionException {
-
-        JarFile originalWar;
-        JarOutputStream newWar = null;
-        File originalWarFile = null;
-        File newWarFile = null;
-
-        boolean success = false;
-
         if (runTimeVersion == null) {
             try {
                 runTimeVersion = getPluginVersion();
@@ -216,6 +154,7 @@ public class Fabric3WarMojo extends AbstractMojo {
                 throw new MojoExecutionException(e.getMessage(), e);
             }
         }
+        getLog().info("Using fabric3 runtime version " + runTimeVersion);
 
         if (bootLibs == null) {
             Dependency dependancy = new Dependency("org.codehaus.fabric3.webapp",
@@ -225,149 +164,52 @@ public class Fabric3WarMojo extends AbstractMojo {
         }
 
         try {
-            originalWarFile = new File(outputDirectory, warName + ".war");
-            originalWar = new JarFile(originalWarFile);
-
-            newWarFile = new File(outputDirectory, warName + "-temp.war");
-            newWar = new JarOutputStream(new FileOutputStream(newWarFile));
-
-            copyOriginal(originalWar, newWar);
-
-            addEntry(newWar, FABRIC3_PATH);
-            addEntry(newWar, BOOT_PATH);
-            addEntry(newWar, EXTENSION_PATH);
-            addEntry(newWar, REPOSITORY_PATH);
-
+            File bootDir = new File(webappDirectory, BOOT_PATH);
+            bootDir.mkdirs();
             for (Dependency dependency : bootLibs) {
-                for (Artifact art : resolveArtifact(dependency.getArtifact(artifactFactory), true)) {
-                    addArtifact(newWar, BOOT_PATH, art);
+                for (Artifact artifact : resolveArtifact(dependency.getArtifact(artifactFactory), true)) {
+                    // FIXME use this when maven updates to plexus-utils-1.4
+                    // FileUtils.copyFileToDirectoryIfModified(bootDir, artifact.getFile());
+                    copyFileToDirectoryIfModified(artifact.getFile(), bootDir);
                 }
             }
 
-            for (Dependency dependency : extensions) {
-                for (Artifact art : resolveArtifact(dependency.getArtifact(artifactFactory), loadExtensionDependencies)) {
-                    if (dependency.match(art)) {
-                        addArtifact(newWar, EXTENSION_PATH, art);
-                    }
+            installExtensions();
 
-                    // Load dependencies even for the extension itself
-                    if (loadExtensionDependencies) {
-                        loadTransitiveDependencies(newWar, art);
-                    }
-
-                }
-            }
-
-            for (Dependency dependency : dependencies) {
-                for (Artifact art : resolveArtifact(dependency.getArtifact(artifactFactory), loadExtensionDependencies)) {
-                    loadTransitiveDependencies(newWar, art);
-                }
-            }
-
-            writeDependencyMetadata(newWar);
-
-            success = true;
-
-        } catch (Exception ex) {
-            throw new MojoExecutionException(ex.getMessage(), ex);
-        } finally {
-            IOUtils.closeQuietly(newWar);
+            // TODO add user dependencies to the war in the way the contribution service expects
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
         }
-
-        if (success) {
-            if (!originalWarFile.delete()) {
-                throw new MojoExecutionException("Unable to rename war file");
-            }
-            if (!newWarFile.renameTo(originalWarFile)) {
-                throw new MojoExecutionException("Unable to rename war file");
-            }
-        }
-
     }
 
-    /**
-     * Adds an entry to the JAR failing safe for duplicate.
-     * 
-     * @param jar JAR to which the entry is added.
-     * @param entry Entry to be added.
-     * @return True if added successfully.
-     * @throws IOException In case of an IO error.
-     */
-    private boolean addEntry(JarOutputStream jar, String entry) throws IOException {
-        try {
-            jar.putNextEntry(new JarEntry(entry));
+    // TODO install extensions as contributions to the runtime domain
+    // FIXME for now just add them to the boot directory
+    private void installExtensions() throws ArtifactNotFoundException, ArtifactResolutionException, IOException,
+            ArtifactMetadataRetrievalException {
+        if (extensions == null) {
+            return;
+        }
+
+        File bootDir = new File(webappDirectory, BOOT_PATH);
+        for (Dependency dependency : extensions) {
+            for (Artifact artifact : resolveArtifact(dependency.getArtifact(artifactFactory), true)) {
+                // FIXME use this when maven updates to plexus-utils-1.4
+                // FileUtils.copyFileToDirectoryIfModified(bootDir, artifact.getFile());
+                copyFileToDirectoryIfModified(artifact.getFile(), bootDir);
+            }
+        }
+    }
+
+    // Use copyFileToDirectoryIfModified from plexus-utils-1.4
+    @Deprecated
+    private static boolean copyFileToDirectoryIfModified(File source, File targetDir) throws IOException {
+        File target = new File(targetDir, source.getName());
+        if (target.lastModified() < source.lastModified()) {
+            FileUtils.copyFile(source, target);
             return true;
-        } catch (ZipException duplicateEntry) {
-            getLog().info(duplicateEntry.getMessage());
+        } else {
             return false;
         }
-    }
-
-    /**
-     * Writes the dependency metadata.
-     * @param newWar WAR to which the metadata is written.
-     * @throws IOException In case of an IO error.
-     */
-    private void writeDependencyMetadata(JarOutputStream newWar) throws IOException {
-
-        FileOutputStream depMapOutStream = null;
-        FileInputStream depMapInStream = null;
-
-        try {
-            String metadataFile = "dependency.metadata";
-
-            File file = new File(outputDirectory, "webapp");
-            file = new File(file, REPOSITORY_PATH);
-            file.mkdirs();
-
-            file = new File(file, metadataFile);
-            file.createNewFile();
-
-            depMapOutStream = new FileOutputStream(file);
-            XMLEncoder xmlEncoder = new XMLEncoder(depMapOutStream);
-            xmlEncoder.writeObject(transDepenedencyMap);
-            xmlEncoder.close();
-
-            if (addEntry(newWar, REPOSITORY_PATH + metadataFile)) {
-                depMapInStream = new FileInputStream(file);
-                IOUtils.copy(depMapInStream, newWar);
-            }
-
-        } finally {
-            IOUtils.closeQuietly(depMapOutStream);
-            IOUtils.closeQuietly(depMapInStream);
-        }
-
-    }
-
-    /**
-     * Builds the transitive dependencies for artifacts.
-     * 
-     * @param newWar WARto which the artifacts are added.
-     * @param art Extension artifact.
-     * @throws IOException In case of an unexpected IO error.
-     * @throws ArtifactResolutionException If the artifact cannot be resolved.
-     * @throws ArtifactNotFoundException If the artifact is not found.
-     * @throws ArtifactMetadataRetrievalException In case of error in retrieving metadata.
-     */
-    private void loadTransitiveDependencies(JarOutputStream newWar, Artifact art) throws IOException, ArtifactResolutionException,
-            ArtifactNotFoundException, ArtifactMetadataRetrievalException {
-
-        String artPath = art.getGroupId() + "/" + art.getArtifactId() + "/" + art.getVersion() + "/";
-        String path = REPOSITORY_PATH + artPath;
-        addArtifact(newWar, path, art);
-
-        Set<String> transDepenedenyList = new HashSet<String>();
-        transDepenedencyMap.put(artPath, transDepenedenyList);
-
-        // Get the transitive dependencies for each dependency.
-        for (Artifact transArt : resolveArtifact(art, true)) {
-            String transArtPath = transArt.getGroupId() + "/" + transArt.getArtifactId() + "/" + transArt.getVersion() + "/";
-            if (addArtifact(newWar, REPOSITORY_PATH + transArtPath, transArt)) {
-                transDepenedenyList.add(transArtPath + transArt.getFile().getName());
-            }
-        }
-
     }
 
     /**
@@ -396,7 +238,10 @@ public class Fabric3WarMojo extends AbstractMojo {
 
         // Transitively resolve all the dependencies
         ResolutionGroup resolutionGroup = metadataSource.retrieve(artifact, localRepository, remoteRepositories);
-        ArtifactResolutionResult result = resolver.resolveTransitively(resolutionGroup.getArtifacts(), artifact, remoteRepositories, localRepository,
+        ArtifactResolutionResult result = resolver.resolveTransitively(resolutionGroup.getArtifacts(),
+                                                                       artifact,
+                                                                       remoteRepositories,
+                                                                       localRepository,
                 metadataSource);
 
         // Add the artifacts to the deployment unit
@@ -404,93 +249,6 @@ public class Fabric3WarMojo extends AbstractMojo {
             resolvedArtifacts.add((Artifact) depArtifact);
         }
         return resolvedArtifacts;
-
-    }
-
-    /**
-     * Adds the artifact to the war file.
-     * 
-     * @param newWar War file to which the artifact is added.
-     * @param path Path within the war file where artifact is added.
-     * @param artifact Artifact to be added.
-     * @throws IOException In case of an unexpected IO error.
-     * @return true if the artifact was added
-     */
-    private boolean addArtifact(JarOutputStream newWar, String path, Artifact artifact) throws IOException {
-
-        FileInputStream artifactStream = null;
-        FileOutputStream fileOutputStream = null;
-
-        try {
-
-            File artifactFile = artifact.getFile();
-            // For extensions, we'll add it even the packagedLibs has it
-            if ((!EXTENSION_PATH.equals(path)) && packagedLibs.contains(artifactFile.getName())) {
-                return false;
-            }
-            artifactStream = new FileInputStream(artifactFile);
-
-            newWar.putNextEntry(new JarEntry(path + artifactFile.getName()));
-
-            File file = new File(outputDirectory, "webapp");
-            file = new File(file, path);
-            file.mkdirs();
-
-            file = new File(file, artifactFile.getName());
-            fileOutputStream = new FileOutputStream(file);
-
-            IOUtils.copy(artifactStream, fileOutputStream);
-            IOUtils.closeQuietly(artifactStream);
-
-            artifactStream = new FileInputStream(artifactFile);
-            IOUtils.copy(artifactStream, newWar);
-
-            packagedLibs.add(artifactFile.getName());
-
-            getLog().info("Processed " + path + artifactFile.getName());
-
-            return true;
-
-        } finally {
-            IOUtils.closeQuietly(artifactStream);
-            IOUtils.closeQuietly(fileOutputStream);
-        }
-
-    }
-
-    /**
-     * Copies the original war file.
-     * 
-     * @param originalWar Original war file.
-     * @param newWar New war file.
-     * @throws IOException In case of an unexpected IO error.
-     */
-    private void copyOriginal(JarFile originalWar, JarOutputStream newWar) throws IOException {
-
-        Enumeration entries = originalWar.entries();
-        packagedLibs.clear();
-
-        while (entries.hasMoreElements()) {
-
-            JarEntry entry = (JarEntry) entries.nextElement();
-            InputStream jarEntryStream = null;
-
-            try {
-                jarEntryStream = originalWar.getInputStream(entry);
-                newWar.putNextEntry(entry);
-                IOUtils.copy(jarEntryStream, newWar);
-                String name = entry.getName();
-
-                if (name.endsWith(".jar")) {
-                    packagedLibs.add(name.substring(name.lastIndexOf("/") + 1));
-                }
-            } finally {
-                IOUtils.closeQuietly(jarEntryStream);
-            }
-
-        }
-
-        originalWar.close();
 
     }
 
@@ -502,7 +260,7 @@ public class Fabric3WarMojo extends AbstractMojo {
             pomProperties.load(is);
             return pomProperties.getProperty("version");
         } finally {
-            IOUtils.closeQuietly(is);
+            IOUtil.close(is);
         }
     }
 }
