9d22bbad28426f489fd05f211739bc442bf31e32
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-05-30 13:50:39 +0000

    First cut of the WSDL 1.1 implementation.

--- modules/trunk/fabric3-interface-wsdl/src/main/java/org/fabric3/idl/wsdl/processor/Wsdl11Processor.java
+++ modules/trunk/fabric3-interface-wsdl/src/main/java/org/fabric3/idl/wsdl/processor/Wsdl11Processor.java
@@ -20,8 +20,17 @@
 package org.fabric3.idl.wsdl.processor;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import javax.wsdl.Definition;
+import javax.wsdl.Input;
+import javax.wsdl.Output;
+import javax.wsdl.PortType;
+import javax.wsdl.WSDLException;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
 import javax.xml.namespace.QName;
 
 import org.fabric3.idl.wsdl.version.WsdlVersionChecker.WsdlVersion;
@@ -45,8 +54,34 @@ public class Wsdl11Processor implements WsdlProcessor {
      * @see @see org.fabric3.idl.wsdl.processor.WsdlProcessor#getOperations(javax.xml.namespace.QName, java.net.URL)
      */
     public List<Operation<?>> getOperations(QName portTypeOrInterfaceName, URL wsdlUrl) {
-        // Implement using wsdl4j
-        throw new UnsupportedOperationException("Not supported yet");
+        
+        try {
+            
+            List<Operation<?>> oepartions = new LinkedList<Operation<?>>();
+            
+            WSDLFactory wsdlFactory = WSDLFactory.newInstance();
+            WSDLReader reader = wsdlFactory.newWSDLReader();
+            
+            Definition definition = reader.readWSDL(wsdlUrl.toExternalForm());
+            PortType portType = definition.getPortType(portTypeOrInterfaceName);
+            
+            if(portType == null) {
+                throw new WsdlProcessorException("Port type not found " + portTypeOrInterfaceName);
+            }
+            
+            for(Object op : portType.getOperations()) {
+                javax.wsdl.Operation operation = (javax.wsdl.Operation) op;
+                Input input = operation.getInput();
+                Output output = operation.getOutput();
+                Map faults = operation.getFaults();
+                // TODO Populate the f3 operation
+            }
+            
+            return oepartions;
+            
+        } catch (WSDLException ex) {
+            throw new WsdlProcessorException("Unable to parse WSDL " + wsdlUrl, ex);
+        }
     }
 
 }
