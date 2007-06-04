c4defd75d8bf23284e21284ecb06d14c01a98c89
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-06-04 18:34:13 +0000

    Added String2Map transformer and test case.

---
+++ modules/trunk/fabric3-transform/src/main/java/org/fabric3/transform/dom2java/StringToMap.java
@@ -0,0 +1,68 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.transform.dom2java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.fabric3.transform.AbstractPullTransformer;
+import org.fabric3.spi.model.type.XSDSimpleType;
+import org.fabric3.spi.model.type.JavaClass;
+import org.fabric3.spi.model.type.DataType;
+import org.fabric3.spi.transform.TransformationException;
+
+/**
+ * Expects the property to be dfined in the format,
+ * 
+ * <code>
+ *   <key1>value1</key1>
+ *   <key2>value2</key2>
+ * </code>
+ * @version $Rev$ $Date$
+ */
+public class StringToMap extends AbstractPullTransformer<Node, Map<String, String>> {
+    private static final XSDSimpleType SOURCE = new XSDSimpleType(Node.class, XSDSimpleType.STRING);
+    private static final JavaClass<Map> TARGET = new JavaClass<Map>(Map.class);
+
+    public DataType<?> getSourceType() {
+        return SOURCE;
+    }
+
+    public DataType<?> getTargetType() {
+        return TARGET;
+    }
+
+    public Map<String, String> transform(Node node) throws TransformationException {
+        
+        Map<String, String> map = new HashMap<String, String>();
+        NodeList nodeList = node.getChildNodes();
+        for(int i = 0;i < nodeList.getLength();i++) {
+            Node child = nodeList.item(i);
+            if(child instanceof Element) {
+                Element element = (Element) child;
+                map.put(element.getTagName(), child.getTextContent());
+            }
+        }
+        return map;
+        
+    }
+    
+}
---
+++ modules/trunk/fabric3-transform/src/test/java/org/fabric3/transform/dom2java/StringToMapTestCase.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.fabric3.transform.dom2java;
+
+import java.io.ByteArrayInputStream;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Node;
+
+import junit.framework.TestCase;
+
+public class StringToMapTestCase extends TestCase {
+
+    public void testTransform() throws Exception {
+
+        StringToMap stringToMap = new StringToMap();
+
+        String xml = "<value><apple>yellow</apple><lime>green</lime><grape>black</grape></value>";
+
+        Node node =
+            DocumentBuilderFactory.newInstance()
+                .newDocumentBuilder()
+                .parse(new ByteArrayInputStream(xml.getBytes()))
+                .getDocumentElement();
+        
+        Map<String, String> map = stringToMap.transform(node);
+
+        assertEquals(3, map.size());
+        assertEquals("yellow", map.get("apple"));
+        assertEquals("green", map.get("lime"));
+        assertEquals("black", map.get("grape"));
+    }
+
+}
