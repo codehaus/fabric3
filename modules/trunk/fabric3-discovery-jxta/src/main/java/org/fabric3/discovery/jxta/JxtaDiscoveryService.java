2d5c05304c7feed2578c77e172e7b67b41649666
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-06-20 01:07:19 +0000

    Added proper presence service.

--- modules/trunk/fabric3-discovery-jxta/src/main/java/org/fabric3/discovery/jxta/JxtaDiscoveryService.java
+++ modules/trunk/fabric3-discovery-jxta/src/main/java/org/fabric3/discovery/jxta/JxtaDiscoveryService.java
@@ -18,9 +18,8 @@
  */
 package org.fabric3.discovery.jxta;
 
-import static net.jxta.discovery.DiscoveryService.PEER;
+import static net.jxta.discovery.DiscoveryService.ADV;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Map;
@@ -28,19 +27,14 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import net.jxta.discovery.DiscoveryEvent;
 import net.jxta.discovery.DiscoveryListener;
+import net.jxta.document.AdvertisementFactory;
 import net.jxta.peergroup.PeerGroup;
 import net.jxta.protocol.DiscoveryResponseMsg;
-import net.jxta.protocol.PeerAdvertisement;
 
 import org.fabric3.host.runtime.HostInfo;
 import org.fabric3.jxta.JxtaService;
-import org.fabric3.jxta.impl.Fabric3JxtaException;
 import org.fabric3.spi.model.topology.RuntimeInfo;
 import org.fabric3.spi.services.advertisement.AdvertisementService;
 import org.fabric3.spi.services.discovery.DiscoveryService;
@@ -49,11 +43,6 @@ import org.osoa.sca.annotations.Destroy;
 import org.osoa.sca.annotations.Init;
 import org.osoa.sca.annotations.Property;
 import org.osoa.sca.annotations.Reference;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-
-import com.thoughtworks.xstream.XStream;
 
 /**
  * JXTA implementation of the discovery service.
@@ -111,20 +100,11 @@ public class JxtaDiscoveryService implements DiscoveryService {
     // Jxta discovery service
     private net.jxta.discovery.DiscoveryService discoveryService;
 
-    // Peer group advertisement
-    private PeerAdvertisement peerAdvertisement;
-
     // Publisher of advertismenets
     private Publisher publisher;
 
     // Participating runtimes
     private Map<RuntimeInfo, Long> participatingRuntimes = new ConcurrentHashMap<RuntimeInfo, Long>();
-
-    // XStream
-    private XStream xstream = new XStream();
-
-    // SAX Parser TODO Not sure whether this is thread safe
-    private SAXParser parser;
 
     /**
      * @see org.fabric3.spi.services.discovery.DiscoveryService#getParticipatingRuntimes()
@@ -214,18 +194,9 @@ public class JxtaDiscoveryService implements DiscoveryService {
         PeerGroup peerGroup = jxtaService.getDomainGroup();
 
         discoveryService = peerGroup.getDiscoveryService();
-        peerAdvertisement = peerGroup.getPeerAdvertisement();
 
         publisher = new Publisher();
         workScheduler.scheduleWork(publisher);
-
-        try {
-            parser = SAXParserFactory.newInstance().newSAXParser();
-        } catch (ParserConfigurationException ex) {
-            throw new Fabric3JxtaException(ex);
-        } catch (SAXException ex) {
-            throw new Fabric3JxtaException(ex);
-        }
 
     }
 
@@ -247,40 +218,29 @@ public class JxtaDiscoveryService implements DiscoveryService {
             DiscoveryResponseMsg res = discoveryEvent.getResponse();
             Enumeration en = res.getAdvertisements();
 
-            if (en == null) {
-                return;
-            }
+            while (en != null && en.hasMoreElements()) {
 
-            while (en.hasMoreElements()) {
+                Object object = en.nextElement();
+                if(object instanceof PresenceAdvertisement) {
 
-                PeerAdvertisement adv = (PeerAdvertisement) en.nextElement();
-                try {
+                    PresenceAdvertisement presenceAdv = (PresenceAdvertisement) object;
+                    RuntimeInfo runtimeInfo = presenceAdv.getRuntimeInfo();
 
-                    String desc = adv.getDesc().toString();
-                    WhitespaceStripper stripper = new WhitespaceStripper();
-                    parser.parse(new ByteArrayInputStream(desc.getBytes()), stripper);
-
-                    RuntimeInfo runtimeInfo = (RuntimeInfo) xstream.fromXML(stripper.getXml());
                     if(runtimeInfo.getId().equals(hostInfo.getRuntimeId())) {
                         continue;
                     }
 
                     participatingRuntimes.put(runtimeInfo, System.currentTimeMillis());
-                    System.err.println(hostInfo.getRuntimeId() + ":" + runtimeInfo.getId());
-
-                    for (RuntimeInfo info : participatingRuntimes.keySet()) {
-                        long lastActive = participatingRuntimes.get(info);
-                        if (System.currentTimeMillis() - lastActive > expirationThreshold) {
-                            participatingRuntimes.remove(info);
-                        }
-                    }
-
-                } catch (SAXException ex) {
-                    // TODO Notify the monitor
-                } catch (IOException ex) {
-                    // TODO Notify the monitor
                 }
 
+            }
+
+            // Expire inactive runtimes
+            for (RuntimeInfo info : participatingRuntimes.keySet()) {
+                long lastActive = participatingRuntimes.get(info);
+                if (System.currentTimeMillis() - lastActive > expirationThreshold) {
+                    participatingRuntimes.remove(info);
+                }
             }
 
         }
@@ -309,16 +269,18 @@ public class JxtaDiscoveryService implements DiscoveryService {
 
                     Thread.sleep(interval);
 
-                    discoveryService.getRemoteAdvertisements(null, PEER, null, null, 5);
+                    discoveryService.getRemoteAdvertisements(null, ADV, null, null, 5);
+
+                    PresenceAdvertisement presenceAdv = null;
+                    presenceAdv = (PresenceAdvertisement) AdvertisementFactory.newAdvertisement(PresenceAdvertisement.getAdvertisementType());
 
                     RuntimeInfo runtimeInfo = new RuntimeInfo(hostInfo.getRuntimeId());
                     runtimeInfo.setFeatures(advertisementService.getFeatures());
 
-                    String runtimeInfoXml = xstream.toXML(runtimeInfo);
-                    peerAdvertisement.setDescription(runtimeInfoXml);
+                    presenceAdv.setRuntimeInfo(runtimeInfo);
 
-                    discoveryService.publish(peerAdvertisement);
-                    discoveryService.remotePublish(peerAdvertisement);
+                    discoveryService.publish(presenceAdv);
+                    discoveryService.remotePublish(presenceAdv);
 
                 } catch (InterruptedException ex) {
                     return;
@@ -327,73 +289,6 @@ public class JxtaDiscoveryService implements DiscoveryService {
                 }
             }
 
-        }
-
-    }
-
-    /*
-     * Hack for the JXTA RI stone age handling of XML.
-     */
-    private class WhitespaceStripper extends DefaultHandler {
-
-        StringBuilder builder = new StringBuilder();
-
-        /**
-         * Returns the XML stripped of white spaces.
-         *
-         * @return XStream xml.
-         */
-        public String getXml() {
-            return builder.toString();
-        }
-
-        /**
-         * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
-         */
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-            builder.append(new String(ch, start, length).trim());
-        }
-
-        /**
-         * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String,
-         *      java.lang.String, java.lang.String)
-         */
-        @Override
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-
-            if ("Desc".equals(qName)) {
-                return;
-            }
-
-            builder.append("</" + qName + ">");
-
-        }
-
-        /**
-         * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String,
-         *      java.lang.String, java.lang.String, org.xml.sax.Attributes)
-         */
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-
-            if ("Desc".equals(qName)) {
-                return;
-            }
-
-            builder.append("<");
-            builder.append(qName);
-            if (attributes.getLength() > 0) {
-                for (int i = 0; i < attributes.getLength(); i++) {
-                    builder.append(" ");
-                    builder.append(attributes.getQName(i));
-                    builder.append("=");
-                    builder.append("'");
-                    builder.append(attributes.getValue(i));
-                    builder.append("'");
-                }
-            }
-            builder.append(">");
         }
 
     }
---
+++ modules/trunk/fabric3-discovery-jxta/src/main/java/org/fabric3/discovery/jxta/PresenceAdvertisement.java
@@ -0,0 +1,243 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.discovery.jxta;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.fabric3.spi.model.topology.RuntimeInfo;
+
+import net.jxta.document.Advertisement;
+import net.jxta.document.AdvertisementFactory;
+import net.jxta.document.Document;
+import net.jxta.document.Element;
+import net.jxta.document.MimeMediaType;
+import net.jxta.document.StructuredDocument;
+import net.jxta.document.StructuredDocumentFactory;
+import net.jxta.id.ID;
+import net.jxta.id.IDFactory;
+import net.jxta.peer.PeerID;
+
+/**
+ *
+ * @version $Revsion$ $Date$
+ *
+ */
+public class PresenceAdvertisement extends Advertisement {
+
+    /*
+     * Register the advertisement type.
+     */
+    static {
+        AdvertisementFactory.registerAdvertisementInstance(getAdvertisementType(), new PresenceAdvertisement.Instantiator());
+    }
+
+    /*
+     * Indexed fields.
+     */
+    public static final String indexFields[] = new String[] {};
+
+    /*
+     * Runtime id.
+     */
+    private String runtimeId;
+
+    /*
+     * Peer id.
+     */
+    private String peerId;
+
+    /*
+     * Features.
+     */
+    private Set<QName> features = new HashSet<QName>();
+
+    /*
+     * Instantiator.
+     */
+    public static final class Instantiator implements AdvertisementFactory.Instantiator {
+
+        /**
+         * @see net.jxta.document.AdvertisementFactory$Instantiator#getAdvertisementType()
+         */
+        public String getAdvertisementType() {
+            return PresenceAdvertisement.getAdvertisementType();
+        }
+
+        /**
+         * @see net.jxta.document.AdvertisementFactory$Instantiator#newInstance()
+         */
+        public Advertisement newInstance() {
+            return new PresenceAdvertisement();
+        }
+
+        /**
+         * @see net.jxta.document.AdvertisementFactory$Instantiator#newInstance(net.jxta.document.Element)
+         */
+        public Advertisement newInstance(Element element) {
+
+            PresenceAdvertisement adv = new PresenceAdvertisement();
+
+            Element elem = (Element) element.getChildren("runtimeId").nextElement();
+            if (elem != null && elem.getValue() != null) {
+                adv.runtimeId = elem.getValue().toString();
+            }
+
+            elem = (Element) element.getChildren("peerId").nextElement();
+            if (elem != null && elem.getValue() != null) {
+                adv.setPeerId(elem.getValue().toString());
+            }
+
+            elem = (Element) element.getChildren("features").nextElement();
+            adv.features = new HashSet<QName>();
+            Enumeration children = elem.getChildren("feature");
+            while(children.hasMoreElements()) {
+                elem = (Element) children.nextElement();
+                if (elem != null && elem.getValue() != null) {
+                    adv.features.add(QName.valueOf(elem.getValue().toString()));
+                }
+            }
+
+            return adv;
+
+        }
+
+    }
+
+    /**
+     * @param runtimeInfo Runtime info.
+     */
+    public void setRuntimeInfo(RuntimeInfo runtimeInfo) {
+        this.runtimeId = runtimeInfo.getId();
+        this.features = runtimeInfo.getFeatures();
+    }
+
+    /**
+     * @return Runtime info.
+     */
+    public RuntimeInfo getRuntimeInfo() {
+        RuntimeInfo runtimeInfo = new RuntimeInfo(runtimeId);
+        runtimeInfo.setFeatures(features);
+        return runtimeInfo;
+    }
+
+    /**
+     * @return Advertisement type.
+     */
+    public static String getAdvertisementType() {
+        return "jxta:f3-presence";
+    }
+
+    /**
+     * @return Base advertisement type.
+     */
+    public final String getBaseAdvType() {
+        return getAdvertisementType();
+    }
+
+    /**
+     * @see net.jxta.document.Advertisement#getDocument(net.jxta.document.MimeMediaType)
+     */
+    public Document getDocument(MimeMediaType mimeMediaType) {
+        StructuredDocument doc = StructuredDocumentFactory.newStructuredDocument(mimeMediaType, getAdvertisementType());
+
+        Element elem = doc.createElement("peerId", peerId);
+        doc.appendChild(elem);
+
+        elem = doc.createElement("runtimeId", runtimeId);
+        doc.appendChild(elem);
+
+        elem = doc.createElement("features");
+        doc.appendChild(elem);
+        Iterator<QName> it = features.iterator();
+        while(it.hasNext()) {
+            elem.appendChild(doc.createElement("feature", it.next().toString()));
+        }
+
+        return doc;
+    }
+
+    /**
+     * @see net.jxta.document.Advertisement#getID()
+     */
+    @SuppressWarnings("deprecation")
+    public ID getID() {
+        try {
+            return (PeerID) IDFactory.fromURL(IDFactory.jxtaURL(peerId));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * @see net.jxta.document.Advertisement#getIndexFields()
+     */
+    public String[] getIndexFields() {
+        return indexFields;
+    }
+
+    /**
+     * @return Peer id that generated the advertisement.
+     */
+    public String getPeerId() {
+        return peerId;
+    }
+
+    /**
+     * @param peerId Peer id that generated the advertisement.
+     */
+    public void setPeerId(String peerId) {
+        this.peerId = peerId;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object target) {
+        if (target instanceof PresenceAdvertisement) {
+            return ((PresenceAdvertisement) target).getID().equals(getID());
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return getID().hashCode();
+    }
+
+    /**
+     * @see net.jxta.document.Advertisement#clone()
+     */
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException impossible) {
+            return null;
+        }
+    }
+
+}
