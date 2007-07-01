dbdaed59556caf1fc356a5388aa575e050e4bccb
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-07-01 12:28:15 +0000

    Moved ws binding to 0.1

---
+++ modules/tags/fabric3-binding-ws-0.1/src/main/java/org/fabric3/binding/ws/model/physical/WsBindingGenerator.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.binding.ws.model.physical;
+
+import org.fabric3.binding.ws.model.logical.WsBindingDefinition;
+import org.fabric3.spi.generator.BindingGenerator;
+import org.fabric3.spi.generator.GenerationException;
+import org.fabric3.spi.generator.GeneratorContext;
+import org.fabric3.spi.generator.GeneratorRegistry;
+import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.model.type.ReferenceDefinition;
+import org.fabric3.spi.model.type.ServiceDefinition;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
+
+/**
+ * Implementation of the hessian binding generator.
+ * 
+ * @version $Revision$ $Date$
+ */
+@EagerInit
+public class WsBindingGenerator implements
+    BindingGenerator<WsWireSourceDefinition, WsWireTargetDefinition, WsBindingDefinition> {
+
+    /**
+     * Injects the generator registry.
+     * 
+     * @param generatorRegistry Generator registry.
+     */
+    public WsBindingGenerator(@Reference
+    GeneratorRegistry generatorRegistry) {
+        generatorRegistry.register(WsBindingDefinition.class, this);
+    }
+
+    /**
+     * @see org.fabric3.spi.generator.BindingGenerator#generateWireSource(org.fabric3.spi.model.instance.LogicalBinding,
+     *      org.fabric3.spi.generator.GeneratorContext,
+     *      org.fabric3.spi.model.type.ServiceDefinition)
+     */
+    public WsWireSourceDefinition generateWireSource(LogicalBinding<WsBindingDefinition> logicalBinding,
+                                                          GeneratorContext generatorContext,
+                                                          ServiceDefinition serviceDefinition)
+        throws GenerationException {
+
+        WsWireSourceDefinition hwsd = new WsWireSourceDefinition();
+        hwsd.setUri(logicalBinding.getBinding().getTargetUri());
+        hwsd.setServiceInterface(serviceDefinition.getServiceContract().getInterfaceClass());
+
+        return hwsd;
+
+    }
+
+    /**
+     * @see org.fabric3.spi.generator.BindingGenerator#generateWireTarget(org.fabric3.spi.model.instance.LogicalBinding,
+     *      org.fabric3.spi.generator.GeneratorContext,
+     *      org.fabric3.spi.model.type.ReferenceDefinition)
+     */
+    public WsWireTargetDefinition generateWireTarget(LogicalBinding<WsBindingDefinition> logicalBinding,
+                                                          GeneratorContext generatorContext,
+                                                          ReferenceDefinition referenceDefinition)
+        throws GenerationException {
+
+        WsWireTargetDefinition hwtd = new WsWireTargetDefinition();
+        hwtd.setUri(logicalBinding.getBinding().getTargetUri());
+        hwtd.setReferenceInterface(referenceDefinition.getServiceContract().getInterfaceClass());
+
+        return hwtd;
+
+    }
+
+}
---
+++ modules/tags/fabric3-binding-ws-0.1/src/main/java/org/fabric3/binding/ws/wire/WsWireAttacher.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.fabric3.binding.ws.wire;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cxf.frontend.ClientProxyFactoryBean;
+import org.apache.cxf.frontend.ServerFactoryBean;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.fabric3.binding.ws.model.physical.WsWireSourceDefinition;
+import org.fabric3.binding.ws.model.physical.WsWireTargetDefinition;
+import org.fabric3.spi.builder.WiringException;
+import org.fabric3.spi.builder.component.WireAttacher;
+import org.fabric3.spi.builder.component.WireAttacherRegistry;
+import org.fabric3.spi.host.ServletHost;
+import org.fabric3.spi.model.physical.PhysicalOperationDefinition;
+import org.fabric3.spi.model.physical.PhysicalWireSourceDefinition;
+import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
+import org.fabric3.spi.wire.InvocationChain;
+import org.fabric3.spi.wire.Wire;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Property;
+import org.osoa.sca.annotations.Reference;
+
+/**
+ * Wire attacher for web services.
+ * 
+ * @version $Revision$ $Date$
+ */
+@EagerInit
+public class WsWireAttacher implements WireAttacher<WsWireSourceDefinition, WsWireTargetDefinition> {
+
+    /**
+     * CXF Servlet that serves the request.
+     */
+    private CXFServlet cxfServlet = new CXFServlet();
+    
+    /**
+     * Injects the wire attacher registry and servlet host.
+     * 
+     * @param wireAttacherRegistry Wire attacher registry.
+     * @param servletHost Servlet host for running CXF.
+     * @param contextPath Context path from which the web services are provisioned.
+     */
+    public WsWireAttacher(@Reference WireAttacherRegistry wireAttacherRegistry,
+                          @Reference ServletHost servletHost,
+                          @Property(name="contextPath") String contextPath) {
+        
+        wireAttacherRegistry.register(WsWireSourceDefinition.class, this);
+        wireAttacherRegistry.register(WsWireTargetDefinition.class, this);
+        servletHost.registerMapping(contextPath, cxfServlet);
+        
+    }
+
+    /**
+     * @see org.fabric3.spi.builder.component.WireAttacher#attachToSource(org.fabric3.spi.model.physical.PhysicalWireSourceDefinition, org.fabric3.spi.model.physical.PhysicalWireTargetDefinition, org.fabric3.spi.wire.Wire)
+     */
+    public void attachToSource(WsWireSourceDefinition sourceDefinition,
+                               PhysicalWireTargetDefinition targetDefinition,
+                               Wire wire) throws WiringException {
+        
+        Map<String, InvocationChain> headInterceptors = new HashMap<String, InvocationChain>();
+    
+        for (Map.Entry<PhysicalOperationDefinition, InvocationChain> entry : wire.getInvocationChains().entrySet()) {
+            headInterceptors.put(entry.getKey().getName(), entry.getValue());
+        }
+            
+        Class<?> service = sourceDefinition.getServiceInterface();
+        Object implementor = ServiceProxyHandler.newInstance(service, headInterceptors, wire);
+            
+        ServerFactoryBean serverFactoryBean = new ServerFactoryBean();
+        serverFactoryBean.setAddress(sourceDefinition.getUri().toASCIIString());
+        serverFactoryBean.setServiceClass(service);
+        serverFactoryBean.setServiceBean(implementor);
+     
+        serverFactoryBean.setBus(cxfServlet.getBus());
+        serverFactoryBean.create();
+
+    }
+
+    /**
+     * @see org.fabric3.spi.builder.component.WireAttacher#attachToTarget(org.fabric3.spi.model.physical.PhysicalWireSourceDefinition, org.fabric3.spi.model.physical.PhysicalWireTargetDefinition, org.fabric3.spi.wire.Wire)
+     */
+    public void attachToTarget(PhysicalWireSourceDefinition sourceDefinition,
+                               WsWireTargetDefinition targetDefinition,
+                               Wire wire) throws WiringException {
+        
+        Class<?> referenceClass = targetDefinition.getReferenceInterface();
+        
+        ClientProxyFactoryBean factory = new ClientProxyFactoryBean();
+        factory.setServiceClass(referenceClass);
+        factory.setAddress(targetDefinition.getUri().toString());
+            
+        Object proxy = factory.create();
+            
+        for(Method method : referenceClass.getDeclaredMethods()) {
+            for (Map.Entry<PhysicalOperationDefinition, InvocationChain> entry : wire.getInvocationChains().entrySet()) {
+                PhysicalOperationDefinition op = entry.getKey();
+                InvocationChain chain = entry.getValue();
+                if(method.getName().equals(op.getName())) {
+                    chain.addInterceptor(new WsTargetInterceptor(method, proxy));
+                }
+            }
+        }
+        
+    }
+
+}
