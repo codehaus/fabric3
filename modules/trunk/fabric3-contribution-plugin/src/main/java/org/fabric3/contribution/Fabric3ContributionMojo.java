79178404205053bd01732700b7a5046dd1ec2774
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-07-05 23:17:45 +0000

    import code for contribution plugin

--- modules/trunk/fabric3-contribution-plugin/src/main/java/org/fabric3/contribution/Fabric3ContributionMojo.java
+++ modules/trunk/fabric3-contribution-plugin/src/main/java/org/fabric3/contribution/Fabric3ContributionMojo.java
@@ -18,25 +18,131 @@ package org.fabric3.contribution;
 
 import java.io.File;
 
+import org.apache.maven.archiver.MavenArchiveConfiguration;
+import org.apache.maven.archiver.MavenArchiver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.archiver.jar.JarArchiver;
 
 /**
  * Builds an archive suitable for contribution to an SCA Domain.
  *
  * @version $Rev$ $Date$
+ * @goal package
+ * @phase package
  */
 public class Fabric3ContributionMojo extends AbstractMojo {
+    private static final String[] DEFAULT_EXCLUDES = new String[]{"**/package.html"};
+
+    private static final String[] DEFAULT_INCLUDES = new String[]{"**/**"};
 
     /**
-     * Name of the final output archive.
+     * Build output directory.
      *
-     * @parameter expression="${project.build.directory}/${project.build.finalName}.f3r"
+     * @parameter expression="${project.build.directory}"
      * @required
      */
-    public File outputFile;
+    protected File outputDirectory;
+
+    /**
+     * Name of the generated composite archive.
+     *
+     * @parameter expression="${project.build.finalName}"
+     */
+    protected String contributionName;
+
+    /**
+     * Classifier to add to the generated artifact.
+     *
+     * @parameter
+     */
+    protected String classifier;
+
+    /**
+     * Directory containing the classes to include in the archive.
+     *
+     * @parameter expression="${project.build.outputDirectory}"
+     * @required
+     */
+    protected File classesDirectory;
+
+    /**
+     * Standard Maven archive configuration.
+     *
+     * @parameter
+     */
+    protected MavenArchiveConfiguration archive = new MavenArchiveConfiguration();
+
+    /**
+     * The Jar archiver.
+     *
+     * @parameter expression="${component.org.codehaus.plexus.archiver.Archiver#jar}"
+     * @required
+     * @readonly
+     */
+    protected JarArchiver jarArchiver;
+
+    /**
+     * The maven project.
+     *
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    protected MavenProject project;
+
+    /**
+     * @component
+     * @required
+     * @readonly
+     */
+    protected MavenProjectHelper projectHelper;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
+        File contribution = createArchive();
+
+        if (classifier != null) {
+            projectHelper.attachArtifact(project, "f3r", classifier, contribution);
+        } else {
+            project.getArtifact().setFile(contribution);
+        }
+    }
+
+    protected File createArchive() throws MojoExecutionException {
+        File contribution = getJarFile(outputDirectory, contributionName, classifier);
+
+        MavenArchiver archiver = new MavenArchiver();
+        archiver.setArchiver(jarArchiver);
+        archiver.setOutputFile(contribution);
+        archive.setForced(true);
+
+        try {
+            File contentDirectory = classesDirectory;
+            if (!contentDirectory.exists()) {
+                getLog().warn("JAR will be empty - no content was marked for inclusion!");
+            } else {
+                archiver.getArchiver().addDirectory(contentDirectory, DEFAULT_INCLUDES, DEFAULT_EXCLUDES);
+            }
+
+            archiver.createArchive(project, archive);
+
+            return contribution;
+        }
+        catch (Exception e) {
+            throw new MojoExecutionException("Error assembling contribution", e);
+        }
+    }
+
+    protected File getJarFile(File buildDir, String finalName, String classifier) {
+        if (classifier != null) {
+            classifier = classifier.trim();
+            if (classifier.length() > 0) {
+                finalName = finalName + '-' + classifier;
+            }
+        }
+        return new File(buildDir, finalName + ".composite");
     }
 }
