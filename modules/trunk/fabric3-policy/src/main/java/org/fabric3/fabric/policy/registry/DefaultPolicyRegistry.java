230e6214c83ca8bbe91e3b6eb0c323a7d7d94920
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-07-20 04:02:28 +0000

    First cut of the policy registry, need to complete the test cases.

--- modules/trunk/fabric3-policy/src/main/java/org/fabric3/fabric/policy/registry/DefaultPolicyRegistry.java
+++ modules/trunk/fabric3-policy/src/main/java/org/fabric3/fabric/policy/registry/DefaultPolicyRegistry.java
@@ -18,6 +18,9 @@
  */
 package org.fabric3.fabric.policy.registry;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
@@ -33,27 +36,52 @@ import org.fabric3.spi.policy.registry.PolicyRegistry;
  */
 public class DefaultPolicyRegistry implements PolicyRegistry {
 
+    private Set<PolicySet> policySets = new HashSet<PolicySet>();
+    private Map<QName, Intent> intents = new HashMap<QName, Intent>();
+    
     /**
      * @see org.fabric3.spi.policy.registry.PolicyRegistry#getInterceptors(org.fabric3.spi.model.instance.LogicalScaArtifact)
      */
-    public Set<QName> getInterceptorBuilders(LogicalScaArtifact<?> arg0) {
-        // TODO Auto-generated method stub
+    public Set<QName> getInterceptorBuilders(final LogicalScaArtifact<?> scaArtifact) {
+        
+        LogicalScaArtifact<?> temp = scaArtifact;
+        
+        Set<QName> intentNames = new HashSet<QName>();
+        while(temp != null) {
+            intentNames.addAll(scaArtifact.getIntents());
+            temp = temp.getParent();
+        }
+        
+        for(QName intentName : intentNames) {
+            Intent intent = intents.get(intentName);
+            if(intent == null || !intent.doesConstrain(scaArtifact.getType())) {
+                intentNames.remove(intentName);
+            }
+        }
+        
+        for(PolicySet policySet : policySets) {
+            if(policySet.doesProvide(intentNames)) {
+                return policySet.getInterceptorBuilders();
+            }
+        }
+
+        
         return null;
+        
     }
 
     /**
      * @see org.fabric3.spi.policy.registry.PolicyRegistry#registerIntent(org.fabric3.spi.policy.model.Intent)
      */
-    public void registerIntent(Intent arg0) {
-        // TODO Auto-generated method stub
+    public void registerIntent(Intent intent) {
+        intents.put(intent.getName(), intent);
     }
 
     /**
      * @see org.fabric3.spi.policy.registry.PolicyRegistry#registerPolicySet(org.fabric3.spi.policy.model.PolicySet)
      */
-    public void registerPolicySet(PolicySet arg0) {
-        // TODO Auto-generated method stub
-
+    public void registerPolicySet(PolicySet policySet) {
+        policySets.add(policySet);
     }
 
 }
