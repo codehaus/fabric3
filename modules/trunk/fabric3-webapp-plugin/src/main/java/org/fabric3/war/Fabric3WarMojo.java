abf35ad3fb4ea1d96319799bfcc8fdc9b8e3cd81
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-08-14 02:21:29 +0000

    update the webapp plugin to have SPI classes loaded in the webapp classloader to provide contributed extensions with visibility to them

--- modules/trunk/fabric3-webapp-plugin/src/main/java/org/fabric3/war/Fabric3WarMojo.java
+++ modules/trunk/fabric3-webapp-plugin/src/main/java/org/fabric3/war/Fabric3WarMojo.java
@@ -43,21 +43,18 @@ import org.codehaus.plexus.util.IOUtil;
 
 /**
  * Add fabric3 runtime dependencies to a webapp.
- * 
+ * <p/>
  * Performs the following tasks.
- * 
- * <ul>
- *   <li>Adds the boot dependencies transitively to WEB-INF/fabric3/boot</li>
- *   <li>By default boot libraries are transitively resolved from webapp-host</li>
- *   <li>The version of boot libraries can be specified using configuration/runTimeVersion element</li>
- *   <li>Boot libraries can be overridden using the configuration/bootLibs element in the plugin</li>
- *   <li>Adds the extension artifacts specified using configuration/extensions to WEB-INF/fabric3/boot</li>
- * </ul>
+ * <p/>
+ * <ul> <li>Adds the boot dependencies transitively to WEB-INF/fabric3/boot</li> <li>By default boot libraries are
+ * transitively resolved from webapp-host</li> <li>The version of boot libraries can be specified using
+ * configuration/runTimeVersion element</li> <li>Boot libraries can be overridden using the configuration/bootLibs
+ * element in the plugin</li> <li>Adds the extension artifacts specified using configuration/extensions to
+ * WEB-INF/fabric3/boot</li> </ul>
+ *
+ * @version $Rev$ $Date$
  * @goal fabric3-war
  * @phase generate-resources
- * 
- * @version $Rev$ $Date$
- * 
  */
 public class Fabric3WarMojo extends AbstractMojo {
 
@@ -72,23 +69,28 @@ public class Fabric3WarMojo extends AbstractMojo {
     private static final String BOOT_PATH = FABRIC3_PATH + "/boot";
 
     /**
+     * Fabric3 boot path.
+     */
+    private static final String EXTENSIONS_PATH = FABRIC3_PATH + "/extensions";
+
+    /**
      * The directory where the webapp is built.
      *
      * @parameter expression="${project.build.directory}/${project.build.finalName}"
      * @required
      */
-     public File webappDirectory;
+    public File webappDirectory;
 
     /**
      * Artifact metadata source.
-     * 
+     *
      * @component
      */
     public ArtifactMetadataSource metadataSource;
 
     /**
      * Used to look up Artifacts in the remote repository.
-     * 
+     *
      * @parameter expression="${component.org.apache.maven.artifact.factory.ArtifactFactory}"
      * @required
      * @readonly
@@ -97,7 +99,7 @@ public class Fabric3WarMojo extends AbstractMojo {
 
     /**
      * Used to look up Artifacts in the remote repository.
-     * 
+     *
      * @parameter expression="${component.org.apache.maven.artifact.resolver.ArtifactResolver}"
      * @required
      * @readonly
@@ -106,7 +108,7 @@ public class Fabric3WarMojo extends AbstractMojo {
 
     /**
      * Location of the local repository.
-     * 
+     *
      * @parameter expression="${localRepository}"
      * @readonly
      * @required
@@ -115,7 +117,7 @@ public class Fabric3WarMojo extends AbstractMojo {
 
     /**
      * List of Remote Repositories used by the resolver
-     * 
+     *
      * @parameter expression="${project.remoteArtifactRepositories}"
      * @readonly
      * @required
@@ -124,10 +126,17 @@ public class Fabric3WarMojo extends AbstractMojo {
 
     /**
      * The directory for the generated WAR.
-     * 
+     *
      * @parameter
      */
     public Dependency[] bootLibs;
+
+    /**
+     * The spi dependencies.
+     *
+     * @parameter
+     */
+    public Dependency[] spiLibs;
 
     /**
      * Set of extension artifacts that should be deployed to the runtime.
@@ -138,10 +147,17 @@ public class Fabric3WarMojo extends AbstractMojo {
 
     /**
      * The default version of the runtime to use.
-     * 
+     *
      * @parameter
      */
     public String runTimeVersion;
+
+    /**
+     * The default version of the spi to use.
+     *
+     * @parameter
+     */
+    public String spiVersion;
 
     /**
      * Executes the MOJO.
@@ -157,23 +173,46 @@ public class Fabric3WarMojo extends AbstractMojo {
         getLog().info("Using fabric3 runtime version " + runTimeVersion);
 
         if (bootLibs == null) {
-            Dependency dependancy = new Dependency("org.codehaus.fabric3.webapp",
-                                                   "webapp-host",
-                                                   runTimeVersion);
-            bootLibs = new Dependency[] {dependancy};
+            Dependency dependancy = new Dependency("org.codehaus.fabric3.webapp", "webapp-host", runTimeVersion);
+            bootLibs = new Dependency[]{dependancy};
         }
 
+        if (spiLibs == null) {
+            String version;
+            if (spiVersion == null) {
+                version = runTimeVersion;
+            } else {
+                version = spiVersion;
+            }
+            Dependency dep1 = new Dependency("org.codehaus.fabric3", "fabric3-extension", version);
+            Dependency dep2 = new Dependency("stax", "stax-api", "1.0");
+            Dependency dep3 = new Dependency("org.codehaus.woodstox", "wstx-asl", "3.2.0");
+            Dependency dep4 = new Dependency("org.apache.tuscany", "commonj-api_r1.1", "1.0-incubator-M2");
+            Dependency dep5 = new Dependency("org.apache.geronimo.specs", "geronimo-j2ee-connector_1.5_spec", "1.0.1");
+            spiLibs = new Dependency[]{dep1, dep2, dep3, dep4, dep5};
+        }
         try {
+            Set<Artifact> spiArtifacts = new HashSet<Artifact>();
+            for (Dependency dependency : spiLibs) {
+                Set<Artifact> artifacts = resolveArtifact(dependency.getArtifact(artifactFactory), true);
+                spiArtifacts.addAll(artifacts);
+            }
             File bootDir = new File(webappDirectory, BOOT_PATH);
             bootDir.mkdirs();
             for (Dependency dependency : bootLibs) {
                 for (Artifact artifact : resolveArtifact(dependency.getArtifact(artifactFactory), true)) {
                     // FIXME use this when maven updates to plexus-utils-1.4
                     // FileUtils.copyFileToDirectoryIfModified(bootDir, artifact.getFile());
-                    copyFileToDirectoryIfModified(artifact.getFile(), bootDir);
+                    if (!spiArtifacts.contains(artifact)) {
+                        // don't copy dependencies related to SPIs as those are loaded in the host classloader
+                        copyFileToDirectoryIfModified(artifact.getFile(), bootDir);
+                    }
                 }
             }
-
+            File libDir = new File(webappDirectory, "WEB-INF/lib");
+            for (Artifact artifact : spiArtifacts) {
+                copyFileToDirectoryIfModified(artifact.getFile(), libDir);
+            }
             installExtensions();
 
             // TODO add user dependencies to the war in the way the contribution service expects
@@ -182,17 +221,15 @@ public class Fabric3WarMojo extends AbstractMojo {
         }
     }
 
-    // TODO install extensions as contributions to the runtime domain
-    // FIXME for now just add them to the boot directory
     private void installExtensions() throws ArtifactNotFoundException, ArtifactResolutionException, IOException,
             ArtifactMetadataRetrievalException {
         if (extensions == null) {
             return;
         }
 
-        File bootDir = new File(webappDirectory, BOOT_PATH);
+        File bootDir = new File(webappDirectory, EXTENSIONS_PATH);
         for (Dependency dependency : extensions) {
-            for (Artifact artifact : resolveArtifact(dependency.getArtifact(artifactFactory), true)) {
+            for (Artifact artifact : resolveArtifact(dependency.getArtifact(artifactFactory), false)) {
                 // FIXME use this when maven updates to plexus-utils-1.4
                 // FileUtils.copyFileToDirectoryIfModified(bootDir, artifact.getFile());
                 copyFileToDirectoryIfModified(artifact.getFile(), bootDir);
@@ -214,16 +251,18 @@ public class Fabric3WarMojo extends AbstractMojo {
 
     /**
      * Resolves the specified artifact.
-     * 
-     * @param artifact Artifact to be resolved.
+     *
+     * @param artifact   Artifact to be resolved.
      * @param transitive Whether to resolve transitively.
      * @return A set of resolved artifacts.
-     * @throws IOException In case of an unexpected IO error.
+     * @throws IOException                 In case of an unexpected IO error.
      * @throws ArtifactResolutionException If the artifact cannot be resolved.
-     * @throws ArtifactNotFoundException If the artifact is not found.
-     * @throws ArtifactMetadataRetrievalException In case of error in retrieving metadata.
+     * @throws ArtifactNotFoundException   If the artifact is not found.
+     * @throws ArtifactMetadataRetrievalException
+     *                                     In case of error in retrieving metadata.
      */
-    private Set<Artifact> resolveArtifact(Artifact artifact, boolean transitive) throws IOException, ArtifactResolutionException,
+    private Set<Artifact> resolveArtifact(Artifact artifact, boolean transitive)
+            throws IOException, ArtifactResolutionException,
             ArtifactNotFoundException, ArtifactMetadataRetrievalException {
 
         Set<Artifact> resolvedArtifacts = new HashSet<Artifact>();
@@ -242,7 +281,7 @@ public class Fabric3WarMojo extends AbstractMojo {
                                                                        artifact,
                                                                        remoteRepositories,
                                                                        localRepository,
-                metadataSource);
+                                                                       metadataSource);
 
         // Add the artifacts to the deployment unit
         for (Object depArtifact : result.getArtifacts()) {
