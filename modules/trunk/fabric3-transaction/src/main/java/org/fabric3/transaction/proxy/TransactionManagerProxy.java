74c3491c2eb4598d00af9791e81723fb80b6c570
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-08-23 20:52:25 +0000

    Renamed package.

--- modules/trunk/fabric3-transaction/src/main/java/org/fabric3/transaction/proxy/TransactionManagerProxy.java
+++ modules/trunk/fabric3-transaction/src/main/java/org/fabric3/transaction/proxy/TransactionManagerProxy.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.fabric3.transaction.jndi;
+package org.fabric3.transaction.proxy;
 
 import java.util.Hashtable;
 
