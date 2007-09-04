d5884a0b1502e7d5c4588fc22517722b29347673
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-09-04 04:27:27 +0000

    Refactored JOTM transaction manager and upped the version of JOTM to 2.0.10

--- modules/trunk/fabric3-tx-jotm/src/main/java/org/fabric3/tx/jotm/JotmTransactionManager.java
+++ modules/trunk/fabric3-tx-jotm/src/main/java/org/fabric3/tx/jotm/JotmTransactionManager.java
@@ -18,15 +18,136 @@
  */
 package org.fabric3.tx.jotm;
 
+import javax.naming.NamingException;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.InvalidTransactionException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+
 import org.objectweb.jotm.Current;
-import org.osoa.sca.annotations.Service;
+import org.objectweb.jotm.Jotm;
+import org.osoa.sca.annotations.Destroy;
+import org.osoa.sca.annotations.Init;
 
 /**
  * JOTM transaction manager with explicit service interface.
  * 
  * @version $Revision$ $Date$
  */
-@SuppressWarnings("serial")
-@Service(javax.transaction.TransactionManager.class)
-public final class JotmTransactionManager extends Current {
+public final class JotmTransactionManager implements TransactionManager {
+
+    private Current delegate;
+    private Jotm jotm;
+
+    /**
+     * Initializes JOTM.
+     * 
+     * @throws NamingException
+     */
+    @Init
+    public void init() throws NamingException {
+
+        this.delegate = Current.getCurrent();
+
+        // If none found, create new local JOTM instance.
+
+        if (this.delegate == null) {
+            this.jotm = new Jotm(true, false);
+            this.delegate = Current.getCurrent();
+
+        }
+
+    }
+    
+    /**
+     * Stops JOTM.
+     */
+    @Destroy
+    public void destroy() {
+        if (this.jotm != null) {
+            this.jotm.stop();
+        }
+    }
+
+    /**
+     * @throws SystemException 
+     * @throws NotSupportedException 
+     * @see javax.transaction.TransactionManager#begin()
+     */
+    public void begin() throws NotSupportedException, SystemException {
+        delegate.begin();
+    }
+
+    /**
+     * @throws SystemException 
+     * @throws RollbackException 
+     * @throws HeuristicRollbackException 
+     * @throws HeuristicMixedException 
+     * @see javax.transaction.TransactionManager#commit()
+     */
+    public void commit() throws HeuristicMixedException, HeuristicRollbackException, RollbackException, SystemException {
+        delegate.commit();
+    }
+
+    /**
+     * @throws SystemException 
+     * @see javax.transaction.TransactionManager#getStatus()
+     */
+    public int getStatus() throws SystemException {
+        return delegate.getStatus();
+    }
+
+    /**
+     * @throws SystemException 
+     * @see javax.transaction.TransactionManager#getTransaction()
+     */
+    public Transaction getTransaction() throws SystemException {
+        return delegate.getTransaction();
+    }
+
+    /**
+     * @throws SystemException 
+     * @throws InvalidTransactionException 
+     * @see javax.transaction.TransactionManager#resume(javax.transaction.Transaction)
+     */
+    public void resume(Transaction transaction) throws InvalidTransactionException, SystemException {
+        delegate.resume(transaction);
+    }
+
+    /**
+     * @throws SystemException  
+     * @see javax.transaction.TransactionManager#rollback()
+     */
+    public void rollback() throws SystemException {
+        delegate.rollback();
+    }
+
+    /**
+     * @throws SystemException 
+     * @see javax.transaction.TransactionManager#setRollbackOnly()
+     */
+    public void setRollbackOnly() throws SystemException {
+        delegate.setRollbackOnly();
+    }
+
+    /**
+     * @throws SystemException 
+     * @see javax.transaction.TransactionManager#setTransactionTimeout(int)
+     */
+    public void setTransactionTimeout(int timeout) throws SystemException {
+        delegate.setTransactionTimeout(timeout);
+    }
+
+    /**
+     * @throws SystemException 
+     * @see javax.transaction.TransactionManager#suspend()
+     */
+    public Transaction suspend() throws SystemException {
+        return delegate.suspend();
+    }
+    
 }
