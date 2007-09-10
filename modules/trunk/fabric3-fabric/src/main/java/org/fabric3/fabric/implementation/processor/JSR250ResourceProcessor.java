d5ee60e0164068b42bd50d7b90f26b67df95011c
aanderson <aanderson@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-09-10 15:51:28 +0000

    FABRICTHREE-17 added support for javax.annotation.Resource/PreDestory/PostCreate

---
+++ modules/trunk/fabric3-fabric/src/main/java/org/fabric3/fabric/implementation/processor/JSR250ResourceProcessor.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.implementation.processor;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import org.fabric3.spi.loader.LoaderContext;
+import org.fabric3.pojo.processor.ImplementationProcessorExtension;
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.pojo.scdl.Resource;
+import org.fabric3.pojo.processor.ProcessingException;
+
+
+/**
+ * Processes an {@link @Resource} annotation, updating the component type with corresponding {@link
+ * org.fabric3.pojo.scdl.Resource}
+ *
+ * @version $Rev: 751 $ $Date: 2007-08-16 14:50:14 -0500 (Thu, 16 Aug 2007) $
+ */
+public class JSR250ResourceProcessor extends ImplementationProcessorExtension {
+
+    //TODO suport superclass injections
+    public JSR250ResourceProcessor() {
+    }
+
+    public <T> void visitClass(Class<T> clazz, PojoComponentType type, LoaderContext context) throws ProcessingException {
+        /* TODO according to the JSR250 spec if a class is annotated with the
+        resource annotation it signifies that the class will look up the
+        resource at runtime. Since the JSR does not define a lookup method
+        method (maybe jndi?) I am unsure if this needs to be processed or not
+        since we are only interested in dependency injection
+         */
+        javax.annotation.Resources resources = clazz.getAnnotation(javax.annotation.Resources.class);
+        if (resources != null) {
+            for (javax.annotation.Resource resource : resources.value()) {
+                //TODO declare intent
+            }
+        }
+        javax.annotation.Resource resource = clazz.getAnnotation(javax.annotation.Resource.class);
+        if (resource != null) {
+            //TODO declare intent
+        }
+    }
+
+    public void visitMethod(Method method, PojoComponentType type, LoaderContext context) throws ProcessingException {
+        javax.annotation.Resource annotation = method.getAnnotation(javax.annotation.Resource.class);
+        if (annotation == null) {
+            return;
+        }
+        if (method.getParameterTypes().length != 1) {
+            throw new IllegalResourceException("Resource setter must have one parameter", method.toString());
+        }
+        
+        String methodName = method.getName();
+        if (methodName.startsWith("set") && methodName.length() > 3) {
+            methodName = Character.toLowerCase(methodName.charAt(3)) + methodName.substring(4);
+        } else {
+            throw new IllegalResourceException("Setter method names must begin with set", method.toString());
+        }
+        
+        if (void.class != method.getReturnType()) {
+            throw new IllegalResourceException("Setter method must have void return type", method.toString());
+        }
+        
+
+        String name = annotation.name();
+        if(name.length() < 1){
+            name=methodName;
+        }
+
+        if (type.getResources().get(name) != null) {
+            throw new DuplicateResourceException(name);
+        }
+
+        Class<?> methodParameterType = method.getParameterTypes()[0];
+        Class<?> declaredType = annotation.type();
+        if (declaredType != Object.class) {
+            if (!methodParameterType.isAssignableFrom(declaredType)) {
+                throw new IllegalResourceException("Resource type " + declaredType+ " is not compatible with method parameter", method.toString());
+            }
+        } else {
+            declaredType = methodParameterType;
+        }
+
+        String mappedName = annotation.mappedName();
+        Resource<?> resource = createResource(name, declaredType, method);
+        resource.setOptional(false);
+        if (mappedName.length() > 0) {
+            resource.setMappedName(mappedName);
+        }
+        //TODO test handling of super methods, support sharable
+        type.add(resource);
+    }
+
+    public void visitField(Field field, PojoComponentType type, LoaderContext context) throws ProcessingException {
+
+        javax.annotation.Resource annotation = field.getAnnotation(javax.annotation.Resource.class);
+        if (annotation == null) {
+            return;
+        }
+        String name = annotation.name();
+        if (name.length() < 1) {
+            name = field.getName();
+        }
+        if (type.getResources().get(name) != null) {
+            throw new DuplicateResourceException(name);
+        }
+
+        Class<?> fieldType = field.getType();
+        Class<?> declaredType = annotation.type();
+        if (declaredType != Object.class) {
+            if (!fieldType.isAssignableFrom(declaredType)) {
+                throw new IllegalResourceException("Resource type " + declaredType+ " is not compatible with field ", field.toString());
+            }
+        } else {
+            declaredType = fieldType;
+        }
+        String mappedName = annotation.mappedName();
+
+        Resource<?> resource = createResource(name, declaredType, field);
+        resource.setOptional(false);
+        if (mappedName.length() > 0) {
+            resource.setMappedName(mappedName);
+        }
+        //TODO test handling of super fields, support sharable
+        type.add(resource);
+    }
+
+    public <T> Resource<T> createResource(String name, Class<T> type, Member member) {
+        return new Resource<T>(name, type, member);
+    }
+
+    public <T> void visitConstructor(Constructor<T> constructor, PojoComponentType type, LoaderContext context) throws ProcessingException {
+    }
+}
---
+++ modules/trunk/fabric3-fabric/src/test/java/org/fabric3/fabric/implementation/processor/JSR250ResourceProcessorTestCase.java
@@ -0,0 +1,171 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.implementation.processor;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.pojo.scdl.Resource;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Rev: 751 $ $Date: 2007-08-16 14:50:14 -0500 (Thu, 16 Aug 2007) $
+ */
+public class JSR250ResourceProcessorTestCase extends TestCase {
+
+    PojoComponentType type;
+    JSR250ResourceProcessor processor = new JSR250ResourceProcessor();
+
+    public void testVisitField() throws Exception {
+        Field field = JSR250ResourceProcessorTestCase.Foo.class.getDeclaredField("bar");
+        processor.visitField(field, type, null);
+        Resource resource = type.getResources().get("bar");
+        assertNotNull(resource);
+        assertFalse(resource.isOptional());
+        assertNull(resource.getMappedName());
+        assertEquals(field.getType(), resource.getType());
+        
+        field = JSR250ResourceProcessorTestCase.Foo.class.getDeclaredField("subBar");
+        processor.visitField(field, type, null);
+        resource = type.getResources().get("someName");
+        assertNotNull(resource);
+        assertFalse(resource.isOptional());
+        assertEquals("mapped",resource.getMappedName());
+        assertEquals(JSR250ResourceProcessorTestCase.SubBar.class, resource.getType());
+    }
+
+    public void testVisitMethod() throws Exception {
+        Method method = JSR250ResourceProcessorTestCase.Foo.class.getMethod("setBar", JSR250ResourceProcessorTestCase.Bar.class);
+        processor.visitMethod(method, type, null);
+        Resource resource = type.getResources().get("bar");
+        assertNotNull(resource);
+        assertFalse(resource.isOptional());
+        assertNull(resource.getMappedName());
+        assertEquals(method.getParameterTypes()[0], resource.getType());
+        
+        method = JSR250ResourceProcessorTestCase.Foo.class.getMethod("setSubBar", JSR250ResourceProcessorTestCase.Bar.class);
+        processor.visitMethod(method, type, null);
+        resource = type.getResources().get("someName");
+        assertNotNull(resource);
+        assertFalse(resource.isOptional());
+        assertEquals("mapped",resource.getMappedName());
+        assertEquals(JSR250ResourceProcessorTestCase.SubBar.class, resource.getType());
+        
+    }
+
+    public void testVisitBadMethod() throws Exception {
+        Method method = JSR250ResourceProcessorTestCase.Foo.class.getMethod("setBadMethod");
+        try {
+            processor.visitMethod(method, type, null);
+            fail();
+        } catch (IllegalResourceException e) {
+            // expected
+        }
+        method = JSR250ResourceProcessorTestCase.Foo.class.getMethod("setBadMethodType",Bar.class);
+        try {
+            processor.visitMethod(method, type, null);
+            fail();
+        } catch (IllegalResourceException e) {
+            // expected
+        } 
+        method = JSR250ResourceProcessorTestCase.Foo.class.getMethod("setBadMethodReturnType");
+        try {
+            processor.visitMethod(method, type, null);
+            fail();
+        } catch (IllegalResourceException e) {
+            // expected
+        }
+    }
+    
+        public void testVisitBadField() throws Exception {
+        Field field = JSR250ResourceProcessorTestCase.Foo.class.getDeclaredField("badField");
+        try {
+            processor.visitField(field, type, null);
+            fail();
+        } catch (IllegalResourceException e) {
+            // expected
+        }
+    }
+
+
+    public void testDuplicateResources() throws Exception {
+        Field field = JSR250ResourceProcessorTestCase.Foo.class.getDeclaredField("bar");
+        processor.visitField(field, type, null);
+        try {
+            processor.visitField(field, type, null);
+            fail();
+        } catch (DuplicateResourceException e) {
+            //expected
+        }
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        type = new PojoComponentType(null);
+    }
+
+    private class Foo {
+
+        @javax.annotation.Resource
+        protected Bar bar;
+
+        @javax.annotation.Resource(name = "someName", mappedName = "mapped", type = SubBar.class, shareable=false)
+        protected Bar subBar;
+
+        @javax.annotation.Resource(type=Foo.class)
+        protected Bar badField;
+
+        @javax.annotation.Resource
+        public void setBar(Bar bar) {
+        }
+
+        @javax.annotation.Resource(name = "someName", mappedName = "mapped", type = SubBar.class, shareable=false)
+        public void setSubBar(Bar bar) {
+        }
+
+        
+        @javax.annotation.Resource
+        public void setBadMethod() {
+        }
+        
+        @javax.annotation.Resource(type=Foo.class)
+        public void setBadMethodType(Bar bar) {
+        }
+        
+        @javax.annotation.Resource
+        public String setBadMethodReturnType() {
+            return null;
+        }
+
+    }
+    
+    private class SubFoo extends Foo {
+        
+    }
+
+    private interface Bar {
+
+    }
+    
+    private interface SubBar extends Bar {
+
+    }
+}
