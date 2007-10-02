42f0bea95435093615ea6ea6a7cea44f2581865e
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-10-02 18:56:23 +0000

    Started on the persistence unit annotation processor.

---
+++ modules/trunk/fabric3-jpa/src/main/java/org/fabric3/jpa/PersistenceUnitResource.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.jpa;
+
+import java.lang.reflect.Member;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.fabric3.pojo.scdl.JavaMappedResource;
+
+/**
+ * Represents an entity manager factory treated as a resource.
+ *
+ * @version $Revision$ $Date$
+ */
+public final class PersistenceUnitResource extends JavaMappedResource<EntityManagerFactory> {
+    
+    private final String unitName;
+
+    /**
+     * Initializes the resource name and persistence unit name.
+     * 
+     * @param name Name of the resource.
+     * @param unitName Persistence unit name.
+     * @param member Member against which the annotation is declared.
+     */
+    public PersistenceUnitResource(String name, String unitName, Member member) {
+        super(name, EntityManagerFactory.class, member);
+        this.unitName = unitName;
+    }
+    
+    /**
+     * Gets the persistence unit name.
+     * @return Persistence unit name.
+     */
+    public final String getUnitName() {
+        return this.unitName;
+    }
+
+}
