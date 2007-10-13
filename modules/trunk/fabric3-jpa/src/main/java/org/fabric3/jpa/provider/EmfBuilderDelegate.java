d72fb1e9d84dd662135c0d2e476716229ea42d48
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-10-13 16:58:48 +0000

    Added the delegate interface.

---
+++ modules/trunk/fabric3-jpa/src/main/java/org/fabric3/jpa/provider/EmfBuilderDelegate.java
@@ -0,0 +1,40 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.jpa.provider;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.spi.PersistenceUnitInfo;
+
+/**
+ * Delegate interface for creating entity manager factories for adding 
+ * provider specific hook-ins.
+ * 
+ * @version $Revision$ $Date$
+ */
+public interface EmfBuilderDelegate {
+    
+    /**
+     * @param info
+     * @param classLoader
+     * @param props
+     * @return
+     */
+    EntityManagerFactory build(PersistenceUnitInfo info, ClassLoader classLoader, Properties props);
+
+}
