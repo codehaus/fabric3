df6055154cb1df058e7831216c41cec5f54f60dc
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-10-30 09:40:24 +0000

    Added the source files

---
+++ tests/trunk/test-binding-ws-cxf-axis2/src/main/java/org/fabric3/ws/interop/hello/client/HelloTest.java
@@ -0,0 +1,39 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.ws.interop.hello.client;
+
+import org.osoa.sca.annotations.Reference;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class HelloTest extends TestCase {
+    
+    private HelloClient helloClient;
+    
+    @Reference
+    public void setHelloClient(HelloClient helloClient) {
+        this.helloClient = helloClient;
+    }
+    
+    public void testInterop() {
+        assertEquals("Hello, Fred", helloClient.sayHello("Fred"));
+    }
+
+}
