6135fe0d0c8688149827e2f59965ce4fd014874b
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-11-29 18:28:00 +0000

    maven namespace

--- modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/Constants.java
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/Constants.java
@@ -10,6 +10,11 @@ public interface Constants {
     String FABRIC3_NS = "http://fabric3.org/xmlns/sca/2.0-alpha";
 
     /**
+     * Fabric3 base maven namespace.
+     */
+    String FABRIC3_MAVEN_NS = "http://fabric3.org/xmlns/sca/2.0-alpha/maven";
+
+    /**
      * Fabric3 system namespace.
      */
     String FABRIC3_SYSTEM_NS = "http://fabric3.org/xmlns/sca/system/2.0-alpha";
