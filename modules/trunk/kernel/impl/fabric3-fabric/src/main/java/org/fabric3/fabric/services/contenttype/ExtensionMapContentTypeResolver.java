9f67772daf5e0a013e68c8bdb9629cd940e03af5
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-12-05 22:24:34 +0000

    have configured extension types override default mappings as dev environments may map SCA extensions (e.g. *.composite) to know types such as application/xml

--- modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/services/contenttype/ExtensionMapContentTypeResolver.java
+++ modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/services/contenttype/ExtensionMapContentTypeResolver.java
@@ -60,16 +60,16 @@ public class ExtensionMapContentTypeResolver implements ContentTypeResolver {
         try {
 
             URLConnection connection = contentUrl.openConnection();
-            String contentType = connection.getContentType();
+            String extension = urlString.substring(urlString.lastIndexOf('.') + 1);
+            String contentType = extensionMap.get(extension);
+            if (contentType != null) {
+                return contentType;
+            }
+            contentType = connection.getContentType();
 
             if(contentType == null || UNKNOWN_CONTENT.equals(contentType) || "application/octet-stream".equals(contentType)) {
                 String filename = contentUrl.getFile();
                 contentType = typeMap.getContentType(filename);
-            }
-
-            if(contentType == null || UNKNOWN_CONTENT.equals(contentType) || "application/octet-stream".equals(contentType)) {
-                String extension = urlString.substring(urlString.lastIndexOf('.') + 1);
-                contentType = extensionMap.get(extension);
             }
 
             return contentType;
