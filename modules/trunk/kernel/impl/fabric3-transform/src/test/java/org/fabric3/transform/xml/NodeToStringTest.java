6ba72036fed0e91f5438353dd71a66807149865e
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-12-15 21:18:48 +0000

    Added transformer for serializing XML.

---
+++ modules/trunk/kernel/impl/fabric3-transform/src/test/java/org/fabric3/transform/xml/NodeToStringTest.java
@@ -0,0 +1,44 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.transform.xml;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class NodeToStringTest extends TestCase {
+
+    public void testTransform() throws Exception {
+        
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><policy xmlns=\"http://www.fabric3.org\">Test data</policy>";
+        
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        
+        Element el = doc.createElementNS("http://www.fabric3.org", "policy");
+        el.appendChild(doc.createTextNode("Test data"));
+        
+        String output = new NodeToString().transform(el, null);
+        assertEquals(expected, output);
+    }
+
+}
