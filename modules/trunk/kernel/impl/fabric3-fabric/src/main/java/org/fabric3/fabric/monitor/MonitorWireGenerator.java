f018a4f3a6d4364da48f26fcf90a4f738f77e5b7
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-12-20 16:59:03 +0000

    add skeleton wire generator for monitors

---
+++ modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/monitor/MonitorWireGenerator.java
@@ -0,0 +1,56 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.monitor;
+
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.spi.generator.GenerationException;
+import org.fabric3.spi.generator.GeneratorContext;
+import org.fabric3.spi.generator.GeneratorRegistry;
+import org.fabric3.spi.generator.ResourceWireGenerator;
+import org.fabric3.spi.model.instance.LogicalResource;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class MonitorWireGenerator implements ResourceWireGenerator<MonitorWireTargetDefinition, MonitorResource> {
+
+    private final GeneratorRegistry registry;
+
+    public MonitorWireGenerator(@Reference(name = "registry")GeneratorRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Init
+    public void init() {
+        registry.register(MonitorResource.class, this);
+    }
+
+/*
+    @Destroy
+    public void destroy() {
+        registry.unregister(MonitorResource.class);
+    }
+*/
+
+    public MonitorWireTargetDefinition genearteWireTargetDefinition(LogicalResource<MonitorResource> resource,
+                                                                    GeneratorContext context)
+            throws GenerationException {
+        return null;
+    }
+}
