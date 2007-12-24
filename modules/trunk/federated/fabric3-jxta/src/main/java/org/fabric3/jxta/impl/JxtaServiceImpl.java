3bb71e15d3c919a23716c7349673090696a89240
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-12-24 13:31:53 +0000

    javadoc cleanup and remove unnecessary assertions

--- modules/trunk/federated/fabric3-jxta/src/main/java/org/fabric3/jxta/impl/JxtaServiceImpl.java
+++ modules/trunk/federated/fabric3-jxta/src/main/java/org/fabric3/jxta/impl/JxtaServiceImpl.java
@@ -1,9 +1,24 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.fabric3.jxta.impl;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-
 import javax.security.cert.CertificateException;
 
 import net.jxta.credential.AuthenticationCredential;
@@ -14,25 +29,24 @@ import net.jxta.peergroup.NetPeerGroupFactory;
 import net.jxta.peergroup.PeerGroup;
 import net.jxta.platform.NetworkConfigurator;
 import net.jxta.protocol.ModuleImplAdvertisement;
-
-import org.fabric3.host.runtime.HostInfo;
-import org.fabric3.jxta.JxtaService;
 import org.osoa.sca.annotations.EagerInit;
 import org.osoa.sca.annotations.Init;
 import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.host.runtime.HostInfo;
+import org.fabric3.jxta.JxtaService;
 
 /**
  * Default implementation of the JXTA service.
  *
  * @version $Revsion$ $Date$
- *
  */
 @EagerInit
 public class JxtaServiceImpl implements JxtaService {
 
     /**
      * Well known peer group id.
-     *
+     * <p/>
      * TODO This may need to be generated from the domain name.
      */
     private static final Fabric3PeerGroupID PEER_GROUP_ID =
@@ -47,15 +61,13 @@ public class JxtaServiceImpl implements JxtaService {
     // Network configurator
     private NetworkConfigurator networkConfigurator;
 
-    /**
-     * @see org.fabric3.jxta.JxtaService#getDomainGroup()
-     */
     public PeerGroup getDomainGroup() {
         return domainGroup;
     }
 
     /**
      * Injects the host info.
+     *
      * @param hostInfo Information on the host.
      */
     @Reference
@@ -64,10 +76,9 @@ public class JxtaServiceImpl implements JxtaService {
     }
 
     /**
-     * Injects the JXTA network configurator. This is required to be configure with
-     * the principal and credentials to stop the JXTA configuration window being
-     * started interactively. The configurator can also be pre-injected with the
-     * protocol parameters like TCP listen port etc.
+     * Injects the JXTA network configurator. This is required to be configure with the principal and credentials to
+     * stop the JXTA configuration window being started interactively. The configurator can also be pre-injected with
+     * the protocol parameters like TCP listen port etc.
      *
      * @param networkConfigurator JXTA network configurator.
      */
@@ -78,17 +89,11 @@ public class JxtaServiceImpl implements JxtaService {
 
     /**
      * Joins the domain and creates the discovery and resolver services.
-     *
      */
     @Init
     public void start() {
-
-        assert hostInfo != null;
-        assert networkConfigurator != null;
-
         configure();
         createAndJoinDomainGroup();
-
     }
 
     /**
@@ -123,8 +128,6 @@ public class JxtaServiceImpl implements JxtaService {
 
     /**
      * Creates and joins the domain peer group.
-     *
-     * @throws Exception In case of unexpected JXTA exceptions.
      */
     private void createAndJoinDomainGroup() {
 
@@ -155,8 +158,9 @@ public class JxtaServiceImpl implements JxtaService {
     /*
      * Well known peer grroup.
      */
-    @SuppressWarnings("serial")
     private static class Fabric3PeerGroupID extends net.jxta.impl.id.CBID.PeerGroupID {
+        private static final long serialVersionUID = 2529239017940982956L;
+
         public Fabric3PeerGroupID(UUID uuid) {
             super(uuid);
         }
