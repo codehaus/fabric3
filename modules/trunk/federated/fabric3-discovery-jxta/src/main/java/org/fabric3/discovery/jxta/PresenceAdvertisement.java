7307fcfcc0ef6313e700429418980c72e0e34778
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-12-27 14:08:41 +0000

    marshall resource description information and change Instantiator from being an inner class for clarity

--- modules/trunk/federated/fabric3-discovery-jxta/src/main/java/org/fabric3/discovery/jxta/PresenceAdvertisement.java
+++ modules/trunk/federated/fabric3-discovery-jxta/src/main/java/org/fabric3/discovery/jxta/PresenceAdvertisement.java
@@ -19,10 +19,10 @@
 package org.fabric3.discovery.jxta;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
 import javax.xml.namespace.QName;
 
 import net.jxta.document.Advertisement;
@@ -35,127 +35,48 @@ import net.jxta.document.StructuredDocumentFactory;
 import net.jxta.id.ID;
 import net.jxta.id.IDFactory;
 
-import org.fabric3.jxta.impl.Fabric3JxtaException;
+import org.fabric3.scdl.ResourceDescription;
+import org.fabric3.spi.model.topology.ClassLoaderResourceDescription;
 import org.fabric3.spi.model.topology.RuntimeInfo;
 
 /**
  * @version $Revsion$ $Date$
  */
 public class PresenceAdvertisement extends Advertisement {
-
     /*
      * Register the advertisement type.
      */
     static {
-        AdvertisementFactory.registerAdvertisementInstance(getAdvertisementType(),
-                                                           new PresenceAdvertisement.Instantiator());
+        AdvertisementFactory.registerAdvertisementInstance(getAdvertisementType(), new Instantiator());
     }
 
-    /*
-     * Indexed fields.
-     */
     public static final String indexFields[] = new String[]{};
 
-    /*
-     * Runtime id.
-     */
     private URI runtimeId;
-
-    /*
-     * Peer id.
-     */
     private String peerId;
-
-    /*
-     * Message destination.
-     */
     private String messageDestination;
-
-    /*
-     * Features.
-     */
     private Set<QName> features = new HashSet<QName>();
-
-    /*
-     * Component URIs.
-     *
-     */
     private Set<URI> components = new HashSet<URI>();
+    private List<ResourceDescription<?>> resourceDescriptions = new ArrayList<ResourceDescription<?>>();
 
-    /*
-     * Instantiator.
-     */
-    public static final class Instantiator implements AdvertisementFactory.Instantiator {
-
-        public String getAdvertisementType() {
-            return PresenceAdvertisement.getAdvertisementType();
+    @SuppressWarnings("deprecation")
+    public ID getID() {
+        try {
+            return IDFactory.fromURL(IDFactory.jxtaURL(peerId));
+        } catch (Exception e) {
+            return null;
         }
+    }
 
-        public Advertisement newInstance() {
-            return new PresenceAdvertisement();
-        }
-
-        public Advertisement newInstance(Element element) {
-
-            PresenceAdvertisement adv = new PresenceAdvertisement();
-
-            Element elem = (Element) element.getChildren("runtimeId").nextElement();
-            if (elem != null && elem.getValue() != null) {
-                adv.runtimeId = URI.create(elem.getValue().toString());
-            }
-
-            elem = (Element) element.getChildren("peerId").nextElement();
-            if (elem != null && elem.getValue() != null) {
-                adv.setPeerId(elem.getValue().toString());
-            }
-
-            elem = (Element) element.getChildren("messageDestination").nextElement();
-            if (elem != null && elem.getValue() != null) {
-                adv.messageDestination = elem.getValue().toString();
-            }
-
-            elem = (Element) element.getChildren("features").nextElement();
-            adv.features = new HashSet<QName>();
-            Enumeration children = elem.getChildren("feature");
-            while (children.hasMoreElements()) {
-                elem = (Element) children.nextElement();
-                if (elem != null && elem.getValue() != null) {
-                    adv.features.add(QName.valueOf(elem.getValue().toString()));
-                }
-            }
-
-            elem = (Element) element.getChildren("components").nextElement();
-            adv.components = new HashSet<URI>();
-            children = elem.getChildren("component");
-            while (children.hasMoreElements()) {
-                elem = (Element) children.nextElement();
-                if (elem != null && elem.getValue() != null) {
-                    try {
-                        adv.components.add(new URI(elem.getValue().toString()));
-                    } catch (URISyntaxException ex) {
-                        throw new Fabric3JxtaException(ex);
-                    }
-                }
-            }
-
-            return adv;
-
-        }
-
+    public String[] getIndexFields() {
+        return indexFields;
     }
 
     /**
-     * @param runtimeInfo Runtime info.
+     * @return Peer id that generated the advertisement.
      */
-    public void setRuntimeInfo(RuntimeInfo runtimeInfo) {
-        runtimeId = runtimeInfo.getId();
-        if (runtimeInfo.getFeatures() != null) {
-            features = runtimeInfo.getFeatures();
-        }
-        messageDestination = runtimeInfo.getMessageDestination();
-        if (runtimeInfo.getComponents() != null) {
-            components = runtimeInfo.getComponents();
-        }
+    public String getPeerId() {
+        return peerId;
     }
 
     /**
@@ -198,54 +119,44 @@ public class PresenceAdvertisement extends Advertisement {
         elem = doc.createElement("messageDestination", messageDestination);
         doc.appendChild(elem);
 
+        // serialize feature qnames
         elem = doc.createElement("features");
         doc.appendChild(elem);
         for (QName feature : features) {
             elem.appendChild(doc.createElement("feature", feature.toString()));
         }
 
+        // serialize component uris
         elem = doc.createElement("components");
         doc.appendChild(elem);
         for (URI component : components) {
             elem.appendChild(doc.createElement("component", component.toString()));
         }
 
+        // serialize resource descriptions
+        elem = doc.createElement("resourceDescriptions");
+        doc.appendChild(elem);
+        for (ResourceDescription<?> description : resourceDescriptions) {
+            // TODO this may need to be made extensible
+            if (!(ClassLoaderResourceDescription.class.isInstance(description))) {
+                String type = description.getClass().getName();
+                throw new UnknownResourceDescriptionType("Unknown resource type [" + type + "]", type);
+            }
+            ClassLoaderResourceDescription clr = ClassLoaderResourceDescription.class.cast(description);
+            Element clrElement = doc.createElement("classLoaderResourceDescription");
+            elem.appendChild(clrElement);
+            Element identifierElement = doc.createElement("identifier", clr.getIdentifier().toString());
+            clrElement.appendChild(identifierElement);
+            for (URI uri : clr.getParents()) {
+                Element parent = doc.createElement("parent", uri.toString());
+                clrElement.appendChild(parent);
+            }
+        }
         return doc;
     }
 
-    @SuppressWarnings("deprecation")
-    public ID getID() {
-        try {
-            return IDFactory.fromURL(IDFactory.jxtaURL(peerId));
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public String[] getIndexFields() {
-        return indexFields;
-    }
-
-    /**
-     * @return Peer id that generated the advertisement.
-     */
-    public String getPeerId() {
-        return peerId;
-    }
-
-    /**
-     * @param peerId Peer id that generated the advertisement.
-     */
-    public void setPeerId(String peerId) {
-        this.peerId = peerId;
-    }
-
     public boolean equals(Object target) {
-        if (target instanceof PresenceAdvertisement) {
-            return ((PresenceAdvertisement) target).getID().equals(getID());
-        } else {
-            return false;
-        }
+        return target instanceof PresenceAdvertisement && ((PresenceAdvertisement) target).getID().equals(getID());
     }
 
     @Override
@@ -259,6 +170,52 @@ public class PresenceAdvertisement extends Advertisement {
         } catch (CloneNotSupportedException impossible) {
             return null;
         }
+    }
+
+    /**
+     * @param runtimeInfo Runtime info.
+     */
+    void setRuntimeInfo(RuntimeInfo runtimeInfo) {
+        runtimeId = runtimeInfo.getId();
+        if (runtimeInfo.getFeatures() != null) {
+            features = runtimeInfo.getFeatures();
+        }
+        messageDestination = runtimeInfo.getMessageDestination();
+        if (runtimeInfo.getComponents() != null) {
+            components = runtimeInfo.getComponents();
+        }
+        if (runtimeInfo.getResourceDescriptions() != null) {
+            resourceDescriptions = runtimeInfo.getResourceDescriptions();
+        }
+    }
+
+    /**
+     * Sets the peer id that generated the advertisement.
+     *
+     * @param peerId Peer id that generated the advertisement
+     */
+    void setPeerId(String peerId) {
+        this.peerId = peerId;
+    }
+
+    void setRuntimeId(URI runtimeId) {
+        this.runtimeId = runtimeId;
+    }
+
+    void setMessageDestination(String messageDestination) {
+        this.messageDestination = messageDestination;
+    }
+
+    void setFeatures(Set<QName> features) {
+        this.features = features;
+    }
+
+    void setComponents(Set<URI> components) {
+        this.components = components;
+    }
+
+    void setResourceDescriptions(List<ResourceDescription<?>> resourceDescriptions) {
+        this.resourceDescriptions = resourceDescriptions;
     }
 
 }
