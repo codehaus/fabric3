50395f11ded652b35e332b6507574ed4b1cc4264
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2007-12-29 16:54:25 +0000

    consolidate federated listeners into one package

--- modules/trunk/federated/fabric3-messaging/src/main/java/org/fabric3/messaging/listener/PhysicalChangeSetListener.java
+++ modules/trunk/federated/fabric3-messaging/src/main/java/org/fabric3/messaging/listener/PhysicalChangeSetListener.java
@@ -14,14 +14,14 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.fabric3.messaging.impl;
+package org.fabric3.messaging.listener;
 
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamReader;
 
+import org.osoa.sca.annotations.EagerInit;
 import org.osoa.sca.annotations.Init;
 import org.osoa.sca.annotations.Reference;
-import org.osoa.sca.annotations.EagerInit;
 
 import org.fabric3.host.monitor.MonitorFactory;
 import org.fabric3.spi.deployer.Deployer;
@@ -34,20 +34,20 @@ import org.fabric3.spi.services.messaging.RequestListener;
  * @version $Rev$ $Date$
  */
 @EagerInit
-public class DeploymentListener implements RequestListener {
+public class PhysicalChangeSetListener implements RequestListener {
     private MarshalService marshalService;
-    private DeployerMonitor monitor;
+    private ListenerMonitor monitor;
     private MessagingEventService messagingService;
     private Deployer deployer;
 
-    public DeploymentListener(@Reference MarshalService marshalService,
-                              @Reference MessagingEventService messagingService,
-                              @Reference Deployer deployer,
-                              @Reference MonitorFactory factory) {
+    public PhysicalChangeSetListener(@Reference MarshalService marshalService,
+                                     @Reference MessagingEventService messagingService,
+                                     @Reference Deployer deployer,
+                                     @Reference MonitorFactory factory) {
         this.marshalService = marshalService;
         this.messagingService = messagingService;
         this.deployer = deployer;
-        monitor = factory.getMonitor(DeployerMonitor.class);
+        monitor = factory.getMonitor(ListenerMonitor.class);
     }
 
     @Init
@@ -57,22 +57,20 @@ public class DeploymentListener implements RequestListener {
     }
 
     /**
-     * Deploys the component.
+     * Applies the changes set
      *
-     * @param content SCDL content.
+     * @param content the serialized changeset.
      * @return Response to the request message.
-     *         <p/>
-     *         TODO Handle response messages.
      */
     public XMLStreamReader onRequest(XMLStreamReader content) {
         try {
             PhysicalChangeSet changeSet = marshalService.unmarshall(PhysicalChangeSet.class, content);
             deployer.applyChangeSet(changeSet);
         } catch (Throwable ex) {
-            monitor.error("Demarshalling changeset", ex);
+            monitor.error(ex);
             return null;
         }
-
+        // TODO Handle response messages.
         return null;
     }
 }
