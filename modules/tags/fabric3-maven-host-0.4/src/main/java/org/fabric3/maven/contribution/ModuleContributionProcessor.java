d4df63a3ac1ee0c34acab440786938f808b45d0b
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-01-21 19:50:31 +0000

    [maven-release-plugin]  copy for tag fabric3-maven-host-0.4

---
+++ modules/tags/fabric3-maven-host-0.4/src/main/java/org/fabric3/maven/contribution/ModuleContributionProcessor.java
@@ -0,0 +1,196 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.maven.contribution;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.fabric.services.contribution.processor.Action;
+import org.fabric3.fabric.util.FileHelper;
+import org.fabric3.host.contribution.ContributionException;
+import org.fabric3.loader.common.LoaderContextImpl;
+import org.fabric3.maven.runtime.MavenHostInfo;
+import org.fabric3.spi.loader.Loader;
+import org.fabric3.spi.loader.LoaderContext;
+import org.fabric3.spi.loader.LoaderException;
+import org.fabric3.spi.model.type.ContributionResourceDescription;
+import org.fabric3.spi.services.contenttype.ContentTypeResolutionException;
+import org.fabric3.spi.services.contenttype.ContentTypeResolver;
+import org.fabric3.spi.services.contribution.ArtifactLocationEncoder;
+import org.fabric3.spi.services.contribution.Contribution;
+import org.fabric3.spi.services.contribution.ContributionManifest;
+import org.fabric3.spi.services.contribution.ContributionProcessor;
+import org.fabric3.spi.services.contribution.ProcessorRegistry;
+import org.fabric3.spi.services.contribution.Resource;
+
+/**
+ * Processes a Maven module directory.
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class ModuleContributionProcessor implements ContributionProcessor {
+    public static final String[] CONTENT_TYPES = {"application/vnd.fabric3.maven-project"};
+
+    private ProcessorRegistry registry;
+    private ContentTypeResolver contentTypeResolver;
+    private ArtifactLocationEncoder encoder;
+    private MavenHostInfo hostInfo;
+    private Loader loader;
+
+    public ModuleContributionProcessor(@Reference ProcessorRegistry registry,
+                                       @Reference ContentTypeResolver contentTypeResolver,
+                                       @Reference ArtifactLocationEncoder encoder,
+                                       @Reference MavenHostInfo hostInfo,
+                                       @Reference Loader loader) {
+        this.registry = registry;
+        this.contentTypeResolver = contentTypeResolver;
+        this.encoder = encoder;
+        this.hostInfo = hostInfo;
+        this.loader = loader;
+    }
+
+    public String[] getContentTypes() {
+        return CONTENT_TYPES;
+    }
+
+    @Init
+    public void init() {
+        registry.register(this);
+    }
+
+    public void process(Contribution contribution, ClassLoader loader) throws ContributionException {
+        ClassLoader oldClassloader = Thread.currentThread().getContextClassLoader();
+        URI contributionUri = contribution.getUri();
+        try {
+            Thread.currentThread().setContextClassLoader(loader);
+            for (Resource resource : contribution.getResources()) {
+                registry.processResource(contributionUri, resource, loader);
+            }
+        } finally {
+            Thread.currentThread().setContextClassLoader(oldClassloader);
+        }
+    }
+
+    public void processManifest(Contribution contribution) throws ContributionException {
+        ContributionManifest manifest;
+        try {
+            URL sourceUrl = contribution.getLocation();
+            URL manifestURL = new URL(sourceUrl.toExternalForm() + "/classes/META-INF/sca-contribution.xml");
+            ClassLoader cl = getClass().getClassLoader();
+            URI uri = contribution.getUri();
+            LoaderContext context = new LoaderContextImpl(cl, uri, null);
+            manifest = loader.load(manifestURL, ContributionManifest.class, context);
+        } catch (LoaderException e) {
+            if (e.getCause() instanceof FileNotFoundException) {
+                manifest = new ContributionManifest();
+            } else {
+                throw new ContributionException(e);
+            }
+        } catch (MalformedURLException e) {
+            manifest = new ContributionManifest();
+        }
+        contribution.setManifest(manifest);
+
+        iterateArtifacts(contribution, new Action() {
+            public void process(Contribution contribution, String contentType, URL url)
+                    throws ContributionException {
+                InputStream stream = null;
+                try {
+                    stream = url.openStream();
+                    registry.processManifestArtifact(contribution.getManifest(), contentType, stream);
+                } catch (IOException e) {
+                    throw new ContributionException(e);
+                } finally {
+                    try {
+                        if (stream != null) {
+                            stream.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public void index(Contribution contribution) throws ContributionException {
+        iterateArtifacts(contribution, new Action() {
+            public void process(Contribution contribution, String contentType, URL url)
+                    throws ContributionException {
+                registry.indexResource(contribution, contentType, url);
+            }
+        });
+    }
+
+    public void updateContributionDescription(Contribution contribution, ContributionResourceDescription description)
+            throws ContributionException {
+        String file = contribution.getLocation().getFile();
+        try {
+            URL classes = new File(file, "classes").toURI().toURL();
+            URL testClasses = new File(file, "test-classes").toURI().toURL();
+            URL encodedClasses = encoder.encode(classes);
+            description.addArtifactUrl(encodedClasses);
+            URL encodedTestClasses = encoder.encode(testClasses);
+            description.addArtifactUrl(encodedTestClasses);
+            for (URL url : hostInfo.getDependencyUrls()) {
+                description.addArtifactUrl(encoder.encode(url));
+            }
+        } catch (MalformedURLException e) {
+            throw new ContributionException(e);
+        }
+    }
+
+    private void iterateArtifacts(Contribution contribution, Action action) throws ContributionException {
+        File root = FileHelper.toFile(contribution.getLocation());
+        assert root.isDirectory();
+        iterateArtifactsResursive(contribution, action, root);
+    }
+
+    private void iterateArtifactsResursive(Contribution contribution, Action action, File dir)
+            throws ContributionException {
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                iterateArtifactsResursive(contribution, action, file);
+            } else {
+                try {
+                    URL entryUrl = file.toURI().toURL();
+                    String contentType = contentTypeResolver.getContentType(entryUrl);
+                    action.process(contribution, contentType, entryUrl);
+                } catch (MalformedURLException e) {
+                    throw new ContributionException(e);
+                } catch (IOException e) {
+                    throw new ContributionException(e);
+                } catch (ContentTypeResolutionException e) {
+                    throw new ContributionException(e);
+                }
+            }
+        }
+
+    }
+
+}
