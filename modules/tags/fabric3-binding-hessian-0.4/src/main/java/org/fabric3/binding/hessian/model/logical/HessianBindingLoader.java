38bf2ddd1378d392b931bca0fe716bc454fcc4c1
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-01-21 21:47:53 +0000

    [maven-release-plugin]  copy for tag fabric3-binding-hessian-0.4

---
+++ modules/tags/fabric3-binding-hessian-0.4/src/main/java/org/fabric3/binding/hessian/model/logical/HessianBindingLoader.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.binding.hessian.model.logical;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.fabric3.extension.loader.LoaderExtension;
+import org.fabric3.spi.loader.LoaderContext;
+import org.fabric3.spi.loader.LoaderException;
+import org.fabric3.spi.loader.LoaderRegistry;
+import org.fabric3.spi.loader.LoaderUtil;
+import org.fabric3.spi.loader.PolicyHelper;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
+
+/**               
+ * @version $Revision$ $Date$
+ */
+@EagerInit
+public class HessianBindingLoader extends LoaderExtension<HessianBindingDefinition> {
+
+    /** Qualified name for the binding element. */
+    public static final QName BINDING_QNAME = 
+        new QName("http://www.fabric3.org/binding/hessian/0.2", "binding.hessian");
+    
+    private final PolicyHelper policyHelper;
+    
+    /**
+     * Injects the registry.
+     * @param registry Loader registry.
+     */
+    public HessianBindingLoader(@Reference LoaderRegistry registry, @Reference PolicyHelper policyHelper) {
+        super(registry);
+        this.policyHelper = policyHelper;
+    }
+
+    @Override
+    public QName getXMLType() {
+        return BINDING_QNAME;
+    }
+
+    public HessianBindingDefinition load(XMLStreamReader reader, LoaderContext loaderContext)
+        throws XMLStreamException, LoaderException {
+        
+        HessianBindingDefinition bd = null;
+        
+        try {
+
+            String uri = reader.getAttributeValue(null, "uri");
+            if(uri == null) {
+                throw new LoaderException("The uri attribute is not specified");
+            }
+            bd = new HessianBindingDefinition(new URI(uri));
+            
+            policyHelper.loadPolicySetsAndIntents(bd, reader);
+            
+        } catch(URISyntaxException ex) {
+            throw new LoaderException(ex);
+        }
+        
+        LoaderUtil.skipToEndElement(reader);
+        return bd;
+        
+    }
+
+}
---
+++ modules/tags/fabric3-binding-hessian-0.4/src/main/java/org/fabric3/binding/hessian/model/physical/HessianWireTargetDefinition.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.binding.hessian.model.physical;
+
+import java.net.URI;
+
+import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
+
+/**
+ * Physical wire target definition for Hessian binding.
+ * 
+ * @version $Revision$ $Date$
+ */
+public class HessianWireTargetDefinition extends PhysicalWireTargetDefinition {
+    private final URI classLoaderId;
+
+    /**
+     * Constructor.
+     *
+     * @param classLoaderId the classloader id to deserialize responses with
+     */
+
+    public HessianWireTargetDefinition(URI classLoaderId) {
+        this.classLoaderId = classLoaderId;
+    }
+
+    /**
+     * Returns the classloader id to deserialize responses with.
+     *
+     * @return the classloader id to deserialize responses with
+     */
+    public URI getClassLoaderId() {
+        return classLoaderId;
+    }
+}
