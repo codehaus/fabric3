d4f5d9be05262d41f7dbccc32cc92c7842634a99
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-01-21 18:43:28 +0000

    [maven-release-plugin]  copy for tag fabric3-java-0.2

---
+++ modules/tags/fabric3-java-0.2/src/main/java/org/fabric3/java/JavaWireAttacher.java
@@ -0,0 +1,194 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.java;
+
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import org.osoa.sca.annotations.Destroy;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.pojo.reflection.InvokerInterceptor;
+import org.fabric3.pojo.wire.PojoWireAttacher;
+import org.fabric3.spi.ObjectFactory;
+import org.fabric3.spi.builder.WiringException;
+import org.fabric3.spi.builder.component.SourceWireAttacher;
+import org.fabric3.spi.builder.component.SourceWireAttacherRegistry;
+import org.fabric3.spi.builder.component.TargetWireAttacher;
+import org.fabric3.spi.builder.component.TargetWireAttacherRegistry;
+import org.fabric3.spi.builder.component.WireAttachException;
+import org.fabric3.spi.component.Component;
+import org.fabric3.spi.component.ScopeContainer;
+import org.fabric3.spi.model.instance.ValueSource;
+import org.fabric3.spi.model.physical.PhysicalOperationDefinition;
+import org.fabric3.spi.model.physical.PhysicalWireSourceDefinition;
+import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
+import org.fabric3.spi.runtime.component.ComponentManager;
+import org.fabric3.spi.services.classloading.ClassLoaderRegistry;
+import org.fabric3.spi.transform.PullTransformer;
+import org.fabric3.spi.transform.TransformerRegistry;
+import org.fabric3.spi.util.UriHelper;
+import org.fabric3.spi.wire.InvocationChain;
+import org.fabric3.spi.wire.ProxyService;
+import org.fabric3.spi.wire.Wire;
+
+/**
+ * The component builder for Java implementation types. Responsible for creating the Component runtime artifact from a
+ * physical component definition
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class JavaWireAttacher extends PojoWireAttacher implements SourceWireAttacher<JavaWireSourceDefinition>, TargetWireAttacher<JavaWireTargetDefinition> {
+
+    private final SourceWireAttacherRegistry sourceWireAttacherRegistry;
+    private final TargetWireAttacherRegistry targetWireAttacherRegistry;
+    private final ComponentManager manager;
+    private final ProxyService proxyService;
+    private final ClassLoaderRegistry classLoaderRegistry;
+
+    public JavaWireAttacher(@Reference ComponentManager manager,
+                            @Reference SourceWireAttacherRegistry sourceWireAttacherRegistry,
+                            @Reference TargetWireAttacherRegistry targetWireAttacherRegistry,
+                            @Reference ProxyService proxyService,
+                            @Reference ClassLoaderRegistry classLoaderRegistry,
+                            @Reference(name = "transformerRegistry")TransformerRegistry<PullTransformer<?, ?>> transformerRegistry) {
+        super(transformerRegistry, classLoaderRegistry);
+        this.sourceWireAttacherRegistry = sourceWireAttacherRegistry;
+        this.targetWireAttacherRegistry = targetWireAttacherRegistry;
+        this.manager = manager;
+        this.proxyService = proxyService;
+        this.classLoaderRegistry = classLoaderRegistry;
+    }
+
+    @Init
+    public void init() {
+        sourceWireAttacherRegistry.register(JavaWireSourceDefinition.class, this);
+        targetWireAttacherRegistry.register(JavaWireTargetDefinition.class, this);
+    }
+
+    @Destroy
+    public void destroy() {
+        sourceWireAttacherRegistry.unregister(JavaWireSourceDefinition.class, this);
+        targetWireAttacherRegistry.unregister(JavaWireTargetDefinition.class, this);
+    }
+
+    public void attachToSource(JavaWireSourceDefinition sourceDefinition,
+                               PhysicalWireTargetDefinition targetDefinition,
+                               Wire wire) throws WireAttachException {
+
+        URI sourceUri = sourceDefinition.getUri();
+        URI sourceName = UriHelper.getDefragmentedName(sourceDefinition.getUri());
+        JavaComponent<?> source = (JavaComponent) manager.getComponent(sourceName);
+        ValueSource referenceSource = new ValueSource(ValueSource.ValueSourceType.REFERENCE, sourceUri.getFragment());
+
+        Class<?> type;
+        try {
+            type = classLoaderRegistry.loadClass(sourceDefinition.getClassLoaderId(), sourceDefinition.getInterfaceName());
+        } catch (ClassNotFoundException e) {
+            throw new WireAttachException("Unable to load interface class: " + sourceDefinition.getInterfaceName(),
+                                          sourceUri,
+                                          null,
+                                          e);
+        }
+
+        ObjectFactory<?> factory = createWireObjectFactory(type, sourceDefinition.isConversational(), wire);
+        Object key = getKey(sourceDefinition, source, referenceSource);
+        source.attachReferenceToTarget(referenceSource, factory, key);
+
+        if (!wire.getCallbackInvocationChains().isEmpty()) {
+            URI callbackUri = sourceDefinition.getCallbackUri();
+            ValueSource callbackSource =
+                    new ValueSource(ValueSource.ValueSourceType.SERVICE, callbackUri.getFragment());
+            Class<?> callbackType = source.getMemberType(callbackSource);
+            source.setObjectFactory(callbackSource, createCallbackWireObjectFactory(callbackType));
+        }
+    }
+
+    public void attachToTarget(PhysicalWireSourceDefinition sourceDefinition,
+                               JavaWireTargetDefinition targetDefinition,
+                               Wire wire) throws WireAttachException {
+        URI targetName = UriHelper.getDefragmentedName(targetDefinition.getUri());
+        Component component = manager.getComponent(targetName);
+        assert component instanceof JavaComponent;
+        JavaComponent<?> target = (JavaComponent) component;
+
+        ScopeContainer<?> scopeContainer = target.getScopeContainer();
+        Class<?> implementationClass = target.getImplementationClass();
+        ClassLoader loader = implementationClass.getClassLoader();
+        // attach the invoker interceptor to forward invocation chains
+        for (Map.Entry<PhysicalOperationDefinition, InvocationChain> entry : wire.getInvocationChains().entrySet()) {
+            PhysicalOperationDefinition operation = entry.getKey();
+            InvocationChain chain = entry.getValue();
+            List<String> params = operation.getParameters();
+            Class<?>[] paramTypes = new Class<?>[params.size()];
+            assert loader != null;
+            for (int i = 0; i < params.size(); i++) {
+                String param = params.get(i);
+                try {
+                    paramTypes[i] = classLoaderRegistry.loadClass(loader, param);
+                } catch (ClassNotFoundException e) {
+                    URI sourceUri = sourceDefinition.getUri();
+                    URI targetUri = targetDefinition.getUri();
+                    throw new WireAttachException("Implementation class not found", sourceUri, targetUri, e);
+                }
+            }
+            Method method;
+            try {
+                method = implementationClass.getMethod(operation.getName(), paramTypes);
+            } catch (NoSuchMethodException e) {
+                URI sourceUri = sourceDefinition.getUri();
+                URI targetUri = targetDefinition.getUri();
+                throw new WireAttachException("No matching method found", sourceUri, targetUri, e);
+            }
+            chain.addInterceptor(createInterceptor(method, target, scopeContainer));
+        }
+    }
+
+    public void attachObjectFactory(JavaWireSourceDefinition source, ObjectFactory<?> objectFactory) throws WiringException {
+        URI sourceId = UriHelper.getDefragmentedName(source.getUri());
+        JavaComponent<?> sourceComponent = (JavaComponent<?>) manager.getComponent(sourceId);
+        ValueSource referenceSource = new ValueSource(ValueSource.ValueSourceType.REFERENCE, source.getUri().getFragment());
+
+        Object key = getKey(source, sourceComponent, referenceSource);
+        sourceComponent.attachReferenceToTarget(referenceSource, objectFactory, key);
+    }
+
+    public ObjectFactory<?> createObjectFactory(JavaWireTargetDefinition target) throws WiringException {
+        URI targetId = UriHelper.getDefragmentedName(target.getUri());
+        JavaComponent<?> targetComponent = (JavaComponent<?>) manager.getComponent(targetId);
+        return targetComponent.createObjectFactory();
+    }
+
+    private <T, CONTEXT> InvokerInterceptor<T, CONTEXT> createInterceptor(Method method,
+                                                                          JavaComponent<T> component,
+                                                                          ScopeContainer<CONTEXT> scopeContainer) {
+        return new InvokerInterceptor<T, CONTEXT>(method, component, scopeContainer);
+    }
+
+    private <T> ObjectFactory<T> createWireObjectFactory(Class<T> type, boolean isConversational, Wire wire) {
+        return proxyService.createObjectFactory(type, isConversational, wire);
+    }
+
+    private <T> ObjectFactory<T> createCallbackWireObjectFactory(Class<T> type) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ modules/tags/fabric3-java-0.2/src/test/java/org/fabric3/java/JavaComponentTypeLoaderTestCase.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.java;
+
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
+import org.easymock.IAnswer;
+
+import org.fabric3.pojo.processor.IntrospectionRegistry;
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.spi.loader.LoaderContext;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class JavaComponentTypeLoaderTestCase extends TestCase {
+
+    @SuppressWarnings("unchecked")
+    public void testPojoComponentTypeCreatedForIntrospection() throws Exception {
+        IntrospectionRegistry registry = EasyMock.createMock(IntrospectionRegistry.class);
+        registry.introspect(
+                (Class) EasyMock.isA(Object.class),
+                EasyMock.isA(PojoComponentType.class),
+                EasyMock.isA(LoaderContext.class));
+        EasyMock.expectLastCall().andStubAnswer(new IAnswer() {
+            public Object answer() throws Throwable {
+                return null;
+            }
+        });
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        LoaderContext context = EasyMock.createMock(LoaderContext.class);
+        EasyMock.expect(context.getTargetClassLoader()).andStubReturn(cl);
+        EasyMock.replay(registry, context);
+        JavaComponentTypeLoaderImpl loader = new JavaComponentTypeLoaderImpl(registry);
+        JavaImplementation implementation = new JavaImplementation();
+        implementation.setImplementationClass(Object.class.getName());
+        loader.loadByIntrospection(implementation, context);
+        EasyMock.verify(registry, context);
+    }
+
+/*
+    @SuppressWarnings("unchecked")
+    public void testPojoComponentTypeCreatedForSideFileLoadAndReturned() throws Exception {
+        LoaderRegistry registry = EasyMock.createMock(LoaderRegistry.class);
+        registry.load(
+                (URL) EasyMock.isNull(),
+            EasyMock.eq(PojoComponentType.class),
+            (LoaderContext) EasyMock.isNull());
+        EasyMock.expectLastCall().andStubAnswer(new IAnswer() {
+            public Object answer() throws Throwable {
+                return EasyMock.getCurrentArguments()[0];
+            }
+        });
+        EasyMock.replay(registry);
+        JavaComponentTypeLoader loader = new JavaComponentTypeLoader(registry, null);
+        assertEquals(PojoComponentType.class, loader.loadFromSidefile(null, null).getClass());
+        EasyMock.verify(registry);
+    }
+*/
+
+
+}
