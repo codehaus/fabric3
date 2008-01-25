9e8326f98ed5e4358ad1d5adce40860d38603dd9
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-01-25 10:55:32 +0000

    [maven-release-plugin]  copy for tag fabric3-async-runtime-0.1

---
+++ modules/tags/fabric3-async-runtime-0.1/src/main/java/org/fabric3/fabric/async/NonBlockingInterceptorBuilder.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.async;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.spi.builder.BuilderException;
+import org.fabric3.spi.builder.interceptor.InterceptorBuilder;
+import org.fabric3.spi.builder.interceptor.InterceptorBuilderRegistry;
+import org.fabric3.spi.services.work.WorkScheduler;
+
+/**
+ * Creates a non-blocking interceptor
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class NonBlockingInterceptorBuilder
+        implements InterceptorBuilder<NonBlockingInterceptorDefinition, NonBlockingInterceptor> {
+    private WorkScheduler scheduler;
+    private InterceptorBuilderRegistry registry;
+
+    public NonBlockingInterceptorBuilder(@Reference WorkScheduler scheduler,
+                                         @Reference InterceptorBuilderRegistry registry) {
+        this.scheduler = scheduler;
+        this.registry = registry;
+    }
+
+    @Init
+    public void init() {
+        registry.register(NonBlockingInterceptorDefinition.class, this);
+    }
+
+
+    public NonBlockingInterceptor build(NonBlockingInterceptorDefinition definition) throws BuilderException {
+        return new NonBlockingInterceptor(scheduler);
+    }
+
+}
