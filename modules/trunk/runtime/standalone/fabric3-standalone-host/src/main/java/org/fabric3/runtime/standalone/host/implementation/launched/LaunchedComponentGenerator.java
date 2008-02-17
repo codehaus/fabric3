a1e114bc2d0d19cefeba0974796732969d6007ae
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-02-17 09:52:18 +0000

    refactor Launched to use generation helper

--- modules/trunk/runtime/standalone/fabric3-standalone-host/src/main/java/org/fabric3/runtime/standalone/host/implementation/launched/LaunchedComponentGenerator.java
+++ modules/trunk/runtime/standalone/fabric3-standalone-host/src/main/java/org/fabric3/runtime/standalone/host/implementation/launched/LaunchedComponentGenerator.java
@@ -19,23 +19,21 @@
 package org.fabric3.runtime.standalone.host.implementation.launched;
 
 import java.net.URI;
-import java.util.Map;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
 
 import org.fabric3.java.JavaComponentDefinition;
 import org.fabric3.java.JavaWireSourceDefinition;
 import org.fabric3.java.JavaWireTargetDefinition;
-import org.fabric3.scdl.InjectionSiteMapping;
 import org.fabric3.pojo.instancefactory.InstanceFactoryDefinition;
-import org.fabric3.pojo.scdl.ConstructorDefinition;
-import org.fabric3.pojo.scdl.JavaMappedProperty;
-import org.fabric3.pojo.scdl.JavaMappedReference;
-import org.fabric3.scdl.InjectionSite;
+import org.fabric3.pojo.instancefactory.InstanceFactoryGenerationHelper;
 import org.fabric3.pojo.scdl.PojoComponentType;
 import org.fabric3.scdl.ComponentDefinition;
-import org.fabric3.scdl.Property;
 import org.fabric3.scdl.ValueSource;
-import org.fabric3.scdl.ServiceDefinition;
+import org.fabric3.spi.generator.ClassLoaderGenerator;
 import org.fabric3.spi.generator.ComponentGenerator;
+import org.fabric3.spi.generator.GenerationException;
 import org.fabric3.spi.generator.GeneratorContext;
 import org.fabric3.spi.generator.GeneratorRegistry;
 import org.fabric3.spi.model.instance.LogicalComponent;
@@ -46,53 +44,52 @@ import org.fabric3.spi.model.physical.PhysicalComponentDefinition;
 import org.fabric3.spi.model.physical.PhysicalWireSourceDefinition;
 import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
 import org.fabric3.spi.policy.Policy;
-import org.osoa.sca.annotations.EagerInit;
-import org.osoa.sca.annotations.Reference;
 
 /**
  * @version $Rev$ $Date$
  */
 @EagerInit
 public class LaunchedComponentGenerator implements ComponentGenerator<LogicalComponent<Launched>> {
+    private final InstanceFactoryGenerationHelper helper;
+    private final ClassLoaderGenerator classLoaderGenerator;
 
-    public LaunchedComponentGenerator(@Reference GeneratorRegistry registry) {
+    public LaunchedComponentGenerator(@Reference GeneratorRegistry registry,
+                                      @Reference ClassLoaderGenerator classLoaderGenerator,
+                                      @Reference InstanceFactoryGenerationHelper helper) {
+        this.classLoaderGenerator = classLoaderGenerator;
+        this.helper = helper;
         registry.register(Launched.class, this);
     }
 
-    @SuppressWarnings({"unchecked"})
     public PhysicalComponentDefinition generate(LogicalComponent<Launched> component,
-                                                GeneratorContext context) {
+                                                GeneratorContext context) throws GenerationException {
         
         ComponentDefinition<Launched> definition = component.getDefinition();
         Launched implementation = definition.getImplementation();
-        // TODO not a safe cast
         PojoComponentType type = implementation.getComponentType();
-        JavaComponentDefinition pDefinition = new JavaComponentDefinition();
-        URI componentId = component.getUri();
-        pDefinition.setGroupId(component.getParent().getUri());
-        pDefinition.setComponentId(componentId);
-        // set the classloader id temporarily until multiparent classloading is in palce
-        pDefinition.setClassLoaderId(URI.create("sca://./applicationClassLoader"));
-        pDefinition.setScope(type.getImplementationScope());
-        // TODO get classloader id
+
+        // create the instance factory definition
         InstanceFactoryDefinition providerDefinition = new InstanceFactoryDefinition();
+        providerDefinition.setConstructor(type.getConstructorDefinition().getSignature());
         providerDefinition.setInitMethod(type.getInitMethod());
         providerDefinition.setDestroyMethod(type.getDestroyMethod());
-
-        // JFM FIXME seems hacky and add to JavaPCDG
-        Integer level = definition.getInitLevel();
-        if (level == null) {
-            pDefinition.setInitLevel(type.getInitLevel());
-        } else {
-            pDefinition.setInitLevel(level);
-        }
         providerDefinition.setImplementationClass(implementation.getImplementationClass());
-        providerDefinition.setConstructor(type.getConstructorDefinition().getSignature());
-        processConstructorSites(type, providerDefinition);
-        processReferenceSites(type, providerDefinition);
-        // TODO process properties
-        pDefinition.setInstanceFactoryProviderDefinition(providerDefinition);
-        return pDefinition;
+        helper.processInjectionSites(component, providerDefinition);
+
+        // create the physical component definition
+        URI componentId = component.getUri();
+        JavaComponentDefinition physical = new JavaComponentDefinition();
+        physical.setComponentId(componentId);
+        physical.setGroupId(component.getParent().getUri());
+        physical.setScope(type.getImplementationScope());
+        physical.setInitLevel(helper.getInitLevel(definition, type));
+        physical.setInstanceFactoryProviderDefinition(providerDefinition);
+        helper.processPropertyValues(component, physical);
+        // generate the classloader resource definition
+        URI classLoaderId = classLoaderGenerator.generate(component, context);
+        physical.setClassLoaderId(classLoaderId);
+
+        return physical;
     }
 
     public PhysicalWireSourceDefinition generateWireSource(LogicalComponent<Launched> source,
@@ -119,71 +116,6 @@ public class LaunchedComponentGenerator implements ComponentGenerator<LogicalCom
         JavaWireTargetDefinition wireDefinition = new JavaWireTargetDefinition();
         wireDefinition.setUri(service.getUri());
         return wireDefinition;
-    }
-
-    /**
-     * Creates InjectionSources for constructor parameters for the component implementation
-     *
-     * @param type               the component type corresponding to the implementation
-     * @param providerDefinition the instance factory provider definition
-     */
-    private void processConstructorSites(PojoComponentType type,
-                                         InstanceFactoryDefinition providerDefinition) {
-        Map<String, JavaMappedReference> references = type.getReferences();
-        Map<String, JavaMappedProperty<?>> properties = type.getProperties();
-        Map<String, ServiceDefinition> services = type.getServices();
-
-        // process constructor injectors
-        ConstructorDefinition ctorDef = type.getConstructorDefinition();
-        for (String name : ctorDef.getInjectionNames()) {
-            JavaMappedReference reference = references.get(name);
-            if (reference != null) {
-                ValueSource source = new ValueSource(ValueSource.ValueSourceType.REFERENCE, name);
-                providerDefinition.addCdiSource(source);
-                continue;
-            }
-            Property<?> property = properties.get(name);
-            if (property != null) {
-                ValueSource source = new ValueSource(ValueSource.ValueSourceType.PROPERTY, name);
-                providerDefinition.addCdiSource(source);
-                continue;
-            }
-            ServiceDefinition service = services.get(name);
-            if (service != null) {
-                // SPEC The SCA spec does not specifically allow this yet -  submit an enhnacement request
-                ValueSource source = new ValueSource(ValueSource.ValueSourceType.CALLBACK, name);
-                providerDefinition.addCdiSource(source);
-                continue;
-            }
-            throw new AssertionError();
-        }
-
-    }
-
-    /**
-     * Creates InjectionSiteMappings for references declared by the component implementation
-     *
-     * @param type               the component type corresponding to the implementation
-     * @param providerDefinition the instance factory provider definition
-     */
-    private void processReferenceSites(PojoComponentType type,
-                                       InstanceFactoryDefinition providerDefinition) {
-        Map<String, JavaMappedReference> references = type.getReferences();
-        for (Map.Entry<String, JavaMappedReference> entry : references.entrySet()) {
-            JavaMappedReference reference = entry.getValue();
-            InjectionSite injectionSite = reference.getMemberSite();
-            if (injectionSite == null) {
-                // JFM this is dubious, the reference is mapped to a constructor so skip processing
-                // ImplementationProcessorService does not set the member type to a ctor when creating the ref
-                continue;
-            }
-            ValueSource source = new ValueSource(ValueSource.ValueSourceType.REFERENCE, entry.getKey());
-
-            InjectionSiteMapping mapping = new InjectionSiteMapping();
-            mapping.setSource(source);
-            mapping.setSite(injectionSite);
-            providerDefinition.addInjectionSite(mapping);
-        }
     }
 
 }
