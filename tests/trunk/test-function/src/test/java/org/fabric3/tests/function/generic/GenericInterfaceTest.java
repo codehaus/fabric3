f5848233dcfa0c73438b6590ade4474670a64326
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-02-26 18:45:25 +0000

    add disabled test case for FABRICTHREE-228 and FABRICTHREE-229

---
+++ tests/trunk/test-function/src/test/java/org/fabric3/tests/function/generic/GenericInterfaceTest.java
@@ -0,0 +1,38 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.tests.function.generic;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+import org.osoa.sca.annotations.Reference;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class GenericInterfaceTest extends TestCase {
+
+    private final List<GenericInterface<String>> service;
+
+    public GenericInterfaceTest(@Reference(name="service") List<GenericInterface<String>> service) {
+        this.service = service;
+    }
+
+    public void testGenericWiring() {
+        assertEquals("Hello", service.get(0).echo("Hello"));
+    }
+}
