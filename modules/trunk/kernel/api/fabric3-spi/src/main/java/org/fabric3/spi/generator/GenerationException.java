23f68573a02a3349f547168ed8da320035d2a812
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-03-02 19:04:25 +0000

    add serialVersionUID

--- modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/generator/GenerationException.java
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/generator/GenerationException.java
@@ -26,6 +26,7 @@ import org.fabric3.host.Fabric3Exception;
  * @version $Rev$ $Date$
  */
 public abstract class GenerationException extends Fabric3Exception {
+    private static final long serialVersionUID = -1307164947930260525L;
 
     public GenerationException() {
     }
