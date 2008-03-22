49beb80709f1aaa55f1183c293d6f1ccc20d0647
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-03-22 19:39:27 +0000

    [maven-release-plugin]  copy for tag fabric3-introspection-spi-0.5-ALPHA-1

---
+++ modules/tags/fabric3-introspection-spi-0.5-ALPHA-1/src/main/java/org/fabric3/introspection/TypeMapping.java
@@ -0,0 +1,91 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.introspection;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A mapping from formal types to actual types.
+ *
+ * @version $Rev$ $Date$
+ */
+public class TypeMapping {
+    private final Map<? super Type, Type> mappings = new HashMap<Type, Type>();
+
+    /**
+     * Add a mapping from a TypeVariable to an actual type
+     *
+     * @param typeVariable the formal type variable
+     * @param type         the actual type it maps to
+     */
+    public void addMapping(TypeVariable<?> typeVariable, Type type) {
+        mappings.put(typeVariable, type);
+    }
+
+    /**
+     * Return the actual type of the supplied formal type.
+     *
+     * @param type the formal type parameter
+     * @return the actual type; may be a TypeVariable if the type is not bound
+     */
+    public Type getActualType(Type type) {
+        while (true) {
+            Type actual = mappings.get(type);
+            if (actual == null) {
+                return type;
+            } else {
+                type = actual;
+            }
+        }
+    }
+
+    /**
+     * Return the raw type of the supplied formal type.
+     *
+     * @param type the formal type parameter
+     * @return the actual class for that parameter
+     */
+    public Class<?> getRawType(Type type) {
+        Type actualType = getActualType(type);
+        if (actualType instanceof Class<?>) {
+            return (Class<?>) actualType;
+        } else if (actualType instanceof TypeVariable) {
+            TypeVariable typeVariable = (TypeVariable) actualType;
+            return getRawType(typeVariable.getBounds()[0]);
+        } else if (actualType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) actualType;
+            return (Class<?>) parameterizedType.getRawType();
+        } else if (actualType instanceof GenericArrayType) {
+            GenericArrayType arrayType = (GenericArrayType) actualType;
+            Class<?> componentType = getRawType(arrayType.getGenericComponentType());
+            return Array.newInstance(componentType, 0).getClass();
+        } else if (actualType instanceof WildcardType) {
+            WildcardType wildcardType = (WildcardType) actualType;
+            Type[] bounds = wildcardType.getUpperBounds();
+            return getRawType(bounds[0]);
+        } else {
+            throw new AssertionError();
+        }
+    }
+}
---
+++ modules/tags/fabric3-introspection-spi-0.5-ALPHA-1/src/main/java/org/fabric3/introspection/xml/UnrecognizedElementException.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.introspection.xml;
+
+import javax.xml.namespace.QName;
+
+/**
+ * Exception that indicates an element was encountered that could not be handled.
+ *
+ * @version $Rev$ $Date$
+ */
+public class UnrecognizedElementException extends LoaderException {
+    private static final long serialVersionUID = 2549543622209829032L;
+    private final QName element;
+
+    /**
+     * Constructor that indicates which resource could not be found. The supplied parameter is also returned as the
+     * message.
+     *
+     * @param element the element that could not be handled
+     */
+    public UnrecognizedElementException(QName element) {
+        super("Unrecognized element", element.toString());
+        this.element = element;
+    }
+
+    public QName getElement() {
+        return element;
+    }
+
+    public String getMessage() {
+        StringBuffer b = new StringBuffer("The element ").append(getElement());
+        return b.append(
+                " was not reconginized. If this is not a typo, check to ensure extensions are configured properly.").toString();
+
+
+    }
+}
