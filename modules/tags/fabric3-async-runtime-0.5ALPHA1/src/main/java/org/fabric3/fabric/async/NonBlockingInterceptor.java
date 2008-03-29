681b4dbd27a479a6f894ff25c8ca2216cde32ffe
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-03-29 15:40:24 +0000

    [maven-release-plugin]  copy for tag fabric3-async-runtime-0.5ALPHA1

---
+++ modules/tags/fabric3-async-runtime-0.5ALPHA1/src/main/java/org/fabric3/fabric/async/NonBlockingInterceptor.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.async;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.fabric3.spi.invocation.CallFrame;
+import org.fabric3.spi.invocation.WorkContext;
+import org.fabric3.spi.invocation.Message;
+import org.fabric3.spi.services.work.WorkScheduler;
+import org.fabric3.spi.wire.Interceptor;
+
+/**
+ * Adds non-blocking behavior to an invocation chain
+ *
+ * @version $$Rev$$ $$Date$$
+ */
+public class NonBlockingInterceptor implements Interceptor {
+
+    protected static final Message RESPONSE = new ImmutableMessage();
+
+    private final WorkScheduler workScheduler;
+    private Interceptor next;
+
+    public NonBlockingInterceptor(WorkScheduler workScheduler) {
+        this.workScheduler = workScheduler;
+    }
+
+    public Message invoke(final Message msg) {
+        WorkContext workContext = msg.getWorkContext();
+        List<CallFrame> newStack = null;
+        List<CallFrame> stack = workContext.getCallFrameStack();
+        if (stack != null && !stack.isEmpty()) {
+            // clone the callstack to avoid multiple threads seeing changes
+            newStack = new ArrayList<CallFrame>(stack);
+        }
+        msg.setWorkContext(null);
+        AsyncRequest request = new AsyncRequest(next, msg, newStack);
+        workScheduler.scheduleWork(request);
+        return RESPONSE;
+    }
+
+    public Interceptor getNext() {
+        return next;
+    }
+
+    public void setNext(Interceptor next) {
+        this.next = next;
+    }
+
+    public boolean isOptimizable() {
+        return false;
+    }
+
+    /**
+     * A dummy message passed back on an invocation
+     */
+    private static class ImmutableMessage implements Message {
+
+        public Object getBody() {
+            return null;
+        }
+
+        public void setBody(Object body) {
+            if (body != null) {
+                throw new UnsupportedOperationException();
+            }
+        }
+
+        public WorkContext getWorkContext() {
+            throw new UnsupportedOperationException();
+        }
+
+        public void setWorkContext(WorkContext workContext) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean isFault() {
+            return false;
+        }
+
+        public void setBodyWithFault(Object fault) {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+}
