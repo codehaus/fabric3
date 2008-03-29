f112b8feb9e620157c357321e84f16c7e666aa16
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-03-29 15:04:05 +0000

    [maven-release-plugin]  copy for tag fabric3-junit-0.5ALPHA1

---
+++ modules/tags/fabric3-junit-0.5ALPHA1/src/main/java/org/fabric3/junit/introspection/JUnitImplementationLoader.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.junit.introspection;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.osoa.sca.annotations.Destroy;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.IntrospectionException;
+import org.fabric3.introspection.xml.LoaderException;
+import org.fabric3.introspection.xml.LoaderRegistry;
+import org.fabric3.introspection.xml.LoaderUtil;
+import org.fabric3.introspection.xml.TypeLoader;
+import org.fabric3.junit.scdl.JUnitImplementation;
+import org.fabric3.pojo.processor.ProcessingException;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class JUnitImplementationLoader implements TypeLoader<JUnitImplementation> {
+
+    private final LoaderRegistry registry;
+    private final JUnitImplementationProcessor implementationProcessor;
+
+    public JUnitImplementationLoader(@Reference LoaderRegistry registry,
+                                     @Reference JUnitImplementationProcessor implementationProcessor) {
+        this.registry = registry;
+        this.implementationProcessor = implementationProcessor;
+    }
+
+    @Init
+    public void init() {
+        registry.registerLoader(JUnitImplementation.IMPLEMENTATION_JUNIT, this);
+    }
+
+    @Destroy
+    public void destroy() {
+        registry.unregisterLoader(JUnitImplementation.IMPLEMENTATION_JUNIT);
+    }
+
+    public JUnitImplementation load(XMLStreamReader reader, IntrospectionContext introspectionContext)
+            throws XMLStreamException, LoaderException {
+        String className = reader.getAttributeValue(null, "class");
+        LoaderUtil.skipToEndElement(reader);
+
+        JUnitImplementation impl = new JUnitImplementation(className);
+        try {
+            implementationProcessor.introspect(impl, introspectionContext);
+        } catch (IntrospectionException e) {
+            throw new ProcessingException(e);
+        }
+        return impl;
+    }
+
+}
---
+++ modules/tags/fabric3-junit-0.5ALPHA1/src/main/java/org/fabric3/junit/introspection/JUnitServiceHeuristic.java
@@ -0,0 +1,81 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.junit.introspection;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.IntrospectionException;
+import org.fabric3.introspection.java.HeuristicProcessor;
+import org.fabric3.junit.scdl.JUnitImplementation;
+import org.fabric3.junit.scdl.JUnitServiceContract;
+import org.fabric3.scdl.DataType;
+import org.fabric3.scdl.Operation;
+import org.fabric3.scdl.ServiceDefinition;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class JUnitServiceHeuristic implements HeuristicProcessor<JUnitImplementation> {
+    private static final String TEST_SERVICE_NAME = "testService";
+
+    public void applyHeuristics(JUnitImplementation implementation, Class<?> implClass, IntrospectionContext context) throws IntrospectionException {
+
+        JUnitServiceContract testContract = generateTestContract(implClass);
+        ServiceDefinition testService = new ServiceDefinition(TEST_SERVICE_NAME, testContract);
+        implementation.getComponentType().add(testService);
+    }
+
+    private static final DataType<List<DataType<Type>>> INPUT_TYPE;
+    private static final DataType<Type> OUTPUT_TYPE;
+    private static final List<DataType<Type>> FAULT_TYPE;
+
+    static {
+        List<DataType<Type>> paramDataTypes = Collections.emptyList();
+        INPUT_TYPE = new DataType<List<DataType<Type>>>(Object[].class, paramDataTypes);
+        OUTPUT_TYPE = new DataType<Type>(void.class, void.class);
+        FAULT_TYPE = Collections.emptyList();
+    }
+
+    JUnitServiceContract generateTestContract(Class<?> implClass) {
+        List<Operation<Type>> operations = new ArrayList<Operation<Type>>();
+        for (Method method : implClass.getMethods()) {
+            // see if this is a test method
+            if (Modifier.isStatic(method.getModifiers())) {
+                continue;
+            }
+            if (method.getReturnType() != void.class) {
+                continue;
+            }
+            if (method.getParameterTypes().length != 0) {
+                continue;
+            }
+            String name = method.getName();
+            if (name.length() < 5 || !name.startsWith("test")) {
+                continue;
+            }
+            Operation<Type> operation = new Operation<Type>(name, INPUT_TYPE, OUTPUT_TYPE, FAULT_TYPE);
+            operations.add(operation);
+        }
+        return new JUnitServiceContract(operations);
+    }
+}
