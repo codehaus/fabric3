2a116c9c8f1b304f1794843fe053b5428cbcade1
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-03-31 00:14:05 +0000

    return ServletContext to decouple webapp component builder from WebAppHostInfo

--- modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/host/ServletHost.java
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/host/ServletHost.java
@@ -19,6 +19,7 @@
 package org.fabric3.spi.host;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
 
 /**
  * Interface implemented by host environments that allow Servlets to be registered.
@@ -28,9 +29,17 @@ import javax.servlet.Servlet;
  * @version $Rev$ $Date$
  */
 public interface ServletHost {
+
     /**
-     * Register a mapping for an instance of a Servlet. This requests that the servlet container direct all requests to
-     * the designated mapping to the supplied Servlet instance.
+     * Returns the default servlet context associated with the host.
+     *
+     * @return the default servlet context associated with the host
+     */
+    ServletContext getServletContext();
+
+    /**
+     * Register a mapping for an instance of a Servlet. This requests that the servlet container direct all requests to the designated mapping to the
+     * supplied Servlet instance.
      *
      * @param mapping the uri-mapping for the Servlet
      * @param servlet the Servlet that should be invoked
@@ -38,21 +47,20 @@ public interface ServletHost {
     void registerMapping(String mapping, Servlet servlet);
 
     /**
-     * Unregister a servlet mapping. This directs the servlet contain not to direct any more requests to a previously
-     * registered Servlet.
+     * Unregister a servlet mapping. This directs the servlet contain not to direct any more requests to a previously registered Servlet.
      *
      * @param mapping the uri-mapping for the Servlet
      * @return the servlet that was registered to the mapping, null if nothing was registered to the mapping
      */
     Servlet unregisterMapping(String mapping);
-    
+
     /**
      * Check to see if a mapping exists.
-     * 
-     * @param mapping  the uri-mapping for the Servlet
+     *
+     * @param mapping the uri-mapping for the Servlet
      * @return true if mapping is registered, false otherwise
      */
     boolean isMappingRegistered(String mapping);
-        
+
 
 }
