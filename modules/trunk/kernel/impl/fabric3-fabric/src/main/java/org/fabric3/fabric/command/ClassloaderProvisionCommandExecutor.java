0dce2c71e4292e70401b83de21d2f09da1ff95dd
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-17 01:27:09 +0000

    java doc update

--- modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/command/ClassloaderProvisionCommandExecutor.java
+++ modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/command/ClassloaderProvisionCommandExecutor.java
@@ -29,7 +29,7 @@ import org.fabric3.spi.command.ExecutionException;
 import org.fabric3.spi.model.physical.PhysicalClassLoaderDefinition;
 
 /**
- * Eagerly initializes a component on a service node.
+ * Creates a classloader on a runtime corresponding to a PhysicalClassLoaderDefinition.
  *
  * @version $Rev: 2878 $ $Date: 2008-02-23 18:42:09 +0000 (Sat, 23 Feb 2008) $
  */
