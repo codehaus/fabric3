780c43105fdf1d9478fd14e220220f36c1ddb2d4
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-25 16:48:38 +0000

    Clean up imports

--- modules/trunk/kernel/impl/fabric3-loader/src/main/java/org/fabric3/loader/impl/DefaultLoaderHelper.java
+++ modules/trunk/kernel/impl/fabric3-loader/src/main/java/org/fabric3/loader/impl/DefaultLoaderHelper.java
@@ -19,28 +19,27 @@
 package org.fabric3.loader.impl;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.StringTokenizer;
-import java.util.List;
 import java.util.ArrayList;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import org.fabric3.scdl.PolicyAware;
 import org.fabric3.introspection.xml.LoaderException;
 import org.fabric3.introspection.xml.LoaderHelper;
-import org.fabric3.transform.xml.Stream2Element2;
-import org.fabric3.transform.TransformationException;
 import org.fabric3.loader.common.InvalidPrefixException;
+import org.fabric3.scdl.PolicyAware;
+import org.fabric3.transform.TransformationException;
+import org.fabric3.transform.xml.Stream2Element2;
 
 /**
  * Default implementation of the loader helper.
