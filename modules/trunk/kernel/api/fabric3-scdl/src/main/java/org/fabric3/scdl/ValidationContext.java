f7efcd993697f4e8ee05a5119201048deae6523e
jboynes <jboynes@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-25 17:05:48 +0000

    Add support for validation warnings as well as errors (the idea being that warnings are indicators that there might be problems later during activation but that the model object on it own is ok).

--- modules/trunk/kernel/api/fabric3-scdl/src/main/java/org/fabric3/scdl/ValidationContext.java
+++ modules/trunk/kernel/api/fabric3-scdl/src/main/java/org/fabric3/scdl/ValidationContext.java
@@ -20,20 +20,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Context for gathering the results of model validation.
+ *
  * @version $Rev$ $Date$
  */
 public class ValidationContext {
     private final List<ValidationFailure> errors = new ArrayList<ValidationFailure>();
+    private final List<ValidationFailure> warnings = new ArrayList<ValidationFailure>();
 
+    /**
+     * Returns true if the validation has detected any fatal errors.
+     *
+     * @return true if the validation has detected any fatal errors
+     */
     public boolean hasErrors() {
         return !errors.isEmpty();
     }
 
+    /**
+     * Returns the list of fatal errors detected during validation.
+     *
+     * @return the list of fatal errors detected during validation
+     */
     public List<ValidationFailure> getErrors() {
         return errors;
     }
 
+    /**
+     * Add a fatal error to the validation results.
+     *
+     * @param e the fatal error that has been found
+     */
     public void addError(ValidationFailure e) {
         errors.add(e);
+    }
+
+    /**
+     * Returns true if the validation has detected any non-fatal warnings.
+     *
+     * @return true if the validation has detected any non-fatal warnings
+     */
+    public boolean hasWarnings() {
+        return !warnings.isEmpty();
+    }
+
+    /**
+     * Returns the list of non-fatal warnings detected during validation.
+     *
+     * @return the list of non-fatal warnings detected during validation
+     */
+    public List<ValidationFailure> getWarnings() {
+        return warnings;
+    }
+
+    /**
+     * Add a non-fatal warning to the validation results.
+     *
+     * @param e the non-fatal warning that has been found
+     */
+    public void addWarning(ValidationFailure e) {
+        warnings.add(e);
     }
 }
