822dac6f0db123d0d64cbd86912548c6ca4bacfb
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 18:14:28 +0000

    [maven-release-plugin]  copy for tag fabric3-java-0.5ALPHA2

---
+++ modules/tags/fabric3-java-0.5ALPHA2/src/main/java/org/fabric3/java/introspection/JavaHeuristic.java
@@ -0,0 +1,209 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.java.introspection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.Map;
+
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.IntrospectionException;
+import org.fabric3.introspection.contract.ContractProcessor;
+import org.fabric3.introspection.java.HeuristicProcessor;
+import org.fabric3.introspection.IntrospectionHelper;
+import org.fabric3.introspection.TypeMapping;
+import org.fabric3.introspection.contract.InvalidServiceContractException;
+import org.fabric3.introspection.java.UnsupportedTypeException;
+import org.fabric3.java.scdl.JavaImplementation;
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.scdl.ConstructorInjectionSite;
+import org.fabric3.scdl.FieldInjectionSite;
+import org.fabric3.scdl.InjectionSite;
+import org.fabric3.scdl.MethodInjectionSite;
+import org.fabric3.scdl.Multiplicity;
+import org.fabric3.scdl.Property;
+import org.fabric3.scdl.ReferenceDefinition;
+import org.fabric3.scdl.ServiceContract;
+import org.fabric3.scdl.Signature;
+import org.fabric3.scdl.InjectableAttribute;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class JavaHeuristic implements HeuristicProcessor<JavaImplementation> {
+
+    private final IntrospectionHelper helper;
+    private final ContractProcessor contractProcessor;
+
+    private final HeuristicProcessor<JavaImplementation> serviceHeuristic;
+    private final HeuristicProcessor<JavaImplementation> dataTypeHeuristic;
+
+    public JavaHeuristic(@Reference IntrospectionHelper helper,
+                         @Reference ContractProcessor contractProcessor,
+                         @Reference(name = "service")HeuristicProcessor<JavaImplementation> serviceHeuristic,
+                         @Reference(name = "dataType")HeuristicProcessor<JavaImplementation> dataTypeHeuristic) {
+        this.helper = helper;
+        this.contractProcessor = contractProcessor;
+        this.serviceHeuristic = serviceHeuristic;
+        this.dataTypeHeuristic = dataTypeHeuristic;
+    }
+
+    public void applyHeuristics(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) throws IntrospectionException {
+
+        PojoComponentType componentType = implementation.getComponentType();
+
+        // apply service heuristic
+        serviceHeuristic.applyHeuristics(implementation, implClass, context);
+
+        if (componentType.getConstructor() == null) {
+            componentType.setConstructor(findConstructor(implClass));
+        }
+
+        if (componentType.getProperties().isEmpty() && componentType.getReferences().isEmpty() && componentType.getResources().isEmpty()) {
+            evaluateConstructor(implementation, implClass, context);
+            evaluateSetters(implementation, implClass, context);
+            evaluateFields(implementation, implClass, context);
+        }
+
+        // apply data type mapping heuristic
+        dataTypeHeuristic.applyHeuristics(implementation, implClass, context);
+    }
+
+    Signature findConstructor(Class<?> implClass) throws IntrospectionException {
+        Constructor<?>[] constructors = implClass.getDeclaredConstructors();
+        Constructor<?> selected = null;
+        if (constructors.length == 1) {
+            selected = constructors[0];
+        } else {
+            for (Constructor<?> constructor : constructors) {
+                if (constructor.isAnnotationPresent(org.osoa.sca.annotations.Constructor.class)) {
+                    if (selected != null) {
+                        throw new AmbiguousConstructorException(implClass.getName());
+                    }
+                    selected = constructor;
+                }
+            }
+            if (selected == null) {
+                throw new NoConstructorException(implClass.getName());
+            }
+        }
+        return new Signature(selected);
+    }
+
+    void evaluateConstructor(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) throws IntrospectionException {
+        PojoComponentType componentType = implementation.getComponentType();
+        Map<InjectionSite, InjectableAttribute> sites = componentType.getInjectionSites();
+        Constructor<?> constructor;
+        try {
+            constructor = componentType.getConstructor().getConstructor(implClass);
+        } catch (ClassNotFoundException e) {
+            throw new AssertionError(e);
+        } catch (NoSuchMethodException e) {
+            throw new AssertionError(e);
+        }
+
+        TypeMapping typeMapping = context.getTypeMapping();
+        Type[] parameterTypes = constructor.getGenericParameterTypes();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            InjectionSite site = new ConstructorInjectionSite(constructor, i);
+
+            // skip sites that have already been mapped
+            if (sites.containsKey(site)) {
+                continue;
+            }
+
+            Type parameterType = parameterTypes[i];
+            String name = helper.getSiteName(constructor, i, null);
+            processSite(componentType, typeMapping, name, parameterType, site);
+        }
+    }
+
+    void evaluateSetters(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) throws IntrospectionException {
+        PojoComponentType componentType = implementation.getComponentType();
+        Map<InjectionSite, InjectableAttribute> sites = componentType.getInjectionSites();
+        TypeMapping typeMapping = context.getTypeMapping();
+        Set<Method> setters = helper.getInjectionMethods(implClass, componentType.getServices().values());
+        for (Method setter : setters) {
+            InjectionSite site = new MethodInjectionSite(setter, 0);
+
+            // skip sites that have already been mapped
+            if (sites.containsKey(site)) {
+                continue;
+            }
+
+            String name = helper.getSiteName(setter, null);
+            Type parameterType = setter.getGenericParameterTypes()[0];
+            processSite(componentType, typeMapping, name, parameterType, site);
+        }
+    }
+
+    void evaluateFields(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) throws IntrospectionException {
+        PojoComponentType componentType = implementation.getComponentType();
+        Map<InjectionSite,InjectableAttribute> sites = componentType.getInjectionSites();
+        TypeMapping typeMapping = context.getTypeMapping();
+        Set<Field> fields = helper.getInjectionFields(implClass);
+        for (Field field : fields) {
+            InjectionSite site = new FieldInjectionSite(field);
+
+            // skip sites that have already been mapped
+            if (sites.containsKey(site)) {
+                continue;
+            }
+
+            String name = helper.getSiteName(field, null);
+            Type parameterType = field.getGenericType();
+            processSite(componentType, typeMapping, name, parameterType, site);
+        }
+    }
+
+
+    void processSite(PojoComponentType componentType, TypeMapping typeMapping, String name, Type parameterType, InjectionSite site)
+            throws IntrospectionException {
+        switch (helper.inferType(parameterType, typeMapping)) {
+        case PROPERTY:
+            addProperty(componentType, typeMapping, name, parameterType, site);
+            break;
+        case REFERENCE:
+            addReference(componentType, typeMapping, name, parameterType, site);
+            break;
+        case CALLBACK:
+            // ignore
+            break;
+        default:
+            throw new UnsupportedTypeException(site.toString());
+        }
+    }
+
+    void addProperty(PojoComponentType componentType, TypeMapping typeMapping, String name, Type parameterType, InjectionSite site) {
+        Property property = new Property(name, null);
+        property.setMany(helper.isManyValued(typeMapping, parameterType));
+        componentType.add(property, site);
+    }
+
+    void addReference(PojoComponentType componentType, TypeMapping typeMapping, String name, Type parameterType, InjectionSite site) throws
+            InvalidServiceContractException {
+        ServiceContract<Type> contract = contractProcessor.introspect(typeMapping, parameterType);
+        Multiplicity multiplicity = helper.isManyValued(typeMapping, parameterType) ? Multiplicity.ONE_N : Multiplicity.ONE_ONE;
+        ReferenceDefinition reference = new ReferenceDefinition(name, contract, multiplicity);
+        componentType.add(reference, site);
+    }
+}
---
+++ modules/tags/fabric3-java-0.5ALPHA2/src/main/java/org/fabric3/java/runtime/JavaSourceWireAttacher.java
@@ -0,0 +1,113 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.java.runtime;
+
+import java.net.URI;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.java.provision.JavaWireSourceDefinition;
+import org.fabric3.java.provision.JavaWireTargetDefinition;
+import org.fabric3.pojo.wire.PojoSourceWireAttacher;
+import org.fabric3.scdl.InjectableAttribute;
+import org.fabric3.scdl.InjectableAttributeType;
+import org.fabric3.spi.ObjectFactory;
+import org.fabric3.spi.builder.WiringException;
+import org.fabric3.spi.builder.component.SourceWireAttacher;
+import org.fabric3.spi.builder.component.WireAttachException;
+import org.fabric3.spi.component.ScopeContainer;
+import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
+import org.fabric3.spi.runtime.component.ComponentManager;
+import org.fabric3.spi.services.classloading.ClassLoaderRegistry;
+import org.fabric3.spi.services.proxy.ProxyService;
+import org.fabric3.transform.PullTransformer;
+import org.fabric3.transform.TransformerRegistry;
+import org.fabric3.spi.util.UriHelper;
+import org.fabric3.spi.wire.Wire;
+
+/**
+ * Attaches wires to and from components implemented using the Java programming model.
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class JavaSourceWireAttacher extends PojoSourceWireAttacher implements SourceWireAttacher<JavaWireSourceDefinition> {
+
+    private final ComponentManager manager;
+    private final ProxyService proxyService;
+    private final ClassLoaderRegistry classLoaderRegistry;
+
+    public JavaSourceWireAttacher(@Reference ComponentManager manager,
+                                  @Reference ProxyService proxyService,
+                                  @Reference ClassLoaderRegistry classLoaderRegistry,
+                                  @Reference(name = "transformerRegistry")TransformerRegistry<PullTransformer<?, ?>> transformerRegistry) {
+        super(transformerRegistry, classLoaderRegistry);
+        this.manager = manager;
+        this.proxyService = proxyService;
+        this.classLoaderRegistry = classLoaderRegistry;
+    }
+
+    public void attachToSource(JavaWireSourceDefinition sourceDefinition, PhysicalWireTargetDefinition targetDefinition, Wire wire)
+            throws WireAttachException {
+
+        URI sourceUri = sourceDefinition.getUri();
+        URI sourceName = UriHelper.getDefragmentedName(sourceDefinition.getUri());
+        JavaComponent<?> source = (JavaComponent) manager.getComponent(sourceName);
+        InjectableAttribute injectableAttribute = sourceDefinition.getValueSource();
+
+        Class<?> type;
+        try {
+            type = classLoaderRegistry.loadClass(sourceDefinition.getClassLoaderId(), sourceDefinition.getInterfaceName());
+        } catch (ClassNotFoundException e) {
+            String name = sourceDefinition.getInterfaceName();
+            throw new WireAttachException("Unable to load interface class [" + name + "]", sourceUri, null, e);
+        }
+        if (InjectableAttributeType.CALLBACK.equals(injectableAttribute.getValueType())) {
+            URI targetUri = targetDefinition.getUri();
+            ScopeContainer<?> container = source.getScopeContainer();
+            ObjectFactory<?> factory = proxyService.createCallbackObjectFactory(type, container, targetUri, wire);
+            // JFM TODO inject updates to object factory as this does not support a proxy fronting multiple callback wires
+            source.setObjectFactory(injectableAttribute, factory);
+        } else {
+            String callbackUri = null;
+            URI uri = targetDefinition.getCallbackUri();
+            if (uri != null) {
+                callbackUri = uri.toString();
+            }
+
+            ObjectFactory<?> factory = proxyService.createObjectFactory(type, sourceDefinition.getInteractionType(), wire, callbackUri);
+            Object key = getKey(sourceDefinition, source, injectableAttribute);
+            source.attachReferenceToTarget(injectableAttribute, factory, key);
+        }
+    }
+
+    public void attachObjectFactory(JavaWireSourceDefinition source, ObjectFactory<?> objectFactory) throws WiringException {
+        URI sourceId = UriHelper.getDefragmentedName(source.getUri());
+        JavaComponent<?> sourceComponent = (JavaComponent<?>) manager.getComponent(sourceId);
+        InjectableAttribute injectableAttribute = source.getValueSource();
+
+        Object key = getKey(source, sourceComponent, injectableAttribute);
+        sourceComponent.attachReferenceToTarget(injectableAttribute, objectFactory, key);
+    }
+
+    public ObjectFactory<?> createObjectFactory(JavaWireTargetDefinition target) throws WiringException {
+        URI targetId = UriHelper.getDefragmentedName(target.getUri());
+        JavaComponent<?> targetComponent = (JavaComponent<?>) manager.getComponent(targetId);
+        return targetComponent.createObjectFactory();
+    }
+}
