145da6357f0015c946c124388d0379a55fb756aa
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 21:44:53 +0000

    [maven-release-plugin]  copy for tag fabric3-interface-wsdl-0.5ALPHA2

---
+++ modules/tags/fabric3-interface-wsdl-0.5ALPHA2/src/main/java/org/fabric3/idl/wsdl/processor/WsdlProcessorRegistry.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.fabric3.idl.wsdl.processor;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.apache.ws.commons.schema.XmlSchemaType;
+import org.fabric3.idl.wsdl.version.WsdlVersionChecker;
+import org.fabric3.idl.wsdl.version.WsdlVersionChecker.WsdlVersion;
+import org.fabric3.scdl.Operation;
+
+/**
+ * Default WSDL processor implementation.
+ *
+ * @version $Revsion$ $Date$
+ */
+public class WsdlProcessorRegistry implements WsdlProcessor {
+
+    /**
+     * WSDL processors.
+     */
+    private Map<WsdlVersion, WsdlProcessor> wsdlProcessors = new HashMap<WsdlVersion, WsdlProcessor>();
+    
+    /**
+     * WSDL version checker.
+     */
+    private WsdlVersionChecker versionChecker;
+
+    /**
+     * @param versionChecker Injected WSDL version checker.
+     */
+    public WsdlProcessorRegistry(WsdlVersionChecker versionChecker) {
+        this.versionChecker = versionChecker;
+    }
+
+    public List<Operation<XmlSchemaType>> getOperations(QName portTypeOrInterfaceName, URL wsdlUrl) {
+
+        WsdlVersion wsdlVersion = versionChecker.getVersion(wsdlUrl);
+        if(!wsdlProcessors.containsKey(wsdlVersion)) {
+            throw new WsdlProcessorException("No processor registered for version " + wsdlVersion);
+        }
+        return wsdlProcessors.get(wsdlVersion).getOperations(portTypeOrInterfaceName, wsdlUrl);
+
+    }
+
+    /**
+     * Registers a processor.
+     *
+     * @param wsdlVersion WSDL version.
+     * @param processor WSDL processor.
+     */
+    public void registerProcessor(WsdlVersion wsdlVersion, WsdlProcessor processor) {
+        wsdlProcessors.put(wsdlVersion, processor);
+    }
+
+}
