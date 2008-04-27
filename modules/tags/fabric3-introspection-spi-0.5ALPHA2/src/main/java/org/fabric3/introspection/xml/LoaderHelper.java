f466282bdfdc835dfc178e25b220cfd14b097991
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 08:08:09 +0000

    [maven-release-plugin]  copy for tag fabric3-introspection-spi-0.5ALPHA2

---
+++ modules/tags/fabric3-introspection-spi-0.5ALPHA2/src/main/java/org/fabric3/introspection/xml/LoaderHelper.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.introspection.xml;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Set;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.w3c.dom.Document;
+
+import org.fabric3.scdl.PolicyAware;
+
+/**
+ * Interface for the helper class for loading intents and policy sets into elements aginst which intents and policies can be declared.
+ *
+ * @version $Revision$ $Date$
+ */
+public interface LoaderHelper {
+    /**
+     * Load an XML value from a Stax stream.
+     * <p/>
+     * The reader must be positioned at an element whose body contains an XML value. This will typically be an SCA &lt;property&gt; element (either in
+     * a &lt;composite&gt; or in a &lt;component&gt;). The resulting document comprises a &lt;value&gt; element whose body content will be that of the
+     * original &lt;property&gt; element.
+     *
+     * @param reader a stream containing a property value
+     * @return a standalone document containing the value
+     * @throws javax.xml.stream.XMLStreamException
+     *          if there was a problem reading the stream
+     */
+    Document loadValue(XMLStreamReader reader) throws XMLStreamException;
+
+    /**
+     * Loads policy sets and intents defined against bindings, implementations, services, references and components.
+     *
+     * @param policyAware Element against which policy sets and intents are declared.
+     * @param reader      XML stream reader from where the attributes are read.
+     * @throws LoaderException if there was a problem with the policy set or intents
+     */
+    void loadPolicySetsAndIntents(PolicyAware policyAware, XMLStreamReader reader) throws LoaderException;
+
+    /**
+     * Convert a component URI in the form ${componentName}/${serviceName} to a URI of the form ${componentName}#${serviceName}
+     *
+     * @param target the target URI to convert
+     * @return a URI where the fragment represents the service name
+     */
+    URI getURI(String target);
+
+    /**
+     * Parses a list of qualified names.
+     *
+     * @param reader    XML stream reader.
+     * @param attribute Attribute that contains the list of qualified names.
+     * @return Set containing the qualified names.
+     * @throws LoaderException If the qualified name cannot be resolved.
+     */
+    Set<QName> parseListOfQNames(XMLStreamReader reader, String attribute) throws LoaderException;
+
+    /**
+     * Constructs a QName from the given name. If a namespace prefix is not specified in the name, the namespace context is used
+     *
+     * @param name   the name to parse
+     * @param reader the XML stream reader
+     * @return the parsed QName
+     * @throws LoaderException if a specified namespace prefix is invalid
+     */
+    QName createQName(String name, XMLStreamReader reader) throws LoaderException;
+
+    /**
+     * Parses a list of URIs contained in a attribute.
+     *
+     * @param reader    the XML stream reader
+     * @param attribute the attribute to parse
+     * @return the list of URIs contained in that attribute, or null if the attribute is not present
+     * @throws LoaderException if list contains a value that is not a valid URI
+     */
+    List<URI> parseListOfUris(XMLStreamReader reader, String attribute) throws LoaderException;
+}
