e83dcef361b4aeccff3679add63b9606cff16d3c
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 18:52:52 +0000

    [maven-release-plugin]  copy for tag fabric3-async-loader-0.5ALPHA2

---
+++ modules/tags/fabric3-async-loader-0.5ALPHA2/src/main/java/org/fabric3/fabric/async/NonBlockingGenerator.java
@@ -0,0 +1,59 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.async;
+
+import javax.xml.namespace.QName;
+
+import org.fabric3.scdl.Operation;
+import org.fabric3.spi.Constants;
+import org.fabric3.spi.generator.GenerationException;
+import org.fabric3.spi.generator.GeneratorRegistry;
+import org.fabric3.spi.generator.InterceptorDefinitionGenerator;
+import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.model.physical.PhysicalInterceptorDefinition;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+import org.w3c.dom.Element;
+
+/**
+ * Creates {@link NonBlockingInterceptorDefinition}s for one-way operations.
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class NonBlockingGenerator implements InterceptorDefinitionGenerator {
+    
+    private static final QName QNAME = new QName(Constants.FABRIC3_NS, "oneWayPolicy");
+    
+    private GeneratorRegistry registry;
+
+    public NonBlockingGenerator(@Reference GeneratorRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Init
+    public void init() {
+        registry.register(QNAME, this);
+    }
+
+    public PhysicalInterceptorDefinition generate(Element policyDefinition,
+                                                  Operation<?> operation,
+                                                  LogicalBinding<?> logicalBinding) throws GenerationException {
+        return new NonBlockingInterceptorDefinition();
+    }
+}
