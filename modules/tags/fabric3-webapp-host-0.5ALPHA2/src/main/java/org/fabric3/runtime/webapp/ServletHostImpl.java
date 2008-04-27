f404ad5cb3853e9bd41bc80ab7e4b3affd7a5965
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 18:41:23 +0000

    [maven-release-plugin]  copy for tag fabric3-webapp-host-0.5ALPHA2

---
+++ modules/tags/fabric3-webapp-host-0.5ALPHA2/src/main/java/org/fabric3/runtime/webapp/ServletHostImpl.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.runtime.webapp;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
+import org.osoa.sca.annotations.Service;
+
+import org.fabric3.spi.component.ScopeRegistry;
+import org.fabric3.spi.host.ServletHost;
+
+/**
+ * A <code>ServletHost</code> implementation that forwards requests to registered servlets
+ *
+ * @version $Rev$ $Date$
+ */
+@Service(interfaces = {ServletHost.class, ServletRequestInjector.class})
+@EagerInit
+public class ServletHostImpl implements ServletHost, ServletRequestInjector {
+    protected Map<String, Servlet> servlets;
+    protected ScopeRegistry registry;
+    private WebappHostInfo info;
+
+    public ServletHostImpl(@Reference WebappHostInfo info) {
+        this.info = info;
+        servlets = new HashMap<String, Servlet>();
+    }
+
+    public ServletContext getServletContext() {
+        return info.getServletContext();
+    }
+
+    public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+        assert req instanceof HttpServletRequest;
+        String path = ((HttpServletRequest) req).getPathInfo();
+        Servlet servlet = servlets.get(path);
+        if (servlet == null) {
+            int i;
+            servlet = servlets.get(path + "/*");
+            if (servlet == null) {
+                while ((i = path.lastIndexOf("/")) >= 0) {
+                    servlet = servlets.get(path.substring(0, i) + "/*");
+                    if (servlet != null) {
+                        break;
+                    }
+                    path = path.substring(0, i);
+                }
+            }
+            if (servlet == null) {
+                throw new IllegalStateException("No servlet registered for path: " + path);
+            }
+        }
+        servlet.service(req, resp);
+    }
+
+    public void registerMapping(String path, Servlet servlet) {
+        if (servlets.containsKey(path)) {
+            throw new IllegalStateException("Servlet already registered at path: " + path);
+        }
+        servlets.put(path, servlet);
+    }
+
+    public boolean isMappingRegistered(String mapping) {
+        return servlets.containsKey(mapping);
+
+    }
+
+    public Servlet unregisterMapping(String path) {
+        return servlets.remove(path);
+    }
+
+}
---
+++ modules/tags/fabric3-webapp-host-0.5ALPHA2/src/main/java/org/fabric3/runtime/webapp/activator/EmbeddedWebApplicationActivator.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.runtime.webapp.activator;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.osoa.sca.ComponentContext;
+import org.osoa.sca.annotations.Reference;
+import org.osoa.sca.annotations.Service;
+
+import org.fabric3.container.web.spi.WebApplicationActivationException;
+import org.fabric3.container.web.spi.WebApplicationActivator;
+import org.fabric3.pojo.reflection.Injector;
+import org.fabric3.spi.ObjectCreationException;
+import org.fabric3.spi.host.ServletHost;
+
+/**
+ * A WebApplicationActivator used in a runtime embedded in a WAR.
+ *
+ * @version $Revision$ $Date$
+ */
+@Service(interfaces = {WebApplicationActivator.class, HttpSessionListener.class})
+public class EmbeddedWebApplicationActivator implements WebApplicationActivator, HttpSessionListener {
+    private ServletHost host;
+    private List<Injector<HttpSession>> sessionInjectors;
+
+    public EmbeddedWebApplicationActivator(@Reference ServletHost host) {
+        this.host = host;
+    }
+
+    public ClassLoader getWebComponentClassLoader(URI componentId) {
+        //  As the runtime is embedded in a web app, the TCCL is the webapp classloader
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public ServletContext activate(String contextPath,
+                                   URL url,
+                                   URI parentClassLoaderId,
+                                   Map<String, List<Injector<?>>> injectors,
+                                   ComponentContext context) throws WebApplicationActivationException {
+        try {
+            // the web app has already been activated since it is embedded in a war. Just inject references and properties
+            ServletContext servletContext = host.getServletContext();
+            injectServletContext(servletContext, injectors);
+            sessionInjectors = List.class.cast(injectors.get(SESSION_CONTEXT_SITE));
+            return servletContext;
+        } catch (ObjectCreationException e) {
+            throw new WebApplicationActivationException(e);
+        }
+    }
+
+    public void deactivate(URL url) throws WebApplicationActivationException {
+        // do nothing
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private void injectServletContext(ServletContext servletContext, Map<String, List<Injector<?>>> injectors) throws ObjectCreationException {
+        List<Injector<?>> list = injectors.get(SERVLET_CONTEXT_SITE);
+        if (list == null) {
+            // nothing to inject
+            return;
+        }
+        for (Injector injector : list) {
+            injector.inject(servletContext);
+        }
+    }
+
+    public void sessionCreated(HttpSessionEvent event) {
+        HttpSession session = event.getSession();
+        // inject conversational proxies into the session
+        for (Injector<HttpSession> injector : sessionInjectors) {
+            try {
+                injector.inject(session);
+            } catch (ObjectCreationException e) {
+                throw new RuntimeInjectionException(e);
+            }
+        }
+    }
+
+    public void sessionDestroyed(HttpSessionEvent event) {
+        // do nothing
+    }
+}
