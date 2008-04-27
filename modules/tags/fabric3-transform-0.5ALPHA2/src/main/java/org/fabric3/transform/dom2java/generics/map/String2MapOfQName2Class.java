b424425b304a5779366f04db1d4d9712edb0288f
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 10:38:14 +0000

    [maven-release-plugin]  copy for tag fabric3-transform-0.5ALPHA2

---
+++ modules/tags/fabric3-transform-0.5ALPHA2/src/main/java/org/fabric3/transform/dom2java/generics/map/String2MapOfQName2Class.java
@@ -0,0 +1,101 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.transform.dom2java.generics.map;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.fabric3.scdl.DataType;
+import org.fabric3.spi.model.type.JavaParameterizedType;
+import org.fabric3.spi.services.classloading.ClassLoaderRegistry;
+import org.fabric3.transform.TransformContext;
+import org.fabric3.transform.AbstractPullTransformer;
+import org.fabric3.transform.TransformationException;
+
+import org.osoa.sca.annotations.Reference;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Expects the property to be dfined in the format,
+ * <p/>
+ * <code> <key1>value1</key1> <key2>value2</key2> </code>
+ *
+ * @version $Rev: 1570 $ $Date: 2007-10-20 14:24:19 +0100 (Sat, 20 Oct 2007) $
+ */
+public class String2MapOfQName2Class extends AbstractPullTransformer<Node, Map<QName, Class<?>>> {
+    
+    private static Map<QName, Class<?>> FIELD = null;
+    private static JavaParameterizedType TARGET = null;
+    
+    private ClassLoaderRegistry classLoaderRegistry;
+    
+    static {
+        try {
+            ParameterizedType parameterizedType = (ParameterizedType) String2MapOfQName2Class.class.getDeclaredField("FIELD").getGenericType();
+            TARGET = new JavaParameterizedType(parameterizedType);
+        } catch (NoSuchFieldException ignore) {
+            throw new AssertionError();
+        }
+    }
+    
+    public String2MapOfQName2Class(@Reference ClassLoaderRegistry classLoaderRegistry) {
+        this.classLoaderRegistry = classLoaderRegistry;
+    }
+
+    /**
+     * @see org.fabric3.transform.Transformer#getTargetType()
+     */
+    public DataType<?> getTargetType() {
+        return TARGET;
+    }
+
+    /**
+     * @see org.fabric3.transform.PullTransformer#transform(java.lang.Object, org.fabric3.transform.TransformContext)
+     */
+    public Map<QName, Class<?>> transform(final Node node, final TransformContext context) throws TransformationException {
+
+        final Map<QName, Class<?>> map = new HashMap<QName, Class<?>>();
+        final NodeList nodeList = node.getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node child = nodeList.item(i);
+            if (child instanceof Element) {
+                Element element = (Element) child;
+                
+                String localPart = element.getTagName();
+                String namespaceUri = element.getNamespaceURI();
+                QName qname = new QName(namespaceUri, localPart);
+                String classText = element.getTextContent();
+
+                try {
+                    Class<?> clazz = classLoaderRegistry.loadClass(context.getTargetClassLoader(), classText);
+                    map.put(qname, clazz);
+                } catch (ClassNotFoundException e) {
+                    throw new TransformationException(e);
+                }
+            }
+        }
+        return map;
+    }
+    
+    
+}
