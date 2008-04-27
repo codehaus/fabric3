d52fdbe23a7736beb83777c1c7e4f5e7ae70861a
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 20:26:56 +0000

    [maven-release-plugin]  copy for tag fabric3-binding-ws-jaxws-0.5ALPHA2

---
+++ modules/tags/fabric3-binding-ws-jaxws-0.5ALPHA2/src/main/java/org/fabric3/binding/ws/jaxws/provision/JaxWsWireTargetDefinition.java
@@ -0,0 +1,140 @@
+package org.fabric3.binding.ws.jaxws.provision;
+
+import java.net.URI;
+
+import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
+
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+public class JaxWsWireTargetDefinition extends PhysicalWireTargetDefinition {
+
+  
+    private String wsdlElement;
+    private String referenceInterface;
+    private URI classloaderURI;
+    private String wsdlLocation;
+    private String serviceName;
+    private String portName;
+    private String namespaceURI;
+
+
+    /**
+     * @return Reference interface for the wire target.
+     */
+    public String getReferenceInterface() {
+        return referenceInterface;
+    }
+
+    /**
+     * @param referenceInterface Reference interface for the wire target.
+     */
+    public void setReferenceInterface(String referenceInterface) {
+        this.referenceInterface = referenceInterface;
+    }
+
+    /**
+     * @return Classloader URI.
+     */
+    public URI getClassloaderURI() {
+        return classloaderURI;
+    }
+
+    /**
+     * @param classloaderURI Classloader URI.
+     */
+    public void setClassloaderURI(URI classloaderURI) {
+        this.classloaderURI = classloaderURI;
+    }
+
+
+    /**
+     *
+     * @return WSDL Element
+     */
+    public String getWsdlElement() {
+        return wsdlElement;
+    }
+
+    /**
+     * @param wsdlElement WSDL Element
+     */
+    public void setWsdlElement(String wsdlElement) {
+        this.wsdlElement = wsdlElement;
+        String[] parsed = ProvisionHelper.parseWSDLElement(wsdlElement);
+        setNamespaceURI(parsed[0]);
+        setServiceName(parsed[1]);
+        setPortName(parsed[2]);
+        if (wsdlLocation == null) {
+            setWsdlLocation(namespaceURI  + serviceName + "/" + portName + "?wsdl");
+        }
+    }
+
+    /**
+     * @return WSDL Location
+     */
+    public String getWsdlLocation() {
+        return wsdlLocation;
+    }
+
+    /**
+     * @param wsdlLocation WSDL Location
+     */
+    public void setWsdlLocation(String wsdlLocation) {
+        this.wsdlLocation = wsdlLocation;
+    }
+
+
+    public void setNamespaceURI(String uri) {
+        this.namespaceURI = uri;
+    }
+
+    /**
+     * Get WSDL namespace uri
+     * @return wsdl namespace uri
+     */
+    public String getNamespaceURI() {
+        return namespaceURI;
+    }
+
+    /**
+     * Get WSDL Service Name
+    *
+    * @return service name
+    */
+    public String getServiceName() {
+       return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    /**
+      * Get WSDL port name
+      *
+      * @return portname for the wsdl
+    */
+    public String getPortName() {
+        return portName;
+    }
+
+    public void setPortName(String portName) {
+        this.portName = portName;
+    }
+
+}
---
+++ modules/tags/fabric3-binding-ws-jaxws-0.5ALPHA2/src/main/java/org/fabric3/binding/ws/jaxws/runtime/WsTargetInterceptor.java
@@ -0,0 +1,99 @@
+package org.fabric3.binding.ws.jaxws.runtime;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.MalformedURLException;
+
+import javax.xml.ws.Service;
+import javax.xml.namespace.QName;
+
+import org.osoa.sca.ServiceRuntimeException;
+
+import org.fabric3.spi.wire.Interceptor;
+import org.fabric3.spi.invocation.Message;
+import org.fabric3.spi.invocation.MessageImpl;
+
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+public class WsTargetInterceptor implements Interceptor {
+
+    private final Method method;
+    private final Class<?> interfazz;
+    private Interceptor next;
+    private Object reference;
+    private final QName portQName;
+    private final QName serviceQName;
+    private final String wsdlLocation;
+    private Service service;
+
+    public WsTargetInterceptor(Method method,
+                               Class<?> interfazz,
+                               String wsdlLocation,
+                               String serviceName,
+                               String portName,
+                               String targetNamespace) {
+        this.method = method;
+        this.interfazz = interfazz;
+        this.wsdlLocation = wsdlLocation;
+        this.portQName = new QName(targetNamespace, portName);
+        this.serviceQName = new QName(targetNamespace, serviceName);
+    }
+
+    public Interceptor getNext() {
+        return next;
+    }
+
+    public void setNext(Interceptor next) {
+        this.next = next;
+    }
+
+    public Message invoke(Message message) {
+        Object object = getReference();
+        Object[] parameters = (Object[]) message.getBody();
+        Message result = new MessageImpl();
+        try {
+            result.setBody(method.invoke(object, parameters));
+        } catch (InvocationTargetException ite) {
+            reference = null;
+            result.setBodyWithFault(ite.getCause());
+        } catch (Exception e) {
+            reference = null;
+            throw new ServiceRuntimeException(e);
+        }
+        return result;
+    }
+
+
+    private Object getReference() {
+        if (service == null) {
+            try {
+                service = Service.create(new URL(wsdlLocation), serviceQName);
+            } catch (MalformedURLException mue) {
+                AssertionError ae = new AssertionError("Unexpected exception");
+                ae.initCause(mue);
+                throw ae;
+            }
+        }
+        if (reference == null) {
+            reference = service.getPort(portQName, interfazz);
+        }
+        return reference;
+    }
+
+}
