f0f9a1408d6c111ff5a17c60e8f7736edd1da4c0
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 11:01:01 +0000

    [maven-release-plugin]  copy for tag fabric3-policy-0.5ALPHA2

---
+++ modules/tags/fabric3-policy-0.5ALPHA2/src/main/java/org/fabric3/fabric/policy/helper/InteractionPolicyHelperImpl.java
@@ -0,0 +1,125 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.policy.helper;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.fabric3.fabric.policy.infoset.PolicySetEvaluator;
+import org.fabric3.scdl.Operation;
+import org.fabric3.scdl.definitions.BindingType;
+import org.fabric3.scdl.definitions.Intent;
+import org.fabric3.scdl.definitions.PolicySet;
+import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.policy.PolicyResolutionException;
+import org.fabric3.spi.services.definitions.DefinitionsRegistry;
+import org.osoa.sca.annotations.Reference;
+import org.w3c.dom.Element;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class InteractionPolicyHelperImpl extends AbstractPolicyHelper implements InteractionPolicyHelper {
+
+    public InteractionPolicyHelperImpl(@Reference DefinitionsRegistry definitionsRegistry,
+                                       @Reference PolicySetEvaluator policySetEvaluator) {
+        super(definitionsRegistry, policySetEvaluator);
+    }
+
+    public Set<Intent> getProvidedIntents(LogicalBinding<?> logicalBinding, Operation<?> operation) throws PolicyResolutionException {
+        
+        QName type = logicalBinding.getBinding().getType();
+        BindingType bindingType = definitionsRegistry.getDefinition(type, BindingType.class);
+        
+        // FIXME This should not happen, all binding types should be registsred
+        if (bindingType == null) {
+            return Collections.emptySet();
+        }
+
+        Set<QName> mayProvidedIntents = bindingType.getMayProvide();
+        
+        Set<Intent> requiredIntents = getRequestedIntents(logicalBinding, operation);
+        
+        Set<Intent> intentsToBeProvided = new LinkedHashSet<Intent>();
+        for(Intent intent : requiredIntents) {
+            if(mayProvidedIntents.contains(intent.getName())) {
+                intentsToBeProvided.add(intent);
+            }
+        }
+        
+        return intentsToBeProvided;
+        
+    }
+    
+    public Set<PolicySet> resolveIntents(LogicalBinding<?> logicalBinding, Operation<?> operation, Element target) throws PolicyResolutionException {
+
+        QName type = logicalBinding.getBinding().getType();
+        BindingType bindingType = definitionsRegistry.getDefinition(type, BindingType.class);
+        
+        Set<QName> alwaysProvidedIntents = new LinkedHashSet<QName>();
+        Set<QName> mayProvidedIntents = new LinkedHashSet<QName>();
+
+        // FIXME This should not happen, all binding types should be registsred
+        if (bindingType != null) {
+            alwaysProvidedIntents = bindingType.getAlwaysProvide();
+            mayProvidedIntents = bindingType.getMayProvide();
+        }
+
+        Set<Intent> requiredIntents = getRequestedIntents(logicalBinding, operation);
+        Set<Intent> requiredIntentsCopy = new HashSet<Intent>(requiredIntents);
+        
+        // Remove intents that are provided
+        for(Intent intent : requiredIntentsCopy) {
+            QName intentName = intent.getName();
+            if(alwaysProvidedIntents.contains(intentName) || mayProvidedIntents.contains(intentName)) {
+                requiredIntents.remove(intent);
+            }
+        }
+        
+        Set<PolicySet> policies = resolvePolicies(requiredIntents, target, operation.getName());        
+        if(requiredIntents.size() > 0) {
+            throw new PolicyResolutionException("Unable to resolve all intents", requiredIntents);
+        }
+        
+        return policies;
+        
+    }
+
+    private Set<Intent> getRequestedIntents(LogicalBinding<?> logicalBinding, 
+                                            Operation<?> operation) throws PolicyResolutionException {
+        
+        // Aggregate all the intents from the ancestors
+        Set<QName> intentNames = new LinkedHashSet<QName>();
+        intentNames.addAll(operation.getIntents());
+        intentNames.addAll(logicalBinding.getBinding().getIntents());
+        intentNames.addAll(aggregateIntents(logicalBinding));
+        
+        // Expand all the profile intents
+        Set<Intent> requiredIntents = resolveProfileIntents(intentNames);
+        
+        // Remove intents not applicable to the artifact
+        filterInvalidIntents(Intent.BINDING, requiredIntents);
+        
+        return requiredIntents;
+        
+    }
+
+}
---
+++ modules/tags/fabric3-policy-0.5ALPHA2/src/test/java/org/fabric3/fabric/policy/infoset/DefaultPolicyInfosetBuilderTest.java
@@ -0,0 +1,113 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.fabric.policy.infoset;
+
+import javax.xml.namespace.QName;
+
+import junit.framework.TestCase;
+
+import org.fabric3.scdl.AbstractComponentType;
+import org.fabric3.scdl.BindingDefinition;
+import org.fabric3.scdl.ComponentDefinition;
+import org.fabric3.scdl.Implementation;
+import org.fabric3.scdl.Property;
+import org.fabric3.scdl.ReferenceDefinition;
+import org.fabric3.scdl.ResourceDefinition;
+import org.fabric3.scdl.ServiceDefinition;
+import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.model.instance.LogicalComponent;
+import org.fabric3.spi.model.instance.LogicalReference;
+import org.w3c.dom.Element;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class DefaultPolicyInfosetBuilderTest extends TestCase {
+
+    public void testBuildInfoSetLogicalBinding() {
+        
+        Element element = new DefaultPolicyInfosetBuilder().buildInfoSet(getTestBinding());
+        
+        assertEquals("service", element.getNodeName());
+        assertEquals("testService", element.getAttribute("name"));
+        assertEquals("binding.test", element.getFirstChild().getNodeName());
+        assertEquals("component", element.getParentNode().getNodeName());
+        assertEquals("testComponent", element.getParentNode().getAttributes().getNamedItem("name").getNodeValue());
+        
+    }
+
+    public void testBuildInfoSetLogicalComponent() {
+        
+        Element element = new DefaultPolicyInfosetBuilder().buildInfoSet(getTestComponent());
+        
+        assertEquals("component", element.getNodeName());
+        assertEquals("testComponent", element.getAttribute("name"));
+        assertEquals("implementation.test", element.getFirstChild().getNodeName());
+        
+    }
+    
+    static LogicalComponent<?> getTestComponent() {
+        
+        TestComponentType componentType = new TestComponentType();
+        TestImplementation implementation = new TestImplementation(componentType);
+        ComponentDefinition<TestImplementation> componentDefinition = new ComponentDefinition<TestImplementation>("testComponent", implementation);
+        LogicalComponent<TestImplementation> logicalComponent = new LogicalComponent<TestImplementation>(null, null, componentDefinition, null);
+        
+        return logicalComponent;
+    }
+    
+    static LogicalBinding<?> getTestBinding() {
+        
+        TestComponentType componentType = new TestComponentType();
+        TestImplementation implementation = new TestImplementation(componentType);
+        ComponentDefinition<TestImplementation> componentDefinition = new ComponentDefinition<TestImplementation>("testComponent", implementation);
+        LogicalComponent<TestImplementation> logicalComponent = new LogicalComponent<TestImplementation>(null, null, componentDefinition, null);
+        
+        ReferenceDefinition referenceDefinition = new ReferenceDefinition("testService", null);
+        LogicalReference logicalReference = new LogicalReference(null, referenceDefinition, logicalComponent);
+        
+        TestBinidingDefinition testBinidingDefinition = new TestBinidingDefinition();
+        LogicalBinding<TestBinidingDefinition> logicalBinding = new LogicalBinding<TestBinidingDefinition>(testBinidingDefinition, logicalReference);
+        logicalReference.addBinding(logicalBinding);
+        
+        return logicalBinding;
+        
+    }
+    
+    private static class TestBinidingDefinition extends BindingDefinition {
+        private TestBinidingDefinition() {
+            super(new QName("binding.test"));
+        }
+    }
+    
+    private static class TestComponentType extends AbstractComponentType<ServiceDefinition, ReferenceDefinition, Property, ResourceDefinition> {
+        
+    }
+    
+    private static class TestImplementation extends Implementation<TestComponentType> {
+        
+        private TestImplementation(TestComponentType componentType) {
+            super(componentType);
+        }
+        
+        @Override
+        public QName getType() {
+            return new QName("implementation.test");
+        }
+    }
+
+}
