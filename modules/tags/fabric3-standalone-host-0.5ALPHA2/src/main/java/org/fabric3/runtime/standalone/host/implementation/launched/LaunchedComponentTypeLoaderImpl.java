f1770e71c95528f10eab358610c05eb9fc7fd394
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 21:56:12 +0000

    [maven-release-plugin]  copy for tag fabric3-standalone-host-0.5ALPHA2

---
+++ modules/tags/fabric3-standalone-host-0.5ALPHA2/src/main/java/org/fabric3/runtime/standalone/host/implementation/launched/LaunchedComponentTypeLoaderImpl.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.runtime.standalone.host.implementation.launched;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.xml.LoaderException;
+import org.fabric3.introspection.xml.MissingResourceException;
+import org.fabric3.pojo.processor.ProcessingException;
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.scdl.DataType;
+import org.fabric3.scdl.Operation;
+import org.fabric3.scdl.ServiceContract;
+import org.fabric3.scdl.ServiceDefinition;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class LaunchedComponentTypeLoaderImpl implements LaunchedComponentTypeLoader {
+    private static final String SERVICE_NAME = "main";
+
+    public LaunchedComponentTypeLoaderImpl() {
+    }
+
+    public void load(Launched implementation, IntrospectionContext introspectionContext) throws LoaderException {
+        String className = implementation.getImplementationClass();
+        Class<?> implClass;
+        try {
+            implClass = introspectionContext.getTargetClassLoader().loadClass(className);
+        } catch (ClassNotFoundException e) {
+            throw new MissingResourceException(className, e);
+        }
+        PojoComponentType componentType = loadByIntrospection(implementation, introspectionContext, implClass);
+        componentType.setScope("COMPOSITE");
+        implementation.setComponentType(componentType);
+    }
+
+    protected PojoComponentType loadByIntrospection(
+            Launched implementation,
+            IntrospectionContext introspectionContext,
+            Class<?> implClass) throws ProcessingException {
+        PojoComponentType componentType = new PojoComponentType(implClass.getName());
+        // FIXME we need to introspect the implementation here
+//        introspector.introspect(implClass, componentType, introspectionContext);
+
+        ServiceContract launchedContract = generateContract(implClass);
+        ServiceDefinition testService = new ServiceDefinition(SERVICE_NAME, launchedContract);
+        componentType.add(testService);
+        return componentType;
+    }
+
+    private static final DataType<List<DataType<Type>>> INPUT_TYPE;
+    private static final DataType<Type> OUTPUT_TYPE;
+    private static final List<DataType<Type>> FAULT_TYPE;
+
+    static {
+        List<DataType<Type>> paramDataTypes = new ArrayList<DataType<Type>>();
+        //noinspection unchecked
+        paramDataTypes.add(new DataType(String[].class, String[].class));
+        INPUT_TYPE = new DataType<List<DataType<Type>>>(Object[].class, paramDataTypes);
+        OUTPUT_TYPE = new DataType<Type>(Object.class, Object.class);
+        FAULT_TYPE = Collections.emptyList();
+    }
+
+    protected ServiceContract generateContract(Class<?> implClass) {
+        List<Operation<Type>> operations = new ArrayList<Operation<Type>>();
+        Operation<Type> operation = new Operation<Type>("main", INPUT_TYPE, OUTPUT_TYPE, FAULT_TYPE);
+        operations.add(operation);
+        return new LaunchedServiceContract(operations);
+    }
+
+}
