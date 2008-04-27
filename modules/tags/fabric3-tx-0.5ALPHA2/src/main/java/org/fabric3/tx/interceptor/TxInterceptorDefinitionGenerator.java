bfb961f4bb7ea532d3dcb8216557421037a91a4e
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-04-27 21:14:14 +0000

    [maven-release-plugin]  copy for tag fabric3-tx-0.5ALPHA2

---
+++ modules/tags/fabric3-tx-0.5ALPHA2/src/main/java/org/fabric3/tx/interceptor/TxInterceptorDefinitionGenerator.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.fabric3.tx.interceptor;
+
+import javax.xml.namespace.QName;
+
+import org.fabric3.scdl.Operation;
+import org.fabric3.spi.Constants;
+import org.fabric3.spi.generator.GenerationException;
+import org.fabric3.spi.generator.GeneratorRegistry;
+import org.fabric3.spi.generator.InterceptorDefinitionGenerator;
+import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.model.physical.PhysicalInterceptorDefinition;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+import org.w3c.dom.Element;
+
+/**
+ * Interceptor definition generator for suspend transaction policy extensions.
+ *
+ * @version $Revision$ $Date$
+ */
+@EagerInit
+public class TxInterceptorDefinitionGenerator implements InterceptorDefinitionGenerator {
+    private static final QName EXTENSION_NAME = new QName(Constants.FABRIC3_NS, "transaction");
+    private GeneratorRegistry generatorRegistry;
+
+    public TxInterceptorDefinitionGenerator(@Reference GeneratorRegistry generatorRegistry) {
+        this.generatorRegistry = generatorRegistry;
+    }
+
+    /**
+     * Registers with the registry.
+     */
+    @Init
+    public void start() {
+        generatorRegistry.register(EXTENSION_NAME, this);
+    }
+
+    public PhysicalInterceptorDefinition generate(Element policyDefinition,
+                                                  Operation<?> operation,
+                                                  LogicalBinding<?> logicalBinding) throws GenerationException {
+        String action = policyDefinition.getAttribute("action");
+
+        return new TxInterceptorDefinition(TxAction.valueOf(action));
+    }
+}
