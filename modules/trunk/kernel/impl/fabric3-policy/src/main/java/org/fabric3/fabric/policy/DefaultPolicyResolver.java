23e450ddd2db45bf160361552dcf53623846e8b0
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-05-01 23:13:09 +0000

    Fixed duplicate evaluation of operation level interaction intents.

--- modules/trunk/kernel/impl/fabric3-policy/src/main/java/org/fabric3/fabric/policy/DefaultPolicyResolver.java
+++ modules/trunk/kernel/impl/fabric3-policy/src/main/java/org/fabric3/fabric/policy/DefaultPolicyResolver.java
@@ -87,8 +87,12 @@ public class DefaultPolicyResolver implements PolicyResolver {
                                         LogicalComponent<?> target) throws PolicyResolutionException {
         
         PolicyResultImpl policyResult = new PolicyResultImpl();
+        boolean boundService = source == null;
+        boolean boundReference = target == null;
             
         for (Operation<?> operation : serviceContract.getOperations()) {
+            
+            System.err.println(operation.getName());
             
             policyResult.addSourceIntents(operation, interactionPolicyHelper.getProvidedIntents(sourceBinding, operation));
             if (source != null) {
@@ -110,15 +114,19 @@ public class DefaultPolicyResolver implements PolicyResolver {
                 policyResult.addInterceptedPolicySets(operation, CollectionUtils.filter(policies, INTERCEPTION));
             }
 
-            policyInfoset = policyInfosetBuilder.buildInfoSet(sourceBinding);
-            policies = interactionPolicyHelper.resolveIntents(sourceBinding, operation, policyInfoset);
-            policyResult.addSourcePolicySets(operation, CollectionUtils.filter(policies, PROVIDED));
-            policyResult.addInterceptedPolicySets(operation, CollectionUtils.filter(policies, INTERCEPTION));
+            if (!boundService) {
+                policyInfoset = policyInfosetBuilder.buildInfoSet(sourceBinding);
+                policies = interactionPolicyHelper.resolveIntents(sourceBinding, operation, policyInfoset);
+                policyResult.addSourcePolicySets(operation, CollectionUtils.filter(policies, PROVIDED));
+                policyResult.addInterceptedPolicySets(operation, CollectionUtils.filter(policies, INTERCEPTION));
+            }
 
-            policyInfoset = policyInfosetBuilder.buildInfoSet(targetBinding);
-            policies = interactionPolicyHelper.resolveIntents(targetBinding, operation, policyInfoset);
-            policyResult.addTargetPolicySets(operation, CollectionUtils.filter(policies, PROVIDED));
-            policyResult.addInterceptedPolicySets(operation, CollectionUtils.filter(policies, INTERCEPTION));
+            if (!boundReference) {
+                policyInfoset = policyInfosetBuilder.buildInfoSet(targetBinding);
+                policies = interactionPolicyHelper.resolveIntents(targetBinding, operation, policyInfoset);
+                policyResult.addTargetPolicySets(operation, CollectionUtils.filter(policies, PROVIDED));
+                policyResult.addInterceptedPolicySets(operation, CollectionUtils.filter(policies, INTERCEPTION));
+            }
                 
             if (target != null) {
                 policyInfoset = policyInfosetBuilder.buildInfoSet(target);
