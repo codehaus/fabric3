fd71afc2c4af5448b41d36fe65807eefe7a24a5d
derekd <derekd@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-05-17 11:27:42 +0000

    Added annotations for component scopes (JIRA-210)

---
+++ modules/trunk/kernel/impl/fabric3-introspection/src/main/java/org/fabric3/introspection/impl/annotation/CompositeProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.introspection.impl.annotation;
+
+import org.fabric3.api.annotation.scope.Composite;
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.IntrospectionException;
+import org.fabric3.introspection.java.AbstractAnnotationProcessor;
+import org.fabric3.scdl.Implementation;
+import org.fabric3.scdl.InjectingComponentType;
+import org.osoa.sca.annotations.Scope;
+
+
+public class CompositeProcessor<I extends Implementation<? extends InjectingComponentType>> extends AbstractAnnotationProcessor<Composite, I> {
+
+    public CompositeProcessor() {
+        super(Composite.class);
+    }
+
+    public void visitType(Composite annotation, Class<?> type, I implementation, IntrospectionContext context) throws IntrospectionException {
+        Scope scopeMetaAnnotation = annotation.annotationType().getAnnotation(Scope.class);
+        String scopeName = scopeMetaAnnotation.value();
+        implementation.getComponentType().setScope(scopeName);
+    }
+}
---
+++ modules/trunk/kernel/impl/fabric3-introspection/src/main/java/org/fabric3/introspection/impl/annotation/RequestProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.introspection.impl.annotation;
+
+import org.fabric3.api.annotation.scope.Request;
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.IntrospectionException;
+import org.fabric3.introspection.java.AbstractAnnotationProcessor;
+import org.fabric3.scdl.Implementation;
+import org.fabric3.scdl.InjectingComponentType;
+import org.osoa.sca.annotations.Scope;
+
+
+public class RequestProcessor<I extends Implementation<? extends InjectingComponentType>> extends AbstractAnnotationProcessor<Request, I> {
+
+    public RequestProcessor() {
+        super(Request.class);
+    }
+
+    public void visitType(Request annotation, Class<?> type, I implementation, IntrospectionContext context) throws IntrospectionException {
+        Scope scopeMetaAnnotation = annotation.annotationType().getAnnotation(Scope.class);
+        String scopeName = scopeMetaAnnotation.value();
+        implementation.getComponentType().setScope(scopeName);
+    }
+}
