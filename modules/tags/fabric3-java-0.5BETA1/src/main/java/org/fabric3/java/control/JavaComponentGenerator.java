41f5c493b5b520059999eb8ed7377f670776c4a2
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-05-27 07:20:25 +0000

    [maven-release-plugin]  copy for tag fabric3-java-0.5BETA1

---
+++ modules/tags/fabric3-java-0.5BETA1/src/main/java/org/fabric3/java/control/JavaComponentGenerator.java
@@ -0,0 +1,203 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.java.control;
+
+import java.net.URI;
+import javax.xml.namespace.QName;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.java.provision.JavaComponentDefinition;
+import org.fabric3.java.provision.JavaWireSourceDefinition;
+import org.fabric3.java.provision.JavaWireTargetDefinition;
+import org.fabric3.java.scdl.JavaImplementation;
+import org.fabric3.pojo.instancefactory.InstanceFactoryDefinition;
+import org.fabric3.pojo.instancefactory.InstanceFactoryGenerationHelper;
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.scdl.CallbackDefinition;
+import org.fabric3.scdl.ComponentDefinition;
+import org.fabric3.scdl.InjectableAttribute;
+import org.fabric3.scdl.InjectableAttributeType;
+import org.fabric3.scdl.Operation;
+import org.fabric3.scdl.ServiceContract;
+import org.fabric3.scdl.definitions.Intent;
+import org.fabric3.spi.generator.ComponentGenerator;
+import org.fabric3.spi.generator.GenerationException;
+import org.fabric3.spi.generator.GeneratorRegistry;
+import org.fabric3.spi.model.instance.LogicalComponent;
+import org.fabric3.spi.model.instance.LogicalReference;
+import org.fabric3.spi.model.instance.LogicalResource;
+import org.fabric3.spi.model.instance.LogicalService;
+import org.fabric3.spi.model.physical.PhysicalComponentDefinition;
+import org.fabric3.spi.model.physical.PhysicalWireSourceDefinition;
+import org.fabric3.spi.model.physical.PhysicalWireTargetDefinition;
+import org.fabric3.spi.model.physical.InteractionType;
+import org.fabric3.spi.policy.Policy;
+import org.fabric3.spi.Constants;
+
+/**
+ * Generates a JavaComponentDefinition from a ComponentDefinition corresponding to a Java component implementation
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class JavaComponentGenerator implements ComponentGenerator<LogicalComponent<JavaImplementation>> {
+    private static final QName PROPAGATES_CONVERSATION = new QName(Constants.FABRIC3_NS, "propagatesConversation");
+    private final InstanceFactoryGenerationHelper helper;
+
+    public JavaComponentGenerator(@Reference GeneratorRegistry registry,
+                                  @Reference InstanceFactoryGenerationHelper helper) {
+        registry.register(JavaImplementation.class, this);
+        this.helper = helper;
+    }
+
+    public PhysicalComponentDefinition generate(LogicalComponent<JavaImplementation> component)
+            throws GenerationException {
+        ComponentDefinition<JavaImplementation> definition = component.getDefinition();
+        JavaImplementation implementation = definition.getImplementation();
+        PojoComponentType type = implementation.getComponentType();
+
+        // create the instance factory definition
+        InstanceFactoryDefinition providerDefinition = new InstanceFactoryDefinition();
+        providerDefinition.setConstructor(type.getConstructor());
+        providerDefinition.setInitMethod(type.getInitMethod());
+        providerDefinition.setDestroyMethod(type.getDestroyMethod());
+        providerDefinition.setImplementationClass(implementation.getImplementationClass());
+        helper.processInjectionSites(component, providerDefinition);
+
+        // create the physical component definition
+        URI componentId = component.getUri();
+        JavaComponentDefinition physical = new JavaComponentDefinition();
+        physical.setComponentId(componentId);
+        physical.setGroupId(component.getParent().getUri());
+        physical.setScope(type.getScope());
+        physical.setInitLevel(helper.getInitLevel(definition, type));
+        physical.setMaxAge(type.getMaxAge());
+        physical.setMaxIdleTime(type.getMaxIdleTime());
+        physical.setInstanceFactoryProviderDefinition(providerDefinition);
+        helper.processPropertyValues(component, physical);
+        // generate the classloader resource definition
+        URI classLoaderId = component.getParent().getUri();
+        physical.setClassLoaderId(classLoaderId);
+
+        return physical;
+
+    }
+
+    public PhysicalWireSourceDefinition generateWireSource(LogicalComponent<JavaImplementation> source,
+                                                           LogicalReference reference,
+                                                           Policy policy) throws GenerationException {
+        URI uri = reference.getUri();
+        ServiceContract<?> serviceContract = reference.getDefinition().getServiceContract();
+        String interfaceName = serviceContract.getQualifiedInterfaceName();
+        URI classLoaderId = source.getParent().getUri();
+
+        JavaWireSourceDefinition wireDefinition = new JavaWireSourceDefinition();
+        wireDefinition.setUri(uri);
+        wireDefinition.setValueSource(new InjectableAttribute(InjectableAttributeType.REFERENCE, uri.getFragment()));
+        wireDefinition.setInterfaceName(interfaceName);
+        // assume for now that any wire from a Java component can be optimized
+        wireDefinition.setOptimizable(true);
+
+        wireDefinition.setClassLoaderId(classLoaderId);
+        calculateConversationalPolicy(wireDefinition, serviceContract, policy);
+        return wireDefinition;
+    }
+
+    public PhysicalWireSourceDefinition generateCallbackWireSource(LogicalComponent<JavaImplementation> source,
+                                                                   ServiceContract<?> serviceContract,
+                                                                   Policy policy) throws GenerationException {
+        String interfaceName = serviceContract.getQualifiedInterfaceName();
+        URI classLoaderId = source.getParent().getUri();
+        PojoComponentType type = source.getDefinition().getImplementation().getComponentType();
+        String name = null;
+        for (CallbackDefinition entry : type.getCallbacks().values()) {
+            // NB: This currently only supports the case where one callback injection site of the same type is on an implementation.
+            // TODO clarify with the spec if having more than one callback injection site of the same type is valid
+            if (entry.getServiceContract().isAssignableFrom(serviceContract)) {
+                name = entry.getName();
+                break;
+            }
+        }
+        if (name == null) {
+            String interfaze = serviceContract.getQualifiedInterfaceName();
+            throw new CallbackSiteNotFound("Callback injection site not found for type: " + interfaze, interfaze);
+        }
+
+        JavaWireSourceDefinition wireDefinition = new JavaWireSourceDefinition();
+        wireDefinition.setValueSource(new InjectableAttribute(InjectableAttributeType.CALLBACK, name));
+        wireDefinition.setInterfaceName(interfaceName);
+        wireDefinition.setUri(URI.create(source.getUri().toString() + "#" + name));
+        wireDefinition.setOptimizable(false);
+        wireDefinition.setClassLoaderId(classLoaderId);
+        return wireDefinition;
+    }
+
+    public PhysicalWireSourceDefinition generateResourceWireSource(LogicalComponent<JavaImplementation> source,
+                                                                   LogicalResource<?> resource) throws GenerationException {
+        URI uri = resource.getUri();
+        ServiceContract<?> serviceContract = resource.getResourceDefinition().getServiceContract();
+        String interfaceName = serviceContract.getQualifiedInterfaceName();
+        URI classLoaderId = source.getParent().getUri();
+
+        JavaWireSourceDefinition wireDefinition = new JavaWireSourceDefinition();
+        wireDefinition.setUri(uri);
+        wireDefinition.setValueSource(new InjectableAttribute(InjectableAttributeType.RESOURCE, uri.getFragment()));
+        wireDefinition.setClassLoaderId(classLoaderId);
+        wireDefinition.setInterfaceName(interfaceName);
+        return wireDefinition;
+    }
+
+    public PhysicalWireTargetDefinition generateWireTarget(LogicalService service,
+                                                           LogicalComponent<JavaImplementation> target,
+                                                           Policy policy) throws GenerationException {
+        JavaWireTargetDefinition wireDefinition = new JavaWireTargetDefinition();
+        URI uri;
+        if (service != null) {
+            uri = service.getUri();
+        } else {
+            // no service specified, use the default
+            uri = target.getUri();
+        }
+        wireDefinition.setUri(uri);
+
+        // assume for now that only wires to composite scope components can be optimized
+        String scope = target.getDefinition().getImplementation().getComponentType().getScope();
+        wireDefinition.setOptimizable("COMPOSITE".equals(scope));
+        return wireDefinition;
+    }
+
+    private void calculateConversationalPolicy(JavaWireSourceDefinition wireDefinition, ServiceContract<?> serviceContract, Policy policy) {
+        for (Operation<?> operation : serviceContract.getOperations()) {
+            for (Intent intent : policy.getProvidedIntents(operation)) {
+                if (PROPAGATES_CONVERSATION.equals(intent.getName())) {
+                    wireDefinition.setInteractionType(InteractionType.PROPAGATES_CONVERSATION);
+                    // conversational propagation is for the entire reference so set it an return
+                    return;
+                }
+            }
+        }
+        if (serviceContract.isConversational()) {
+            wireDefinition.setInteractionType(InteractionType.CONVERSATIONAL);
+        }
+
+    }
+
+}
---
+++ modules/tags/fabric3-java-0.5BETA1/src/main/java/org/fabric3/java/introspection/JavaHeuristic.java
@@ -0,0 +1,227 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.java.introspection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.Set;
+
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.introspection.IntrospectionContext;
+import org.fabric3.introspection.IntrospectionHelper;
+import org.fabric3.introspection.TypeMapping;
+import org.fabric3.introspection.contract.ContractProcessor;
+import org.fabric3.introspection.java.HeuristicProcessor;
+import org.fabric3.introspection.java.UnknownInjectionType;
+import org.fabric3.introspection.java.NoConstructorFound;
+import org.fabric3.java.scdl.JavaImplementation;
+import org.fabric3.pojo.scdl.PojoComponentType;
+import org.fabric3.scdl.ConstructorInjectionSite;
+import org.fabric3.scdl.FieldInjectionSite;
+import org.fabric3.scdl.InjectableAttribute;
+import org.fabric3.scdl.InjectableAttributeType;
+import org.fabric3.scdl.InjectionSite;
+import org.fabric3.scdl.MethodInjectionSite;
+import org.fabric3.scdl.Multiplicity;
+import org.fabric3.scdl.Property;
+import org.fabric3.scdl.ReferenceDefinition;
+import org.fabric3.scdl.ServiceContract;
+import org.fabric3.scdl.Signature;
+import org.fabric3.scdl.validation.AmbiguousConstructor;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class JavaHeuristic implements HeuristicProcessor<JavaImplementation> {
+
+    private final IntrospectionHelper helper;
+    private final ContractProcessor contractProcessor;
+
+    private final HeuristicProcessor<JavaImplementation> serviceHeuristic;
+    private final HeuristicProcessor<JavaImplementation> dataTypeHeuristic;
+
+    public JavaHeuristic(@Reference IntrospectionHelper helper,
+                         @Reference ContractProcessor contractProcessor,
+                         @Reference(name = "service")HeuristicProcessor<JavaImplementation> serviceHeuristic,
+                         @Reference(name = "dataType")HeuristicProcessor<JavaImplementation> dataTypeHeuristic) {
+        this.helper = helper;
+        this.contractProcessor = contractProcessor;
+        this.serviceHeuristic = serviceHeuristic;
+        this.dataTypeHeuristic = dataTypeHeuristic;
+    }
+
+    public void applyHeuristics(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) {
+
+        PojoComponentType componentType = implementation.getComponentType();
+
+        // apply service heuristic
+        serviceHeuristic.applyHeuristics(implementation, implClass, context);
+
+        if (componentType.getConstructor() == null) {
+            Signature ctor = findConstructor(implClass, context);
+            componentType.setConstructor(ctor);
+        }
+
+        if (componentType.getProperties().isEmpty() && componentType.getReferences().isEmpty() && componentType.getResources().isEmpty()) {
+            evaluateConstructor(implementation, implClass, context);
+            evaluateSetters(implementation, implClass, context);
+            evaluateFields(implementation, implClass, context);
+        }
+
+        // apply data type mapping heuristic
+        dataTypeHeuristic.applyHeuristics(implementation, implClass, context);
+    }
+
+    Signature findConstructor(Class<?> implClass, IntrospectionContext context) {
+        Constructor<?>[] constructors = implClass.getDeclaredConstructors();
+        Constructor<?> selected = null;
+        if (constructors.length == 1) {
+            selected = constructors[0];
+        } else {
+            for (Constructor<?> constructor : constructors) {
+                if (constructor.isAnnotationPresent(org.osoa.sca.annotations.Constructor.class)) {
+                    if (selected != null) {
+                        context.addError(new AmbiguousConstructor(implClass));
+                        return null;
+                    }
+                    selected = constructor;
+                }
+            }
+            if (selected == null) {
+                context.addError(new NoConstructorFound(implClass));
+                return null;
+            }
+        }
+        return new Signature(selected);
+    }
+
+    void evaluateConstructor(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) {
+        PojoComponentType componentType = implementation.getComponentType();
+        Map<InjectionSite, InjectableAttribute> sites = componentType.getInjectionSites();
+        Constructor<?> constructor;
+        try {
+            Signature ctor = componentType.getConstructor();
+            if (ctor == null) {
+                // there could have been an error evaluating the constructor, in which case no signature will be present
+                return;
+            }
+            constructor = ctor.getConstructor(implClass);
+        } catch (ClassNotFoundException e) {
+            throw new AssertionError(e);
+        } catch (NoSuchMethodException e) {
+            throw new AssertionError(e);
+        }
+
+        TypeMapping typeMapping = context.getTypeMapping();
+        Type[] parameterTypes = constructor.getGenericParameterTypes();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            InjectionSite site = new ConstructorInjectionSite(constructor, i);
+
+            // skip sites that have already been mapped
+            if (sites.containsKey(site)) {
+                continue;
+            }
+
+            Type parameterType = parameterTypes[i];
+            String name = helper.getSiteName(constructor, i, null);
+            processSite(componentType, typeMapping, name, parameterType, site, context);
+        }
+    }
+
+    void evaluateSetters(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) {
+        PojoComponentType componentType = implementation.getComponentType();
+        Map<InjectionSite, InjectableAttribute> sites = componentType.getInjectionSites();
+        TypeMapping typeMapping = context.getTypeMapping();
+        Set<Method> setters = helper.getInjectionMethods(implClass, componentType.getServices().values());
+        for (Method setter : setters) {
+            InjectionSite site = new MethodInjectionSite(setter, 0);
+
+            // skip sites that have already been mapped
+            if (sites.containsKey(site)) {
+                continue;
+            }
+
+            String name = helper.getSiteName(setter, null);
+            Type parameterType = setter.getGenericParameterTypes()[0];
+            processSite(componentType, typeMapping, name, parameterType, site, context);
+        }
+    }
+
+    void evaluateFields(JavaImplementation implementation, Class<?> implClass, IntrospectionContext context) {
+        PojoComponentType componentType = implementation.getComponentType();
+        Map<InjectionSite, InjectableAttribute> sites = componentType.getInjectionSites();
+        TypeMapping typeMapping = context.getTypeMapping();
+        Set<Field> fields = helper.getInjectionFields(implClass);
+        for (Field field : fields) {
+            InjectionSite site = new FieldInjectionSite(field);
+
+            // skip sites that have already been mapped
+            if (sites.containsKey(site)) {
+                continue;
+            }
+
+            String name = helper.getSiteName(field, null);
+            Type parameterType = field.getGenericType();
+            processSite(componentType, typeMapping, name, parameterType, site, context);
+        }
+    }
+
+
+    private void processSite(PojoComponentType componentType,
+                             TypeMapping typeMapping,
+                             String name,
+                             Type parameterType,
+                             InjectionSite site,
+                             IntrospectionContext context) {
+        InjectableAttributeType type = helper.inferType(parameterType, typeMapping);
+        switch (type) {
+        case PROPERTY:
+            addProperty(componentType, typeMapping, name, parameterType, site);
+            break;
+        case REFERENCE:
+            addReference(componentType, typeMapping, name, parameterType, site, context);
+            break;
+        case CALLBACK:
+            context.addError(new UnknownInjectionType(site, type, componentType.getImplClass()));
+            break;
+        default:
+            context.addError(new UnknownInjectionType(site, type, componentType.getImplClass()));
+        }
+    }
+
+    private void addProperty(PojoComponentType componentType, TypeMapping typeMapping, String name, Type parameterType, InjectionSite site) {
+        Property property = new Property(name, null);
+        property.setMany(helper.isManyValued(typeMapping, parameterType));
+        componentType.add(property, site);
+    }
+
+    private void addReference(PojoComponentType componentType,
+                              TypeMapping typeMapping,
+                              String name,
+                              Type parameterType,
+                              InjectionSite site,
+                              IntrospectionContext context) {
+        ServiceContract<Type> contract = contractProcessor.introspect(typeMapping, parameterType, context);
+        Multiplicity multiplicity = helper.isManyValued(typeMapping, parameterType) ? Multiplicity.ONE_N : Multiplicity.ONE_ONE;
+        ReferenceDefinition reference = new ReferenceDefinition(name, contract, multiplicity);
+        componentType.add(reference, site);
+    }
+}
