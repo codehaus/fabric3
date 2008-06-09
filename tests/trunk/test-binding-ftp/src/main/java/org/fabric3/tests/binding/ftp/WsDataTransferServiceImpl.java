fb8cf2edeb885d8eae6e86508aa72d4a144cf454
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-06-09 01:36:39 +0000

    Working integration tests

--- tests/trunk/test-binding-ftp/src/main/java/org/fabric3/tests/binding/ftp/WsDataTransferServiceImpl.java
+++ tests/trunk/test-binding-ftp/src/main/java/org/fabric3/tests/binding/ftp/WsDataTransferServiceImpl.java
@@ -18,7 +18,14 @@
  */
 package org.fabric3.tests.binding.ftp;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+
+import javax.activation.DataHandler;
+
 import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMText;
 
 /**
  *
@@ -27,7 +34,23 @@ import org.apache.axiom.om.OMElement;
 public class WsDataTransferServiceImpl implements WsDataTransferService {
 
     public void transferData(OMElement message) throws Exception {
-        System.err.println("Message received");
+        
+        OMElement fileNameElement = (OMElement) message.getFirstOMChild();
+        String fileName = fileNameElement.getText();
+        System.err.println("File name from web service:" + fileName);
+        
+        OMElement dataElement = (OMElement) fileNameElement.getNextOMSibling();
+        OMText data = (OMText) dataElement.getFirstOMChild();
+        data.setOptimize(true);
+        DataHandler dataHandler = (DataHandler) data.getDataHandler();
+        InputStream is = dataHandler.getInputStream();
+        InputStreamReader reader = new InputStreamReader(is);
+        char buffer[] = new char[1024];
+        StringWriter writer = new StringWriter();
+        for (int count; (count = reader.read(buffer, 0, buffer.length)) > 0;) {
+            writer.write(buffer, 0, count);
+        }
+        System.err.println("File data from web service:" + writer.toString());
     }
 
 }
