66a6812896ca94220dc4b70dd5d92ed6031da047
mbabur <mbabur@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-06-09 15:20:50 +0000

    changes in respect of 0.5BETA2-SNAPSHOT

--- contrib/trunk/fabric3-binding-aq/src/main/java/org/fabric3/binding/aq/connectionfactory/XAConnectionFactoryAccessor.java
+++ contrib/trunk/fabric3-binding-aq/src/main/java/org/fabric3/binding/aq/connectionfactory/XAConnectionFactoryAccessor.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.fabric3.binding.aq.connectionfactory;
@@ -32,25 +32,27 @@ import org.fabric3.binding.aq.model.AQBindingMetadata;
 /**
  * Default implementation that gets the {@link ConnectionFactory} from the
  * {@link AQjmsFactory}
- * 
+ *
  * @version $Revision: 2902 $ $Date: 2007-09-15 13:05:03 +0100 (Sat, 15 Sep 2007) $
  */
 public class XAConnectionFactoryAccessor implements ConnectionFactoryAccessor<XAQueueConnectionFactory> {
 
     /**
      * Gets a QueueConnectionFactory from {@link AQjmsFactory}
-     * 
+     *
      * @param metadata - meta information used to the QueueConnectionFactory
      * @return QueueConnectionFactory
      */
     public XAQueueConnectionFactory getConnectionFactory(final AQBindingMetadata metadata) {
         final XADataSource xaDataSource = (XADataSource)metadata.getDataSource();
-        final XAQueueConnectionFactory connectionFactory;       
+        final XAQueueConnectionFactory connectionFactory;
+
         try {
-            connectionFactory = AQjmsFactory.getXAQueueConnectionFactory(xaDataSource);           
+            connectionFactory = AQjmsFactory.getXAQueueConnectionFactory(xaDataSource);
         } catch (JMSException je) {
             throw new Fabric3AQException("Unable to create AQ connection factory ", je);
         }
         return connectionFactory;
-    }   
+    }
 }
+
