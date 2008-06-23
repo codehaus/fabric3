34f920007fd345143746a40e5b2ecc2452d2c648
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-06-23 22:38:49 +0000

    test multiple firings

--- tests/trunk/test-timer-quartz/src/test/java/org/fabric3/timer/quartz/QuartzTestComponent.java
+++ tests/trunk/test-timer-quartz/src/test/java/org/fabric3/timer/quartz/QuartzTestComponent.java
@@ -18,7 +18,7 @@
  */
 package org.fabric3.timer.quartz;
 
-import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.transaction.Status;
 import javax.transaction.SystemException;
@@ -36,36 +36,51 @@ public class QuartzTestComponent extends TestCase {
 
     @Resource(mappedName = "TransactionalTimerService")
     protected TimerService trxTimerService;
-
     @Resource(mappedName = "NonTransactionalTimerService")
     protected TimerService timerService;
 
     @Resource(mappedName = "TransactionManager")
     protected TransactionManager tm;
 
+    public void testTransactionalScheduleInterval() throws Exception {
+        TrxTestRunnable runnable = new TrxTestRunnable(2);  // test multiple firings
+        trxTimerService.scheduleWithFixedDelay(runnable, 0, 10, TimeUnit.MILLISECONDS);
+        runnable.await();
+        assertTrue(runnable.isTrxStarted());
+    }
+
+    public void testNonTransactionalScheduleInterval() throws Exception {
+        NoTrxTestRunnable runnable = new NoTrxTestRunnable(2);  // test multiple firings
+        timerService.scheduleWithFixedDelay(runnable, 0, 10, TimeUnit.MILLISECONDS);
+        runnable.await();
+        assertTrue(runnable.isNoTrx());
+    }
+
     public void testTransactionalScheduleWithDelay() throws Exception {
-        TrxTestRunnable runnable = new TrxTestRunnable();
-        ScheduledFuture<?> future = trxTimerService.schedule(runnable, 10, TimeUnit.MILLISECONDS);
-        future.get();
-        assertTrue(runnable.isInvoked());
+        TrxTestRunnable runnable = new TrxTestRunnable(1); // fires once
+        trxTimerService.schedule(runnable, 10, TimeUnit.MILLISECONDS);
+        runnable.await();
         assertTrue(runnable.isTrxStarted());
     }
 
     public void testNonTransactionalScheduleWithDelay() throws Exception {
-        NoTrxTestRunnable runnable = new NoTrxTestRunnable();
-        ScheduledFuture<?> future = timerService.schedule(runnable, 10, TimeUnit.MILLISECONDS);
-        future.get();
-        assertTrue(runnable.isInvoked());
+        NoTrxTestRunnable runnable = new NoTrxTestRunnable(1);   // fires once
+        timerService.schedule(runnable, 10, TimeUnit.MILLISECONDS);
+        runnable.await();
         assertTrue(runnable.isNoTrx());
     }
 
     private class TrxTestRunnable implements Runnable {
-        private boolean invoked;
+        private CountDownLatch latch;
         private boolean trxStarted;
 
+        private TrxTestRunnable(int num) {
+            latch = new CountDownLatch(num);
+        }
+
         public void run() {
-            invoked = true;
             try {
+                latch.countDown();
                 if (tm.getStatus() == Status.STATUS_ACTIVE) {
                     trxStarted = true;
                 }
@@ -74,23 +89,27 @@ public class QuartzTestComponent extends TestCase {
             }
         }
 
-        public boolean isInvoked() {
-            return invoked;
-        }
-
         public boolean isTrxStarted() {
             return trxStarted;
+        }
+
+        public void await() throws InterruptedException {
+            latch.await();
         }
     }
 
 
     private class NoTrxTestRunnable implements Runnable {
-        private boolean invoked;
+        private CountDownLatch latch;
         private boolean noTrx;
 
+        private NoTrxTestRunnable(int num) {
+            latch = new CountDownLatch(num);
+        }
+
         public void run() {
-            invoked = true;
             try {
+                latch.countDown();
                 if (tm.getStatus() == Status.STATUS_NO_TRANSACTION) {
                     noTrx = true;
                 }
@@ -99,12 +118,12 @@ public class QuartzTestComponent extends TestCase {
             }
         }
 
-        public boolean isInvoked() {
-            return invoked;
-        }
-
         public boolean isNoTrx() {
             return noTrx;
+        }
+
+        public void await() throws InterruptedException {
+            latch.await();
         }
     }
 
