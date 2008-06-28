89118d137d58dd1702d9890ff87c7917d3f8a580
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-06-28 16:52:09 +0000

    fix typo

--- modules/trunk/extension/implementation/fabric3-java/src/main/java/org/fabric3/java/introspection/JavaImplementationProcessorImpl.java
+++ modules/trunk/extension/implementation/fabric3-java/src/main/java/org/fabric3/java/introspection/JavaImplementationProcessorImpl.java
@@ -64,7 +64,7 @@ public class JavaImplementationProcessorImpl implements JavaImplementationProces
             Throwable cause = e.getCause();
             if (cause instanceof ClassNotFoundException || cause instanceof NoClassDefFoundError) {
                 // CNFE and NCDFE may be thrown as a result of a referenced class not being on the classpath
-                // Ff this is the case, ensure the correct class name is reported, not just the implementation 
+                // If this is the case, ensure the correct class name is reported, not just the implementation 
                 context.addError(new ImplementationArtifactNotFound(implementation, e.getCause().getMessage()));
             } else {
                 context.addError(new ImplementationArtifactNotFound(implementation));
