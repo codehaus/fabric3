5bf0172ef9da67c7f583360148de7609fc8a0910
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-06-28 11:54:49 +0000

    report actual class not found

--- modules/trunk/extension/implementation/fabric3-java/src/main/java/org/fabric3/java/introspection/JavaImplementationProcessorImpl.java
+++ modules/trunk/extension/implementation/fabric3-java/src/main/java/org/fabric3/java/introspection/JavaImplementationProcessorImpl.java
@@ -61,7 +61,14 @@ public class JavaImplementationProcessorImpl implements JavaImplementationProces
         try {
             implClass = helper.loadClass(implClassName, cl);
         } catch (ImplementationNotFoundException e) {
-            context.addError(new ImplementationNotFound(implementation));
+            Throwable cause = e.getCause();
+            if (cause instanceof ClassNotFoundException || cause instanceof NoClassDefFoundError) {
+                // CNFE and NCDFE may be thrown as a result of a referenced class not being on the classpath
+                // Ff this is the case, ensure the correct class name is reported, not just the implementation 
+                context.addError(new ImplementationArtifactNotFound(implementation, e.getCause().getMessage()));
+            } else {
+                context.addError(new ImplementationArtifactNotFound(implementation));
+            }
             return;
         }
         TypeMapping typeMapping = helper.mapTypeParameters(implClass);
