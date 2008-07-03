8a67f5a8a8ba37753f875121da6cf5c2f02c4f38
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-07-03 08:40:09 +0000

    implement QUIT command

--- modules/trunk/extension/other/fabric3-ftp-server/src/main/java/org/fabric3/ftp/server/protocol/FtpSession.java
+++ modules/trunk/extension/other/fabric3-ftp-server/src/main/java/org/fabric3/ftp/server/protocol/FtpSession.java
@@ -20,6 +20,7 @@ package org.fabric3.ftp.server.protocol;
 
 import org.apache.mina.common.IoSession;
 import org.apache.mina.common.WriteFuture;
+
 import org.fabric3.ftp.server.data.DataConnection;
 import org.fabric3.ftp.server.security.User;
 
@@ -29,126 +30,133 @@ import org.fabric3.ftp.server.security.User;
  * @version $Revision$ $Date$
  */
 public class FtpSession {
-    
+
     private static final String USER = "org.fabric3.ftp.server.user";
     private static final String PASSIVE_PORT = "org.fabric3.ftp.server.passive.port";
     private static final String DATA_CONNECTION = "org.fabric3.ftp.server.data.connection";
-    
+
     private IoSession ioSession;
 
     /**
      * Initializes the wrapped IO session.
-     * 
+     *
      * @param ioSession Wrapped IO session.
      */
     public FtpSession(IoSession ioSession) {
         this.ioSession = ioSession;
     }
-    
+
     /**
      * Sets the current user.
-     * 
+     *
      * @param user Current user.
      */
     public void setUser(User user) {
         ioSession.setAttribute(USER, user);
     }
-    
+
     /**
      * Gets the name of the user associated with the session.
-     * 
+     *
      * @return Name of the user associated with the session.
      */
     public String getUserName() {
         User user = getUser();
         return user != null ? user.getName() : "";
     }
-    
+
     /**
      * Gets the current user.
-     * 
+     *
      * @return Current user.
      */
     public User getUser() {
         return (User) ioSession.getAttribute(USER);
     }
-    
+
     /**
      * Sets the session as authenticate.
      */
     public void setAuthenticated() {
         getUser().setAuthenticated();
     }
-    
+
     /**
      * Checks the whether the user is authenticated.
-     * 
+     *
      * @return True if the user is authenticated.
      */
     public boolean isAuthenticated() {
         User user = getUser();
         return user != null && user.isAuthenticated();
     }
-    
+
     /**
      * Gets the passive port.
-     * 
+     *
      * @return Passiv port.
      */
     public int getPassivePort() {
         return (Integer) ioSession.getAttribute(PASSIVE_PORT);
     }
-    
+
     /**
      * Set the passive port.
-     * 
+     *
      * @param passivePort Passive port.
      */
     public void setPassivePort(int passivePort) {
         ioSession.setAttribute(PASSIVE_PORT, passivePort);
     }
-    
+
     /**
      * Writes a message out.
-     * 
+     *
      * @param object Message to be written.
      * @return Write future for the async operation.
      */
     public WriteFuture write(Object object) {
         return ioSession.write(object);
     }
-    
+
     /**
      * Sets the initialized data connection.
-     * 
+     *
      * @param dataConnection Initialized data connection.
      */
     public void setDataConnection(DataConnection dataConnection) {
         ioSession.setAttribute(DATA_CONNECTION, dataConnection);
     }
-    
+
     /**
      * Gets the initialized data connection.
-     * 
+     *
      * @return Initialized data connection.
      */
     public DataConnection getDataConnection() {
         return (DataConnection) ioSession.getAttribute(DATA_CONNECTION);
     }
-    
+
     /**
      * Closes the data connection.
      */
     public void closeDataConnection() {
-        
+
         DataConnection dataConnection = getDataConnection();
         if (null != dataConnection) {
             dataConnection.close();
         }
-        
+
         setDataConnection(null);
         setPassivePort(0);
-        
+
+    }
+
+    /**
+     * Closes the FTP session.
+     */
+    public void close() {
+        ioSession.close();
     }
 
 }
