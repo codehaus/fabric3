879858119784881b464883695fc950b7ab99ed92
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-07-21 17:17:30 +0000

    add metdata for caching large attachments

--- modules/trunk/extension/binding/fabric3-binding-ws-axis2/src/main/java/org/fabric3/binding/ws/axis2/provision/Axis2WireSourceDefinition.java
+++ modules/trunk/extension/binding/fabric3-binding-ws-axis2/src/main/java/org/fabric3/binding/ws/axis2/provision/Axis2WireSourceDefinition.java
@@ -22,17 +22,18 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.fabric3.binding.ws.axis2.provision.AxisPolicy;
 import org.fabric3.spi.model.physical.PhysicalWireSourceDefinition;
 
 /**
  * @version $Revision$ $Date$
  */
 public class Axis2WireSourceDefinition extends PhysicalWireSourceDefinition implements Axis2PolicyAware {
-
+    public static final long CACHE_ALL = -1;
     private String serviceInterface;
     private Map<String, Set<AxisPolicy>> policies = new HashMap<String, Set<AxisPolicy>>();
     private URI classloaderURI;
+    private boolean cacheLargeAttachements;
+    private long cacheThreshold = CACHE_ALL;
 
     /**
      * @return Service interface for the wire source.
@@ -73,11 +74,38 @@ public class Axis2WireSourceDefinition extends PhysicalWireSourceDefinition impl
      * @param policy Policy definitions.
      */
     public void addPolicy(String operation, AxisPolicy policy) {
-        
+
         if (!this.policies.containsKey(operation)) {
             this.policies.put(operation, new HashSet<AxisPolicy>());
         }
         this.policies.get(operation).add(policy);
     }
 
+    /**
+     * @return true if large MTOM attachments should be streamed to disk to avoid buffering in memory.
+     */
+    public boolean isCacheLargeAttachements() {
+        return cacheLargeAttachements;
+    }
+
+    /**
+     * @param cacheLargeAttachements true if large MTOM attachments should be streamed to disk to avoid buffering in memory.
+     */
+    public void setCacheLargeAttachements(boolean cacheLargeAttachements) {
+        this.cacheLargeAttachements = cacheLargeAttachements;
+    }
+
+    /**
+     * @return the file size threshold to cache to disk if MTOM file caching is enabled. Returns {@link #CACHE_ALL} if all files should be cached.
+     */
+    public long getCacheThreshold() {
+        return cacheThreshold;
+    }
+
+    /**
+     * @param threshold the file size threshold to cache to disk if MTOM file caching is enabled.
+     */
+    public void setCacheThreshold(long threshold) {
+        this.cacheThreshold = threshold;
+    }
 }
