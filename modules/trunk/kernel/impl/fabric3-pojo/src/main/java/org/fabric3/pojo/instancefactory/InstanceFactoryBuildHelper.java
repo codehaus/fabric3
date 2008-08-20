f67ef6d32631da6a17a34d00b4f93830a24fe6c9
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-08-20 19:27:39 +0000

    javadoc

--- modules/trunk/kernel/impl/fabric3-pojo/src/main/java/org/fabric3/pojo/instancefactory/InstanceFactoryBuildHelper.java
+++ modules/trunk/kernel/impl/fabric3-pojo/src/main/java/org/fabric3/pojo/instancefactory/InstanceFactoryBuildHelper.java
@@ -22,13 +22,42 @@ import java.lang.reflect.Method;
 import org.fabric3.scdl.Signature;
 
 /**
+ * Utility methods used when creating ReflectiveInstanceFactoryProviders.
+ *
  * @version $Rev$ $Date$
  */
 public interface InstanceFactoryBuildHelper {
+
+    /**
+     * Loads the class using the given classloader
+     *
+     * @param cl   the classloader to load the class with
+     * @param name the name of the class
+     * @return the loaded class
+     * @throws ClassNotFoundException if the class is not accessible to the classloader
+     */
     Class<?> loadClass(ClassLoader cl, String name) throws ClassNotFoundException;
 
+    /**
+     * Returns the constructor on the given class matching the signature.
+     *
+     * @param implClass the class
+     * @param signature the constructor signature
+     * @return the constructor
+     * @throws ClassNotFoundException if one of the constructor parameters could not be loaded
+     * @throws NoSuchMethodException  if no matching constructor could be found
+     */
     <T> Constructor<T> getConstructor(Class<T> implClass, Signature signature) throws ClassNotFoundException, NoSuchMethodException;
 
+    /**
+     * Returns the method on the given class matching the signature.
+     *
+     * @param implClass the class
+     * @param signature the method signature
+     * @return the constructor
+     * @throws ClassNotFoundException if one of the method parameters could not be loaded
+     * @throws NoSuchMethodException  if no matching method could be found
+     */
     Method getMethod(Class<?> implClass, Signature signature) throws NoSuchMethodException, ClassNotFoundException;
 
 }
