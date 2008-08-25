77188b04d0b3b655ec94d016fc8285d98cfc3cff
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-08-25 21:48:36 +0000

    javadoc

--- modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/services/lcm/LogicalComponentManager.java
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/services/lcm/LogicalComponentManager.java
@@ -23,6 +23,8 @@ import org.fabric3.spi.model.instance.LogicalComponent;
 import org.fabric3.spi.model.instance.LogicalCompositeComponent;
 
 /**
+ * Manages logical components in a domain. There is one LogicalComponentManager per domain. Implementations may bve transient or persistent.
+ *
  * @version $Revision$ $Date$
  */
 public interface LogicalComponentManager {
