3a9c11ec4d8793a29609afb8ad74ab11d5a00ea5
meerajk <meerajk@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-09-06 11:16:45 +0000

    Overrode the extension point for creating the MBean server.

--- contrib/trunk/hosts/weblogic92/fabric3-weblogic92-webapp-host/src/main/java/org/fabric3/runtime/weblogic92/WeblogicUtil.java
+++ contrib/trunk/hosts/weblogic92/fabric3-weblogic92-webapp-host/src/main/java/org/fabric3/runtime/weblogic92/WeblogicUtil.java
@@ -18,13 +18,6 @@
  */
 package org.fabric3.runtime.weblogic92;
 
-import static org.fabric3.runtime.webapp.Constants.RUNTIME_DEFAULT;
-import static org.fabric3.runtime.webapp.Constants.RUNTIME_PARAM;
-import static org.fabric3.runtime.webapp.Constants.SYSTEM_MONITORING_DEFAULT;
-import static org.fabric3.runtime.webapp.Constants.SYSTEM_MONITORING_PARAM;
-
-import java.util.logging.Level;
-
 import javax.management.MBeanServer;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -32,10 +25,7 @@ import javax.naming.NamingException;
 import javax.rmi.PortableRemoteObject;
 import javax.servlet.ServletContext;
 
-import org.fabric3.monitor.MonitorFactory;
-import org.fabric3.monitor.impl.JavaLoggingMonitorFactory;
 import org.fabric3.runtime.webapp.Fabric3InitException;
-import org.fabric3.runtime.webapp.WebappRuntime;
 import org.fabric3.runtime.webapp.WebappUtilImpl;
 
 /**
@@ -51,68 +41,27 @@ public class WeblogicUtil extends WebappUtilImpl {
     public WeblogicUtil(ServletContext servletContext) {
         super(servletContext);
     }
-
-    /**
-     * 
-     */
-    @Override
-    public WebappRuntime getRuntime(ClassLoader bootClassLoader) throws Fabric3InitException {
-        
-        try {
-            
-            String className = getInitParameter(RUNTIME_PARAM, RUNTIME_DEFAULT);
-            
-            MBeanServer mBeanServer = getMBeanServer();
-            MonitorFactory monitorFactory = getMonitorFactory();
-            
-            WebappRuntime runtime = (WebappRuntime) bootClassLoader.loadClass(className).newInstance();
-            
-            runtime.setMonitorFactory(monitorFactory);
-            runtime.setMBeanServer(mBeanServer);
-            
-            return runtime;
-            
-        } catch (InstantiationException e) {
-            throw new Fabric3InitException(e);
-        } catch (IllegalAccessException e) {
-            throw new Fabric3InitException(e);
-        } catch (ClassNotFoundException e) {
-            throw new Fabric3InitException("Runtime Implementation not found", e);
-        } catch (NamingException e) {
-            throw new Fabric3InitException(e);
-        }
-        
-    }
     
     /*
      * Gets the MBean server from Weblogic.
      */
-    private MBeanServer getMBeanServer() throws NamingException {
+    @Override
+    protected MBeanServer createMBeanServer() throws Fabric3InitException {
         
         Context ctx = null;        
         try {
             ctx = new InitialContext();
             Object mbeanServer = ctx.lookup("java:comp/env/jmx/runtime");
             return (MBeanServer) PortableRemoteObject.narrow(mbeanServer, MBeanServer.class);
-        } finally {
-            ctx.close();
+        } catch (NamingException e) {
+            throw new Fabric3InitException(e);
+		} finally {
+            try {
+				ctx.close();
+			} catch (NamingException e) {
+	            throw new Fabric3InitException(e);
+			}
         }
-        
-    }
-    
-    /*
-     * Gets the monitor factory.
-     * 
-     * TODO Get the monitor factory using the WLS logger.
-     */
-    private MonitorFactory getMonitorFactory() {
-        
-        String level = getInitParameter(SYSTEM_MONITORING_PARAM, SYSTEM_MONITORING_DEFAULT);
-        MonitorFactory factory = new JavaLoggingMonitorFactory();
-        factory.setDefaultLevel(Level.parse(level));
-        factory.setBundleName("f3");
-        
-        return factory;
         
     }
 
