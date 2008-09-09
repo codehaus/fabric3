af3e110c9977171911a2a12255f5038911d0465b
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-09-09 17:29:48 +0000

    avoid NPEs in hashCode and equals

--- plugins/trunk/fabric3-webapp-plugin/src/main/java/org/fabric3/war/Dependency.java
+++ plugins/trunk/fabric3-webapp-plugin/src/main/java/org/fabric3/war/Dependency.java
@@ -117,35 +117,26 @@ public class Dependency {
         return groupId;
     }
 
-    /**
-     * Implements equals based onartifactId, groupId and version.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        
-        if (!(obj instanceof Dependency)) {
-            return false;
-        }
-        
-        Dependency other = (Dependency) obj;
-        return getArtifactId().equals(other.getArtifactId()) && 
-               getGroupId().equalsIgnoreCase(other.getGroupId()) && 
-               getVersion().equals(other.getVersion());
-        
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Dependency that = (Dependency) o;
+
+        if (artifactId != null ? !artifactId.equals(that.artifactId) : that.artifactId != null) return false;
+        if (groupId != null ? !groupId.equals(that.groupId) : that.groupId != null) return false;
+        if (type != null ? !type.equals(that.type) : that.type != null) return false;
+        if (version != null ? !version.equals(that.version) : that.version != null) return false;
+
+        return true;
     }
 
-    /**
-     * Implements hashCode based onartifactId, groupId and version.
-     */
-    @Override
     public int hashCode() {
-        
-        int hash = 7;
-        hash += 31 * getArtifactId().hashCode();
-        hash += 31 * getGroupId().hashCode();
-        hash += 31 * getVersion().hashCode();
-        
-        return hash;
-        
+        int result;
+        result = (groupId != null ? groupId.hashCode() : 0);
+        result = 31 * result + (artifactId != null ? artifactId.hashCode() : 0);
+        result = 31 * result + (version != null ? version.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
     }
 }
