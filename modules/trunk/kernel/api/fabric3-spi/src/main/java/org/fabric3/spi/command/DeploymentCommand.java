97135fb3811c7cbeba62bbfef579b697402872e2
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-10-07 06:09:26 +0000

    add deployment command

---
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/command/DeploymentCommand.java
@@ -0,0 +1,37 @@
+/*
+ * Fabric3
+ * Copyright © 2008 Metaform Systems Limited
+ *
+ * This proprietary software may be used only connection with the Fabric3 license
+ * (the “License”), a copy of which is included in the software or may be
+ * obtained at: http://www.metaformsystems.com/licenses/license.html.
+
+ * Software distributed under the License is distributed on an “as is” basis,
+ * without warranties or conditions of any kind.  See the License for the
+ * specific language governing permissions and limitations of use of the software.
+ * This software is distributed in conjunction with other software licensed under
+ * different terms.  See the separate licenses for those programs included in the
+ * distribution for the permitted and restricted uses of such software.
+ *
+ */
+package org.fabric3.spi.command;
+
+import java.util.Set;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class DeploymentCommand extends AbstractCommand {
+    private static final long serialVersionUID = 8673100303949676875L;
+
+    private Set<Command> commands;
+
+    public DeploymentCommand(Set<Command> commands) {
+        super(0);
+        this.commands = commands;
+    }
+
+    public Set<Command> getCommands() {
+        return commands;
+    }
+}
