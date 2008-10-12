87ec2c326d1762ae9b895b5ebcf2a99f62e1e401
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-10-12 18:42:39 +0000

    re-enable Hessian implementation of binding.sca

--- modules/trunk/extension/binding/fabric3-binding-hessian/src/main/java/org/fabric3/binding/hessian/control/HessianBindingProvider.java
+++ modules/trunk/extension/binding/fabric3-binding-hessian/src/main/java/org/fabric3/binding/hessian/control/HessianBindingProvider.java
@@ -38,14 +38,16 @@ import java.net.URI;
 import javax.xml.namespace.QName;
 
 import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
 
 import org.fabric3.binding.hessian.scdl.HessianBindingDefinition;
-import org.fabric3.spi.Constants;
 import org.fabric3.spi.binding.BindingProvider;
 import org.fabric3.spi.binding.BindingSelectionException;
 import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.model.instance.LogicalComponent;
 import org.fabric3.spi.model.instance.LogicalReference;
 import org.fabric3.spi.model.instance.LogicalService;
+import org.fabric3.spi.topology.DomainManager;
 
 /**
  * Allows Hessian to be used for sca.binding in a domain.
@@ -54,10 +56,17 @@ import org.fabric3.spi.model.instance.LogicalService;
  */
 @EagerInit
 public class HessianBindingProvider implements BindingProvider {
-    private static final QName HTTP = new QName(Constants.FABRIC3_NS, "transport.http.base");
     private static final QName BINDING_QNAME = new QName("http://www.fabric3.org/binding/hessian/0.2", "binding.hessian");
+    private DomainManager domainManager;
 
-    public HessianBindingProvider() {
+    /**
+     * Injects the domain manager. This reference is optional so the extension can be loaded on non-controller instances.
+     *
+     * @param domainManager the domain manager
+     */
+    @Reference(required = false)
+    public void setDomainManager(DomainManager domainManager) {
+        this.domainManager = domainManager;
     }
 
     public MatchType canBind(LogicalReference source, LogicalService target) {
@@ -66,38 +75,44 @@ public class HessianBindingProvider implements BindingProvider {
     }
 
     public void bind(LogicalReference source, LogicalService target) throws BindingSelectionException {
-        // XCV TODO temporarily comment out until this is refactored to use the ZoneManager
-//        String zone = target.getParent().getZone();
-//        RuntimeInfo targetInfo = discoveryService.getRuntimeInfo(runtimeId);
-//        if (targetInfo == null) {
-//            // This could potentially occur if a runtime is removed from the domain during deployment
-//            throw new BindingSelectionException("Zone not found: " + zone);
-//        }
-//        // determing whether to configure both sides of the wire or just the reference
-//        if (target.getBindings().isEmpty()) {
-//            // configure both sides
-//            configureService(target);
-//            configureReference(source, target, targetInfo);
-//        } else {
-//            configureReference(source, target, targetInfo);
-//        }
+        if (domainManager == null) {
+            throw new BindingSelectionException("Domain manager not configured");
+        }
+        LogicalComponent<?> component = target.getParent();
+        String zone = component.getZone();
+        if (zone == null) {
+            // programming error
+            throw new AssertionError("Component not allocated: " + component.getUri());
+        }
+        String baseUrl = domainManager.getTransportMetaData(zone, String.class, "http");
+        if (baseUrl == null) {
+            throw new BindingSelectionException("HTTP information not found");
+        }
+        // determing whether to configure both sides of the wire or just the reference
+        if (target.getBindings().isEmpty()) {
+            // configure both sides
+            configureService(target);
+            configureReference(source, target, baseUrl);
+        } else {
+            configureReference(source, target, baseUrl);
+        }
     }
 
-//    private void configureReference(LogicalReference source, LogicalService target, RuntimeInfo targetInfo) throws BindingSelectionException {
-//        LogicalBinding<HessianBindingDefinition> binding = null;
-//        for (LogicalBinding<?> entry : target.getBindings()) {
-//            if (entry.getBinding().getType().equals(BINDING_QNAME)) {
-//                //noinspection unchecked
-//                binding = (LogicalBinding<HessianBindingDefinition>) entry;
-//                break;
-//            }
-//        }
-//        if (binding == null) {
-//            throw new BindingSelectionException("Hessian binding on service not found: " + target.getUri());
-//        }
-//        URI targetUri = URI.create("http://" + targetInfo.getTransportMetaData(HTTP) + binding.getBinding().getTargetUri().toString());
-//        constructLogicalReference(source, targetUri);
-//    }
+    private void configureReference(LogicalReference source, LogicalService target, String baseUrl) throws BindingSelectionException {
+        LogicalBinding<HessianBindingDefinition> binding = null;
+        for (LogicalBinding<?> entry : target.getBindings()) {
+            if (entry.getBinding().getType().equals(BINDING_QNAME)) {
+                //noinspection unchecked
+                binding = (LogicalBinding<HessianBindingDefinition>) entry;
+                break;
+            }
+        }
+        if (binding == null) {
+            throw new BindingSelectionException("Hessian binding on service not found: " + target.getUri());
+        }
+        URI targetUri = URI.create(baseUrl + binding.getBinding().getTargetUri().toString());
+        constructLogicalReference(source, targetUri);
+    }
 
     private void constructLogicalReference(LogicalReference source, URI targetUri) {
         HessianBindingDefinition referenceDefinition = new HessianBindingDefinition(targetUri, null);
