fda865ba6de39120d8951b48d6f3b1144c377394
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-10-18 20:14:28 +0000

    add commands

---
+++ modules/trunk/admin/fabric3-admin-interpreter/src/main/java/org/fabric3/admin/interpreter/command/DeployCommand.java
@@ -0,0 +1,74 @@
+/*
+ * Fabric3
+ * Copyright © 2008 Metaform Systems Limited
+ *
+ * This proprietary software may be used only connection with the Fabric3 license
+ * (the “License”), a copy of which is included in the software or may be
+ * obtained at: http://www.metaformsystems.com/licenses/license.html.
+
+ * Software distributed under the License is distributed on an “as is” basis,
+ * without warranties or conditions of any kind.  See the License for the
+ * specific language governing permissions and limitations of use of the software.
+ * This software is distributed in conjunction with other software licensed under
+ * different terms.  See the separate licenses for those programs included in the
+ * distribution for the permitted and restricted uses of such software.
+ *
+ */
+package org.fabric3.admin.interpreter.command;
+
+import org.fabric3.admin.api.AdministrationException;
+import org.fabric3.admin.api.DomainController;
+import org.fabric3.admin.interpreter.Command;
+import org.fabric3.admin.interpreter.CommandException;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class DeployCommand implements Command {
+    private DomainController controller;
+    private String contributionName;
+    private String username;
+    private String password;
+
+    public DeployCommand(DomainController controller) {
+        this.controller = controller;
+    }
+
+    public String getContributionName() {
+        return contributionName;
+    }
+
+    public void setContributionName(String contributionName) {
+        this.contributionName = contributionName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void execute() throws CommandException {
+        try {
+            if (username != null) {
+                controller.setUsername(username);
+            }
+            if (password != null) {
+                controller.setPassword(password);
+            }
+            controller.deploy(contributionName);
+        } catch (AdministrationException e) {
+            throw new CommandException(e);
+        }
+    }
+}
