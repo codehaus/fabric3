52c8c3ad4225f8d8c983d96700d6b27a2951a3db
aanderson <aanderson@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-10-31 12:58:53 +0000

    updated namespaces

--- modules/trunk/extension/implementation/fabric3-xquery/src/main/java/org/fabric3/xquery/scdl/XQueryImplementation.java
+++ modules/trunk/extension/implementation/fabric3-xquery/src/main/java/org/fabric3/xquery/scdl/XQueryImplementation.java
@@ -1,12 +1,12 @@
 /*
  * Fabric3
- * Copyright © 2008 Metaform Systems Limited
+ * Copyright ï¿½ 2008 Metaform Systems Limited
  *
  * This proprietary software may be used only connection with the Fabric3 license
- * (the ÒLicenseÓ), a copy of which is included in the software or may be
+ * (the ï¿½Licenseï¿½), a copy of which is included in the software or may be
  * obtained at: http://www.metaformsystems.com/licenses/license.html.
 
- * Software distributed under the License is distributed on an Òas isÓ basis,
+ * Software distributed under the License is distributed on an ï¿½as isï¿½ basis,
  * without warranties or conditions of any kind.  See the License for the
  * specific language governing permissions and limitations of use of the software.
  * This software is distributed in conjunction with other software licensed under
@@ -19,13 +19,14 @@ package org.fabric3.xquery.scdl;
 import javax.xml.namespace.QName;
 
 import org.fabric3.scdl.Implementation;
+import org.fabric3.spi.Namespaces;
 
 /**
  * @version $Rev$ $Date$
  */
 public class XQueryImplementation extends Implementation<XQueryComponentType> {
 
-    public static final QName QNAME = new QName("http://www.fabric3.org/xmlns/xquery/1.0", "implementation.xquery");
+    public static final QName QNAME = new QName(Namespaces.IMPLEMENTATION, "implementation.xquery");
     private String location;
     private String context;
     private boolean isModule;
