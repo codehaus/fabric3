68cddb96e69260e8ae0d8a9d4602d40421d9fb6f
aanderson <aanderson@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-11-03 07:20:16 +0000

    Increased default read timeout from one second to thirty. This should solve the JMS binding itest hanging problem on slower systems.

--- modules/trunk/extension/binding/fabric3-binding-jms/src/main/java/org/fabric3/binding/jms/runtime/host/standalone/StandalonePullJmsHost.java
+++ modules/trunk/extension/binding/fabric3-binding-jms/src/main/java/org/fabric3/binding/jms/runtime/host/standalone/StandalonePullJmsHost.java
@@ -1,12 +1,12 @@
 /*
  * Fabric3
- * Copyright © 2008 Metaform Systems Limited
+ * Copyright ï¿½ 2008 Metaform Systems Limited
  *
  * This proprietary software may be used only connection with the Fabric3 license
- * (the ÒLicenseÓ), a copy of which is included in the software or may be
+ * (the ï¿½Licenseï¿½), a copy of which is included in the software or may be
  * obtained at: http://www.metaformsystems.com/licenses/license.html.
 
- * Software distributed under the License is distributed on an Òas isÓ basis,
+ * Software distributed under the License is distributed on an ï¿½as isï¿½ basis,
  * without warranties or conditions of any kind.  See the License for the
  * specific language governing permissions and limitations of use of the software.
  * This software is distributed in conjunction with other software licensed under
@@ -65,7 +65,7 @@ import org.osoa.sca.annotations.Reference;
 public class StandalonePullJmsHost implements JmsHost, StandalonePullJmsHostMBean {
 
     private WorkScheduler workScheduler;
-    private long readTimeout = 1000L;
+    private long readTimeout = 30000L;
     private JMSRuntimeMonitor monitor;
     private int receiverCount = 3;
     private Map<URI, List<ConsumerWorker>> consumerWorkerMap = new HashMap<URI, List<ConsumerWorker>>();
