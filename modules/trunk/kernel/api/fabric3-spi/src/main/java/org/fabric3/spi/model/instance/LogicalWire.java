4607c160e10ca76bbdaac4b069537964d0f4eb54
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-11-03 21:18:05 +0000

    set initial wire state

--- modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/model/instance/LogicalWire.java
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/model/instance/LogicalWire.java
@@ -52,7 +52,7 @@ public final class LogicalWire extends LogicalScaArtifact<LogicalComponent<?>> {
 
     private final LogicalReference source;
     private final URI targetUri;
-    private LogicalState state;
+    private LogicalState state = LogicalState.NEW;
 
     /**
      * Instantiates a logical wire.
