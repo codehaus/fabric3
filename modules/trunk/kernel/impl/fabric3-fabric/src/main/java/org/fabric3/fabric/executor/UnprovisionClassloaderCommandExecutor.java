3f63a7b6fdc2efe6b109dca2679e095c4241d1c5
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-11-03 21:20:30 +0000

    remove executor

--- modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/executor/UnprovisionClassloaderCommandExecutor.java
+++ modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/executor/UnprovisionClassloaderCommandExecutor.java
@@ -16,25 +16,24 @@
  */
 package org.fabric3.fabric.executor;
 
-import org.osoa.sca.annotations.Reference;
-import org.osoa.sca.annotations.Init;
 import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
 
-import org.fabric3.fabric.command.UnprovisionClassloaderCommand;
 import org.fabric3.fabric.builder.classloader.ClassLoaderBuilder;
-import org.fabric3.spi.executor.CommandExecutorRegistry;
+import org.fabric3.fabric.command.UnprovisionClassloaderCommand;
 import org.fabric3.spi.executor.CommandExecutor;
+import org.fabric3.spi.executor.CommandExecutorRegistry;
 
 @EagerInit
 public class UnprovisionClassloaderCommandExecutor implements CommandExecutor<UnprovisionClassloaderCommand> {
 
-    private ClassLoaderBuilder classLoaderBuilder;
+    private ClassLoaderBuilder builder;
     private CommandExecutorRegistry commandExecutorRegistry;
 
-    public UnprovisionClassloaderCommandExecutor(@Reference CommandExecutorRegistry commandExecutorRegistry,
-                                               @Reference ClassLoaderBuilder classLoaderBuilder) {
-        this.classLoaderBuilder = classLoaderBuilder;
-        this.commandExecutorRegistry = commandExecutorRegistry;
+    public UnprovisionClassloaderCommandExecutor(@Reference CommandExecutorRegistry executorRegistry, @Reference ClassLoaderBuilder builder) {
+        this.builder = builder;
+        this.commandExecutorRegistry = executorRegistry;
     }
 
     @Init
@@ -44,7 +43,7 @@ public class UnprovisionClassloaderCommandExecutor implements CommandExecutor<Un
 
 
     public void execute(UnprovisionClassloaderCommand command) {
-        classLoaderBuilder.destroy(command.getUri());
+        builder.destroy(command.getUri());
     }
 
 }
