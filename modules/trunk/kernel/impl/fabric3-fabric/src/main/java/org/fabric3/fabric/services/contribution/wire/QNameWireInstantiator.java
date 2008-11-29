28516557915892401d457ad178cf5c0b4e71331e
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2008-11-29 19:34:52 +0000

    add ContributionWire subtypes and ContributionWireInstantiators

---
+++ modules/trunk/kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/services/contribution/wire/QNameWireInstantiator.java
@@ -0,0 +1,36 @@
+/*
+ * Fabric3
+ * Copyright © 2008 Metaform Systems Limited
+ *
+ * This proprietary software may be used only connection with the Fabric3 license
+ * (the “License”), a copy of which is included in the software or may be
+ * obtained at: http://www.metaformsystems.com/licenses/license.html.
+
+ * Software distributed under the License is distributed on an “as is” basis,
+ * without warranties or conditions of any kind.  See the License for the
+ * specific language governing permissions and limitations of use of the software.
+ * This software is distributed in conjunction with other software licensed under
+ * different terms.  See the separate licenses for those programs included in the
+ * distribution for the permitted and restricted uses of such software.
+ *
+ */
+package org.fabric3.fabric.services.contribution.wire;
+
+import java.net.URI;
+
+import org.fabric3.fabric.services.contribution.ContributionWireInstantiator;
+import org.fabric3.spi.services.contribution.QNameExport;
+import org.fabric3.spi.services.contribution.QNameImport;
+
+/**
+ * Creates a QNameContributionWire corresponding to a resolved contribution import.
+ *
+ * @version $Revision$ $Date$
+ */
+public class QNameWireInstantiator implements ContributionWireInstantiator<QNameImport, QNameExport, QNameContributionWire> {
+
+    public QNameContributionWire instantiate(QNameImport imprt, QNameExport export, URI importUri, URI exportUri) {
+        return new QNameContributionWire(imprt, export, importUri, exportUri);
+    }
+
+}
