a3d14964130784ce6eb22f097ba21bb194017a71
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-01-03 06:44:49 +0000

    cleanup toString()

--- modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/contribution/manifest/JavaImport.java
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/contribution/manifest/JavaImport.java
@@ -54,6 +54,6 @@ public class JavaImport implements Import {
     }
 
     public String toString() {
-        return "Imported package [" + packageInfo + "]";
+        return "[" + packageInfo + "]";
     }
 }
