6e6c6eb04e3e250623a0a3a3c9c59998a88c1a67
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-02-08 16:30:52 +0000

    remove support for XmlContribution type (XML contributions still supported)

--- modules/trunk/kernel/impl/fabric3-contribution/src/main/java/org/fabric3/contribution/processor/XmlContributionProcessor.java
+++ modules/trunk/kernel/impl/fabric3-contribution/src/main/java/org/fabric3/contribution/processor/XmlContributionProcessor.java
@@ -16,40 +16,27 @@
  */
 package org.fabric3.contribution.processor;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
-import static javax.xml.stream.XMLStreamConstants.END_DOCUMENT;
-import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
-import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-import static org.osoa.sca.Constants.SCA_NS;
 import org.osoa.sca.annotations.EagerInit;
 import org.osoa.sca.annotations.Init;
 import org.osoa.sca.annotations.Reference;
 
-import org.fabric3.host.contribution.Deployable;
 import org.fabric3.host.contribution.InstallException;
 import org.fabric3.spi.contribution.Contribution;
-import org.fabric3.spi.contribution.ContributionManifest;
 import org.fabric3.spi.contribution.ContributionProcessor;
-import org.fabric3.spi.contribution.Export;
-import org.fabric3.spi.contribution.Import;
 import org.fabric3.spi.contribution.ProcessorRegistry;
 import org.fabric3.spi.contribution.Resource;
 import org.fabric3.spi.contribution.xml.XmlIndexerRegistry;
 import org.fabric3.spi.contribution.xml.XmlProcessorRegistry;
-import org.fabric3.spi.introspection.DefaultIntrospectionContext;
 import org.fabric3.spi.introspection.IntrospectionContext;
-import org.fabric3.spi.introspection.xml.Loader;
-import org.fabric3.spi.introspection.xml.LoaderException;
 import org.fabric3.spi.xml.XMLFactory;
 
 /**
@@ -60,22 +47,18 @@ import org.fabric3.spi.xml.XMLFactory;
 @EagerInit
 public class XmlContributionProcessor implements ContributionProcessor {
     private static final List<String> CONTENT_TYPES = initializeContentTypes();
-    private static final QName SCA_CONTRIBUTION = new QName(SCA_NS, "contribution");
     private XMLInputFactory xmlFactory;
     private ProcessorRegistry processorRegistry;
     private XmlProcessorRegistry xmlProcessorRegistry;
     private XmlIndexerRegistry xmlIndexerRegistry;
-    private Loader loader;
 
     public XmlContributionProcessor(@Reference ProcessorRegistry processorRegistry,
                                     @Reference XmlProcessorRegistry xmlProcessorRegistry,
                                     @Reference XmlIndexerRegistry xmlIndexerRegistry,
-                                    @Reference Loader loader,
                                     @Reference XMLFactory xmlFactory) {
         this.processorRegistry = processorRegistry;
         this.xmlProcessorRegistry = xmlProcessorRegistry;
         this.xmlIndexerRegistry = xmlIndexerRegistry;
-        this.loader = loader;
         this.xmlFactory = xmlFactory.newInputFactoryInstance();
     }
 
@@ -89,26 +72,7 @@ public class XmlContributionProcessor implements ContributionProcessor {
     }
 
     public void processManifest(Contribution contribution, IntrospectionContext context) throws InstallException {
-        ContributionManifest manifest = contribution.getManifest();
-        InputStream stream = null;
-        XMLStreamReader reader = null;
-        try {
-            URL locationURL = contribution.getLocation();
-            stream = locationURL.openStream();
-            reader = xmlFactory.createXMLStreamReader(stream);
-            reader.nextTag();
-            processManifest(manifest, reader, context);
-        } catch (IOException e) {
-            String uri = contribution.getUri().toString();
-            throw new InstallException("Error processing contribution " + uri, e);
-        } catch (XMLStreamException e) {
-            String uri = contribution.getUri().toString();
-            int line = e.getLocation().getLineNumber();
-            int col = e.getLocation().getColumnNumber();
-            throw new InstallException("Error processing contribution " + uri + " [" + line + "," + col + "]", e);
-        } finally {
-            close(stream, reader);
-        }
+        // no-op as XML contributions do not contain manifest headers
     }
 
     public void index(Contribution contribution, IntrospectionContext context) throws InstallException {
@@ -172,61 +136,6 @@ public class XmlContributionProcessor implements ContributionProcessor {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private void processManifest(ContributionManifest manifest, XMLStreamReader reader, IntrospectionContext context) throws InstallException {
-        try {
-            while (true) {
-                int i = reader.next();
-                switch (i) {
-                case START_ELEMENT:
-                    QName qname = reader.getName();
-                    if (SCA_CONTRIBUTION.equals(qname)) {
-                        ClassLoader cl = getClass().getClassLoader();
-                        IntrospectionContext childContext = new DefaultIntrospectionContext(cl, null, null);
-                        ContributionManifest embeddedManifest = loader.load(reader, ContributionManifest.class, childContext);
-                        if (childContext.hasErrors()) {
-                            context.addErrors(childContext.getErrors());
-                            return;
-                        }
-                        if (childContext.hasWarnings()) {
-                            context.addWarnings(childContext.getWarnings());
-                        }
-
-                        // merge the contents
-                        for (Deployable deployable : embeddedManifest.getDeployables()) {
-                            manifest.addDeployable(deployable);
-                        }
-                        for (Export export : embeddedManifest.getExports()) {
-                            manifest.addExport(export);
-                        }
-                        for (Import imprt : embeddedManifest.getImports()) {
-                            manifest.addImport(imprt);
-                        }
-                    }
-                    break;
-                case END_ELEMENT:
-                    if (SCA_CONTRIBUTION.equals(reader.getName())) {
-                        // if we reached here, version was never specified and there are no dependencies
-                        return;
-                    }
-                    break;
-                case END_DOCUMENT:
-                    return;
-                }
-
-            }
-
-        } catch (LoaderException e) {
-            if (e.getCause() instanceof FileNotFoundException) {
-                return;
-            } else {
-                throw new InstallException(e);
-            }
-        } catch (XMLStreamException e) {
-            throw new InstallException(e);
-        }
-
     }
 
     private static List<String> initializeContentTypes() {
