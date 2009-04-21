48163f4f4b7d8d16a668f529a6a9a4f1a428a176
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-04-21 16:16:28 +0000

    add utility for determining types

---
+++ modules/trunk/kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/util/ParamTypes.java
@@ -0,0 +1,43 @@
+/*
+ * Fabric3
+ * Copyright © 2008 Metaform Systems Limited
+ *
+ * This proprietary software may be used only connection with the Fabric3 license
+ * (the “License”), a copy of which is included in the software or may be
+ * obtained at: http://www.metaformsystems.com/licenses/license.html.
+
+ * Software distributed under the License is distributed on an “as is” basis,
+ * without warranties or conditions of any kind.  See the License for the
+ * specific language governing permissions and limitations of use of the software.
+ * This software is distributed in conjunction with other software licensed under
+ * different terms.  See the separate licenses for those programs included in the
+ * distribution for the permitted and restricted uses of such software.
+ *
+ */
+package org.fabric3.spi.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Defines primitives for use in dynamic classloading.
+ *
+ * @version $Revision$ $Date$
+ */
+public class ParamTypes {
+
+    public static final Map<String, Class<?>> PRIMITIVES_TYPES;
+
+    static {
+        PRIMITIVES_TYPES = new HashMap<String, Class<?>>();
+        PRIMITIVES_TYPES.put("void", Void.class);
+        PRIMITIVES_TYPES.put("boolean", Boolean.TYPE);
+        PRIMITIVES_TYPES.put("byte", Byte.TYPE);
+        PRIMITIVES_TYPES.put("short", Short.TYPE);
+        PRIMITIVES_TYPES.put("int", Integer.TYPE);
+        PRIMITIVES_TYPES.put("long", Long.TYPE);
+        PRIMITIVES_TYPES.put("float", Float.TYPE);
+        PRIMITIVES_TYPES.put("double", Double.TYPE);
+    }
+
+}
