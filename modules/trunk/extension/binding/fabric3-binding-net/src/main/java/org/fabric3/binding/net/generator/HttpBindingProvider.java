9960ff64053f4f3387ae86a8702c41ebdee83bc3
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-05-05 16:18:06 +0000

    start work on supporting bnding.sca with the HTTP binding

--- modules/trunk/extension/binding/fabric3-binding-net/src/main/java/org/fabric3/binding/net/generator/HttpBindingProvider.java
+++ modules/trunk/extension/binding/fabric3-binding-net/src/main/java/org/fabric3/binding/net/generator/HttpBindingProvider.java
@@ -16,34 +16,144 @@
  */
 package org.fabric3.binding.net.generator;
 
+import java.net.URI;
 import javax.xml.namespace.QName;
 
 import org.osoa.sca.Constants;
+import org.osoa.sca.annotations.Reference;
 
+import org.fabric3.binding.net.model.HttpBindingDefinition;
 import org.fabric3.spi.binding.provider.BindingMatchResult;
 import org.fabric3.spi.binding.provider.BindingProvider;
 import org.fabric3.spi.binding.provider.BindingSelectionException;
+import org.fabric3.spi.model.instance.LogicalBinding;
+import org.fabric3.spi.model.instance.LogicalComponent;
 import org.fabric3.spi.model.instance.LogicalReference;
 import org.fabric3.spi.model.instance.LogicalService;
+import org.fabric3.spi.topology.DomainManager;
 
 /**
+ * Creates logical configuration for binding.sca using the HTTP binding.
+ *
  * @version $Revision$ $Date$
  */
 public class HttpBindingProvider implements BindingProvider {
     public static final QName HTTP_BINDING = new QName(Constants.SCA_NS, "binding.http");
+
+    private DomainManager domainManager;
+
+    /**
+     * Injects the domain manager. This reference is optional so the extension can be loaded on non-controller instances.
+     *
+     * @param domainManager the domain manager
+     */
+    @Reference(required = false)
+    public void setDomainManager(DomainManager domainManager) {
+        this.domainManager = domainManager;
+    }
 
     public QName getType() {
         return HTTP_BINDING;
     }
 
     public BindingMatchResult canBind(LogicalReference source, LogicalService target) {
-        // disable for now
-        return new BindingMatchResult(false, HTTP_BINDING);
+        return new BindingMatchResult(true, HTTP_BINDING);
     }
 
     public void bind(LogicalReference source, LogicalService target) throws BindingSelectionException {
-        throw new UnsupportedOperationException();
+        if (domainManager == null) {
+            throw new BindingSelectionException("Domain manager not configured");
+        }
+        LogicalComponent<?> targetComponent = target.getParent();
+        String targetZone = targetComponent.getZone();
+        if (targetZone == null) {
+            // programming error
+            throw new AssertionError("Target component not allocated: " + targetComponent.getUri());
+        }
+
+        // get the base URL for the target cluster
+        String targetBaseUrl = domainManager.getTransportMetaData(targetZone, String.class, "binding.net.http");
+        if (targetBaseUrl == null) {
+            throw new BindingSelectionException("Target HTTP information not found");
+        }
+        targetBaseUrl = "http://" + targetBaseUrl;
+        // determing whether to configure both sides of the wire or just the reference
+        if (target.getBindings().isEmpty()) {
+            // configure both sides
+            configureService(source, target);
+            configureReference(source, target, targetBaseUrl);
+        } else {
+            configureReference(source, target, targetBaseUrl);
+        }
+        if (target.getDefinition().getServiceContract().getCallbackContract() != null) {
+            configureCallback(source, target);
+
+        }
     }
 
+
+    @SuppressWarnings("unchecked")
+    private void configureReference(LogicalReference source, LogicalService target, String baseUrl) throws BindingSelectionException {
+        LogicalBinding<HttpBindingDefinition> binding = null;
+        for (LogicalBinding<?> entry : target.getBindings()) {
+            if (entry.getDefinition().getType().equals(HTTP_BINDING)) {
+                binding = (LogicalBinding<HttpBindingDefinition>) entry;
+                break;
+            }
+        }
+        if (binding == null) {
+            throw new BindingSelectionException("HTTP binding on service not found: " + target.getUri());
+        }
+        URI targetUri = URI.create(baseUrl + binding.getDefinition().getTargetUri().toString());
+        constructLogicalReference(source, targetUri);
+    }
+
+    private void constructLogicalReference(LogicalReference source, URI targetUri) {
+        HttpBindingDefinition referenceDefinition = new HttpBindingDefinition(targetUri, null);
+        LogicalBinding<HttpBindingDefinition> referenceBinding = new LogicalBinding<HttpBindingDefinition>(referenceDefinition, source);
+        referenceBinding.setAssigned(true);
+        source.addBinding(referenceBinding);
+    }
+
+    private void configureService(LogicalReference source, LogicalService target) {
+        String endpointName = target.getUri().getPath() + "/" + target.getUri().getFragment();
+        URI endpointUri = URI.create(endpointName);
+        HttpBindingDefinition serviceDefinition = new HttpBindingDefinition(endpointUri, null);
+        QName deployable = source.getParent().getDeployable();
+        LogicalBinding<HttpBindingDefinition> serviceBinding = new LogicalBinding<HttpBindingDefinition>(serviceDefinition, target, deployable);
+        serviceBinding.setAssigned(true);
+        target.addBinding(serviceBinding);
+    }
+
+    private void configureCallback(LogicalReference source, LogicalService target) throws BindingSelectionException {
+        LogicalComponent<?> sourceComponent = source.getParent();
+        String sourceZone = sourceComponent.getZone();
+        if (sourceZone == null) {
+            // programming error
+            LogicalComponent<?> targetComponent = target.getParent();
+            throw new AssertionError("Source component not allocated: " + targetComponent.getUri());
+        }
+
+        // get the base URL for the source cluster
+        String sourceBaseUrl = domainManager.getTransportMetaData(sourceZone, String.class, "binding.net.http");
+        if (sourceBaseUrl == null) {
+            throw new BindingSelectionException("Source HTTP information not found");
+        }
+
+        // configure the callback service on the source side
+        String endpointName = target.getUri().getPath() + "/" + source.getUri().getFragment();
+        URI endpointUri = URI.create(endpointName);
+
+        HttpBindingDefinition sourceCallbackDefinition = new HttpBindingDefinition(endpointUri, null);
+        LogicalBinding<HttpBindingDefinition> sourceBinding = new LogicalBinding<HttpBindingDefinition>(sourceCallbackDefinition, source);
+        sourceBinding.setAssigned(true);
+        source.addCallbackBinding(sourceBinding);
+
+        URI callbackUri = URI.create(sourceBaseUrl + endpointName);
+        HttpBindingDefinition targetCallbackDefinition = new HttpBindingDefinition(callbackUri, null);
+        LogicalBinding<HttpBindingDefinition> targetBinding = new LogicalBinding<HttpBindingDefinition>(targetCallbackDefinition, source);
+        targetBinding.setAssigned(true);
+        target.addCallbackBinding(targetBinding);
+    }
 
 }
