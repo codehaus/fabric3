f21b9d3eebeea4b3c2d5a926d3061846da8bc436
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-05-17 08:07:57 +0000

    add constants

---
+++ modules/trunk/extension/binding/fabric3-binding-jms/src/main/java/org/fabric3/binding/jms/runtime/JmsConstants.java
@@ -0,0 +1,41 @@
+/*
+ * Fabric3
+ * Copyright © 2008 Metaform Systems Limited
+ *
+ * This proprietary software may be used only connection with the Fabric3 license
+ * (the “License”), a copy of which is included in the software or may be
+ * obtained at: http://www.metaformsystems.com/licenses/license.html.
+
+ * Software distributed under the License is distributed on an “as is” basis,
+ * without warranties or conditions of any kind.  See the License for the
+ * specific language governing permissions and limitations of use of the software.
+ * This software is distributed in conjunction with other software licensed under
+ * different terms.  See the separate licenses for those programs included in the
+ * distribution for the permitted and restricted uses of such software.
+ *
+ */
+package org.fabric3.binding.jms.runtime;
+
+/**
+ * Defines JMS constants.
+ *
+ * @version $Revision$ $Date$
+ */
+public interface JmsConstants {
+
+    /**
+     * Header used to specify the service operation name being invoked.
+     */
+    String OPERATION_HEADER = "scaOperationName";
+
+    /**
+     * Header used to send routing (callback and conversation) information.
+     */
+    String ROUTING_HEADER = "f3Context";
+
+    /**
+     * Header used to determine if a response is a fault.
+     */
+    String FAULT_HEADER = "f3Fault";
+
+}
