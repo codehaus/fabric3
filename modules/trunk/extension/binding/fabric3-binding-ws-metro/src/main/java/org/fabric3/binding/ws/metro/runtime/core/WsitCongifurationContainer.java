d80a401b5ebe9b6a7b550e72feda7db06fa50931
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-06-24 15:28:14 +0000

    add WSIT SPI implementation for resolving endpoint configuration files

---
+++ modules/trunk/extension/binding/fabric3-binding-ws-metro/src/main/java/org/fabric3/binding/ws/metro/runtime/core/WsitCongifurationContainer.java
@@ -0,0 +1,84 @@
+/*
+ * Fabric3
+ * Copyright (c) 2009 Metaform Systems
+ *
+ * Fabric3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version, with the
+ * following exception:
+ *
+ * Linking this software statically or dynamically with other
+ * modules is making a combined work based on this software.
+ * Thus, the terms and conditions of the GNU General Public
+ * License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this software
+ * give you permission to link this software with independent
+ * modules to produce an executable, regardless of the license
+ * terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided
+ * that you also meet, for each linked independent module, the
+ * terms and conditions of the license of that module. An
+ * independent module is a module which is not derived from or
+ * based on this software. If you modify this software, you may
+ * extend this exception to your version of the software, but
+ * you are not obligated to do so. If you do not wish to do so,
+ * delete this exception statement from your version.
+ *
+ * Fabric3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the
+ * GNU General Public License along with Fabric3.
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.fabric3.binding.ws.metro.runtime.core;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.sun.xml.ws.api.ResourceLoader;
+import com.sun.xml.ws.api.server.Container;
+
+/**
+ * Wraps a a Metro Container with one that resolves dynamically generated WSDL containing policy configuration.
+ *
+ * @version $Rev$ $Date$
+ */
+public class WsitCongifurationContainer extends Container {
+    private Container delegate;
+    private File wsitConfiguration;
+    private String configName;
+
+    public WsitCongifurationContainer(Container delegate, File wsitConfiguration) {
+        this.delegate = delegate;
+        this.wsitConfiguration = wsitConfiguration;
+        // calculate the configuration file name for the endpoint
+        configName = "wsit-" + wsitConfiguration.getName().substring(0, wsitConfiguration.getName().length() - 5) + ".xml";
+    }
+
+
+    private final ResourceLoader loader = new ResourceLoader() {
+        public URL getResource(String resource) throws MalformedURLException {
+            // if the endpoint configuration file is requested, return the generated WSDL, otherwise return null
+            if (resource.equals(configName)) {
+                String name = wsitConfiguration.getName().substring(0, wsitConfiguration.getName().length() - 5) + "Service.wsdl";
+                return new File(wsitConfiguration.getParent(), name).toURI().toURL();
+            }
+            return null;
+        }
+    };
+
+    public <T> T getSPI(Class<T> spiType) {
+        if (spiType == ResourceLoader.class) {
+            return spiType.cast(loader);
+        }
+        return delegate.getSPI(spiType);
+    }
+
+
+}
