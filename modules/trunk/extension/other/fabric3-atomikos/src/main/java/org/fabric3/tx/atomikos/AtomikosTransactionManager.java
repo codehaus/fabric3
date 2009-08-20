96e6a2cec7275f0190a8a94ae57177e83b6fad2f
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-08-20 15:09:35 +0000

    add Atomikos trx manager and XA datasources

--- modules/trunk/extension/other/fabric3-atomikos/src/main/java/org/fabric3/tx/atomikos/AtomikosTransactionManager.java
+++ modules/trunk/extension/other/fabric3-atomikos/src/main/java/org/fabric3/tx/atomikos/AtomikosTransactionManager.java
@@ -38,8 +38,74 @@
 
 package org.fabric3.tx.atomikos;
 
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.InvalidTransactionException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+
+import com.atomikos.icatch.jta.UserTransactionManager;
+import org.osoa.sca.annotations.Destroy;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Service;
+
 /**
+ * Wraps an Atomikos transaction manager. The transaction manager will startup and perform recovery on first use. Configured JDBC and JMS resource
+ * registration is handled implicity by Atomikos.
+ *
  * @version $Rev$ $Date$
  */
-public class AtomikosTransactionManager {
+@Service(javax.transaction.TransactionManager.class)
+public class AtomikosTransactionManager implements TransactionManager {
+    private UserTransactionManager tm;
+
+    @Init
+    public void init() {
+        tm = new UserTransactionManager();
+    }
+
+    @Destroy
+    public void destroy() {
+        tm.close();
+    }
+
+    public void begin() throws NotSupportedException, SystemException {
+        tm.begin();
+    }
+
+    public void commit()
+            throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException, IllegalStateException, SystemException {
+        tm.commit();
+    }
+
+    public int getStatus() throws SystemException {
+        return tm.getStatus();
+    }
+
+    public Transaction getTransaction() throws SystemException {
+        return tm.getTransaction();
+    }
+
+    public void resume(Transaction trx) throws InvalidTransactionException, IllegalStateException, SystemException {
+        tm.resume(trx);
+    }
+
+    public void rollback() throws IllegalStateException, SecurityException, SystemException {
+        tm.rollback();
+    }
+
+    public void setRollbackOnly() throws IllegalStateException, SystemException {
+        tm.setRollbackOnly();
+    }
+
+    public void setTransactionTimeout(int seconds) throws SystemException {
+        tm.setTransactionTimeout(seconds);
+    }
+
+    public Transaction suspend() throws SystemException {
+        return tm.suspend();
+    }
 }
