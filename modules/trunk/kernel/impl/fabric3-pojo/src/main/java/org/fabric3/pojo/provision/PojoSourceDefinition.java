fbcc23a2c71602699135c715683e49ae84847525
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-09-20 17:17:45 +0000

    add serialVersionUID

--- modules/trunk/kernel/impl/fabric3-pojo/src/main/java/org/fabric3/pojo/provision/PojoSourceDefinition.java
+++ modules/trunk/kernel/impl/fabric3-pojo/src/main/java/org/fabric3/pojo/provision/PojoSourceDefinition.java
@@ -50,6 +50,7 @@ import org.fabric3.spi.model.physical.PhysicalSourceDefinition;
  * @version $Rev$ $Date$
  */
 public class PojoSourceDefinition extends PhysicalSourceDefinition {
+    private static final long serialVersionUID = -7594088400247150995L;
     private String interfaceName;
     private InjectableAttribute injectableAttribute;
 
