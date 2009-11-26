ec700b97e9bcd8e77d6cfcad4654a77a7e00d22e
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2009-11-26 10:53:18 +0000

    delete old tag

--- modules/tags/fabric3-fabric-0.4/src/test/java/org/fabric3/fabric/services/contribution/processor/TestBeanImpl.java
+++
@@ -1,9 +0,0 @@
-package org.fabric3.fabric.services.contribution.processor;
-
-/**
- * TestBean used by the JarContributionProcessor test cases
- *
- * @version $Rev$ $Date$
- */
-public class TestBeanImpl implements TestBean {
-}
