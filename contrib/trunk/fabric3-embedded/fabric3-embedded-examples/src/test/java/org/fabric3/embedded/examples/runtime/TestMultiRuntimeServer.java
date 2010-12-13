73fdf1869744a6d21d321d7ff2ac2012daadad55
michal.capo <michal.capo@83866bfc-822f-0410-aa35-bd5043b85eaf>
2010-12-13 23:09:45 +0000

    - examples tunning

--- contrib/trunk/fabric3-embedded/fabric3-embedded-examples/src/test/java/org/fabric3/embedded/examples/runtime/TestMultiRuntimeServer.java
+++ contrib/trunk/fabric3-embedded/fabric3-embedded-examples/src/test/java/org/fabric3/embedded/examples/runtime/TestMultiRuntimeServer.java
@@ -2,27 +2,39 @@ package org.fabric3.embedded.examples.runtime;
 
 import org.fabric3.host.contribution.ContributionException;
 import org.fabric3.host.domain.DeploymentException;
+import org.fabric3.runtime.embedded.api.EmbeddedProfile;
 import org.fabric3.runtime.embedded.api.EmbeddedServer;
 import org.fabric3.runtime.embedded.exception.EmbeddedFabric3StartupException;
 import org.fabric3.runtime.embedded.factory.EmbeddedServerFactory;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 /**
  * @author Michal Capo
  */
 public class TestMultiRuntimeServer {
 
-    public static void main(String[] args) throws InterruptedException, MalformedURLException, EmbeddedFabric3StartupException, ContributionException, DeploymentException, URISyntaxException {
+    public static void main(String[] args) throws InterruptedException, IOException, EmbeddedFabric3StartupException, ContributionException, DeploymentException, URISyntaxException {
         //TODO make multi runtime working
-        EmbeddedServer server = EmbeddedServerFactory.multiRuntime("/tmp/fabric3_embedded_multi");
+        EmbeddedServer server = EmbeddedServerFactory.multiRuntime("/tmp/fabric3_embedded_multi", EmbeddedProfile.WEB);
         EmbeddedServerFactory.addControllerRuntime(server, "/runtime/controller.xml");
         EmbeddedServerFactory.addParticipantRuntime(server, "runtime1", "/runtime/runtime1.xml");
+        EmbeddedServerFactory.addParticipantRuntime(server, "runtime2", "/runtime/runtime2.xml");
 
         server.start();
 
         server.installComposite("embedded.composite:/composite1/testComposite.composite");
+        server.installComposite("embedded.war:" + projectPath() + "/src/main/webapp/WEB-INF/web.composite");
     }
+
+    private static String projectPath() throws IOException {
+        Properties properties = new Properties();
+        properties.load(TestSingleRuntimeServer.class.getResourceAsStream("/paths.properties"));
+
+        return properties.getProperty("projectPath");
+    }
+
 
 }
