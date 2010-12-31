2317859ad1db2984f42c973d7ce65394fb592a9c
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2010-12-31 15:38:16 +0000

    [maven-release-plugin]  copy for tag sca-api-1.1.1

---
+++ spec/tags/sca-api-1.1.1/src/main/java/org/osoa/sca/annotations/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/**
+ * SCA Common Annotations.
+ * <p/>
+ * Annotations that can be applied to Java code to provide additional meta-data about the SCA component type
+ * or configuration information for the implementation container. These are common annotations that
+ * are intended to provide consistent behaviour across different SCA implementation types but you should consult
+ * the documentation on each implementation type for a definition of how the annotation will be interpreted. 
+ */
+package org.osoa.sca.annotations;
