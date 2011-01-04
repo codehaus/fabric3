c1c137334d711380b62fc96c71620a318104641d
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2011-01-04 07:53:56 +0000

    tag Spring samples

---
+++ samples-spring/tags/fabric3-samples-spring-parent-pom-1.7/apps/bigbank/bigbank-loan/src/main/java/org/fabric3/samples/bigbank/loan/domain/RiskReasonInfo.java
@@ -0,0 +1,54 @@
+package org.fabric3.samples.bigbank.loan.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+/**
+ * @version $Revision$ $Date$
+ */
+@Entity
+public class RiskReasonInfo implements Serializable {
+    private static final long serialVersionUID = -1781028701570454727L;
+    private long id;
+    private long version;
+    private String description;
+
+    public RiskReasonInfo() {
+    }
+
+    public RiskReasonInfo(String description) {
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Version
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
