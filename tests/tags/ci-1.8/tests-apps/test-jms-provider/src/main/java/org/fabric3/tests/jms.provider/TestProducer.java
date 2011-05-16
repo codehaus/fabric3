1cf7ba59d7e43b4e967efa3eadcf1199fb0f18b6
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2011-05-16 12:56:36 +0000

    tag integration tests for release

---
+++ tests/tags/ci-1.8/tests-apps/test-jms-provider/src/main/java/org/fabric3/tests/jms.provider/TestProducer.java
@@ -0,0 +1,13 @@
+package org.fabric3.tests.jms.provider;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public interface TestProducer {
+
+    void start();
+
+    void stop();
+
+
+}
---
+++ tests/tags/ci-1.8/tests-apps/test-jms-provider/src/main/java/org/fabric3/tests/jms.provider/TestProducerImpl.java
@@ -0,0 +1,53 @@
+package org.fabric3.tests.jms.provider;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.oasisopen.sca.annotation.Scope;
+
+import org.fabric3.api.annotation.Producer;
+import org.fabric3.api.annotation.management.Management;
+import org.fabric3.api.annotation.management.ManagementOperation;
+
+/**
+ *
+ * To start: http://localhost:8181/management/TestProducer/start
+ * @version $Rev$ $Date$
+ */
+@Management
+@Scope("COMPOSITE")
+public class TestProducerImpl implements TestProducer {
+
+    @Producer
+    protected TestChannel channel;
+
+    private Runner runner = new Runner();
+    private AtomicBoolean stop = new AtomicBoolean(true);
+
+    @ManagementOperation
+    public void start() {
+        if (!stop.getAndSet(false)){
+           return;
+        }
+        new Thread(runner).start();
+    }
+
+    @ManagementOperation
+    public void stop() {
+        stop.set(true);
+    }
+
+    private class Runner implements Runnable {
+
+        public void run() {
+            while (!stop.get()) {
+                try {
+                    channel.send("test");
+                } catch (RejectedExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
