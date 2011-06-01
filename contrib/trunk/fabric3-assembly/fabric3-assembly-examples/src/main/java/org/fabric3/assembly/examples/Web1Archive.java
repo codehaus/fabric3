6bf7f05988e9cf603c88017425e2229cd7a4836d
michal.capo <michal.capo@83866bfc-822f-0410-aa35-bd5043b85eaf>
2011-06-01 17:02:36 +0000

    - web example update

--- contrib/trunk/fabric3-assembly/fabric3-assembly-examples/src/main/java/org/fabric3/assembly/examples/Web1Archive.java
+++ contrib/trunk/fabric3-assembly/fabric3-assembly-examples/src/main/java/org/fabric3/assembly/examples/Web1Archive.java
@@ -13,11 +13,11 @@ public class Web1Archive {
         return ShrinkWrap.create(WebArchive.class, "web1.war")
                 .addClass(WebComposite.class)
 
-                .addAsWebInfResource("composites/web/webapp/WEB-INF/plan.xml", "plan.xml")
-                .addAsWebInfResource("composites/web/webapp/WEB-INF/sca-contribution.xml", "sca-contribution.xml")
-                .addAsWebInfResource("composites/web/webapp/WEB-INF/web.componentType", "web.componentType")
-                .addAsWebInfResource("composites/web/webapp/WEB-INF/web.composite", "web.composite")
-                .addAsWebInfResource("composites/web/webapp/WEB-INF/web.xml", "web.xml")
+                .addAsWebInfResource("composites/web/WEB-INF/plan.xml", "plan.xml")
+                .addAsWebInfResource("composites/web/WEB-INF/sca-contribution.xml", "sca-contribution.xml")
+                .addAsWebInfResource("composites/web/WEB-INF/web.componentType", "web.componentType")
+                .addAsWebInfResource("composites/web/WEB-INF/web.composite", "web.composite")
+                .addAsWebInfResource("composites/web/WEB-INF/web.xml", "web.xml")
 
                 ;
     }
