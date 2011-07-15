cb248f3d49fa7a502a1bc88bfd843c85a9811f75
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2011-07-15 19:58:10 +0000

    create 1.8.1 tag

---
+++ samples/tags/fabric3-samples-parent-pom-1.8.1/apps/bigbank/bigbank-loan/src/main/java/org/fabric3/samples/bigbank/api/loan/LoanService.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2010 Metaform Systems
+ *
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.samples.bigbank.api.loan;
+
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.oasisopen.sca.annotation.Remotable;
+
+import org.fabric3.samples.bigbank.api.message.LoanApplication;
+import org.fabric3.samples.bigbank.api.message.LoanRequest;
+import org.fabric3.samples.bigbank.api.message.LoanStatus;
+import org.fabric3.samples.bigbank.api.message.OptionSelection;
+
+/**
+ * Service responsible for processing a loan application.
+ *
+ * @version $Rev$ $Date$
+ */
+@Remotable
+@Path("/")
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+public interface LoanService {
+    int SUBMITTED = 1;
+    int AWAITING_ASSESSMENT = 2;
+    int PRICING = 3;
+    int AWAITING_ACCEPTANCE = 4;
+    int AWAITING_APPRAISAL = 5;
+    int SCHEDULED_FOR_FUNDING = 6;
+    int FUNDED = 7;
+    int REJECTED = -1;
+    int DECLINED = -2;
+
+    /**
+     * Initiates a loan application.
+     *
+     * @param request the loan data
+     * @return the loan tracking number
+     * @throws LoanException if an error initiating the application occurs
+     */
+    @POST
+    @Path("application")
+    long apply(LoanRequest request) throws LoanException;
+
+    /**
+     * Returns an in-process loan application.
+     *
+     * @param id the loan tracking number
+     * @return the loan application
+     * @throws LoanException if a retrieval exception was encountered. For example, LoanApplicationNotFoundException.
+     */
+    @GET
+    @Path("application/{id}")
+    LoanApplication retrieve(@PathParam("id") long id) throws LoanException;
+
+    /**
+     * Returns active loan applications for the current user with the given status.
+     *
+     * @param status the loan status
+     * @return all active loan applications for the user
+     * @throws LoanException if a retrieval exception was encountered. For example, LoanApplicationNotFoundException.
+     */
+    @GET
+    @Path("applications/{status}")
+    List<LoanApplication> retrieveApplications(@PathParam("status") int status) throws LoanException;
+
+    /**
+     * Returns the status of all active loan applications for the current user.
+     *
+     * @return the status of all active loan applications for the user
+     * @throws LoanException if a retrieval exception was encountered. For example, LoanApplicationNotFoundException.
+     */
+    @GET
+    @Path("status")
+    List<LoanStatus> getLoanStatus() throws LoanException;
+
+    /**
+     * Declines the terms of a loan application.
+     *
+     * @param id the loan tracking number
+     * @throws LoanException if an error declining the loan occurs
+     */
+    void decline(long id) throws LoanException;
+
+    /**
+     * Accepts the terms of a loan application.
+     *
+     * @param selection contains the loan tracking number and selected terms
+     * @throws LoanException ÊÂ if an error accepting the loan occurs
+     */
+    @POST
+    @Path("/application/selection")
+    void accept(OptionSelection selection) throws LoanException;
+
+}
---
+++ samples/tags/fabric3-samples-parent-pom-1.8.1/apps/bigbank/bigbank-loan/src/main/java/org/fabric3/samples/bigbank/loan/statistics/ApplicationStatisticsComponent.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2010 Metaform Systems
+ *
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.samples.bigbank.loan.statistics;
+
+import java.io.IOException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.oasisopen.sca.annotation.Destroy;
+import org.oasisopen.sca.annotation.EagerInit;
+import org.oasisopen.sca.annotation.Init;
+import org.oasisopen.sca.annotation.ManagedTransaction;
+import org.oasisopen.sca.annotation.Scope;
+
+import org.fabric3.api.annotation.Consumer;
+import org.fabric3.api.annotation.Producer;
+import org.fabric3.api.annotation.Resource;
+import org.fabric3.api.annotation.management.Management;
+import org.fabric3.api.annotation.management.ManagementOperation;
+import org.fabric3.samples.bigbank.api.domain.ApplicationStatistics;
+import org.fabric3.samples.bigbank.api.domain.LoanRecord;
+import org.fabric3.samples.bigbank.api.event.ApplicationEvent;
+import org.fabric3.samples.bigbank.api.event.ApplicationExpired;
+import org.fabric3.samples.bigbank.api.event.ApplicationReady;
+import org.fabric3.samples.bigbank.api.event.ApplicationReceived;
+import org.fabric3.samples.bigbank.api.event.AppraisalScheduled;
+import org.fabric3.samples.bigbank.api.event.ManualRiskAssessmentComplete;
+import org.fabric3.samples.bigbank.api.event.RunningAveragesUpdateEvent;
+
+import static org.fabric3.samples.bigbank.util.GenericsHelper.cast;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+@Management
+@ManagedTransaction
+@Scope("COMPOSITE")
+public class ApplicationStatisticsComponent {
+    private ScheduledExecutorService executorService;
+    private StatisticsChannel statisticsChannel;
+    private EntityManager em;
+
+    private StatisticsRunnable runnable;
+    private volatile long wait = 10000;
+
+    private MovingAverage amountAverage;
+    private MovingAverage approvalAverage;
+    private MovingAverage timeToCompleteAutomatedAverage;
+    private MovingAverage timeToCompleteManualAverage;
+
+    @PersistenceContext(unitName = "loanApplication")
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+
+    @Producer
+    public void setStatisticsChannel(StatisticsChannel channel) {
+        this.statisticsChannel = channel;
+    }
+
+    @Resource
+    public void setExecutorService(ScheduledExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+    @ManagementOperation(description = "Sets the period to wait between moving average updates")
+    public void setWait(long wait) {
+        this.wait = wait;
+    }
+
+    @Init
+    public void init() throws IOException {
+        amountAverage = new MovingAverage(4);
+        approvalAverage = new MovingAverage(4);
+        timeToCompleteAutomatedAverage = new MovingAverage(4);
+        timeToCompleteManualAverage = new MovingAverage(4);
+
+        runnable = new StatisticsRunnable();
+        executorService.schedule(runnable, wait, TimeUnit.MILLISECONDS);
+    }
+
+    @Destroy
+    public void destroy() throws IOException {
+        if (runnable != null) {
+            runnable.stop();
+        }
+    }
+
+    @Consumer("analyticsChannel")
+    public void onEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationReceived) {
+            onReceived((ApplicationReceived) event);
+        } else if (event instanceof ManualRiskAssessmentComplete) {
+            onAssessmentComplete((ManualRiskAssessmentComplete) event);
+        } else if (event instanceof ApplicationReady) {
+            onApplicationReady((ApplicationReady) event);
+        } else if (event instanceof AppraisalScheduled) {
+            onAppraisalScheduled((AppraisalScheduled) event);
+        } else if (event instanceof ApplicationExpired) {
+            onExpired((ApplicationExpired) event);
+        }
+    }
+
+    private void onAppraisalScheduled(AppraisalScheduled event) {
+        long id = event.getLoanId();
+        ApplicationStatistics statistics = findStatistics(id);
+        statistics.setAppraisalScheduledTimestamp(System.currentTimeMillis());
+        em.persist(statistics);
+    }
+
+    private void onReceived(ApplicationReceived event) {
+        ApplicationStatistics statistics = new ApplicationStatistics();
+        statistics.setLoanId(event.getLoanId());
+        statistics.setReceivedTimestamp(System.currentTimeMillis());
+        em.persist(statistics);
+        double amount = event.getAmount();
+        amountAverage.write(amount);
+    }
+
+    private void onAssessmentComplete(ManualRiskAssessmentComplete event) {
+        long id = event.getLoanId();
+        ApplicationStatistics statistics = findStatistics(id);
+        if (event.isApproved()) {
+            statistics.setApprovedTimestamp(System.currentTimeMillis());
+        } else {
+            statistics.setRejectedTimestamp(System.currentTimeMillis());
+        }
+        em.persist(statistics);
+    }
+
+    private void onApplicationReady(ApplicationReady event) {
+        long id = event.getLoanId();
+        ApplicationStatistics statistics = findStatistics(id);
+        statistics.setReadyTimestamp(System.currentTimeMillis());
+        em.persist(statistics);
+
+        double amount = event.getAmount();
+        approvalAverage.write(amount);
+
+        long elapsed = event.getTimestamp() - statistics.getReceivedTimestamp();
+        if (LoanRecord.AUTOMATED_APPROVAL == event.getApprovalType()) {
+            timeToCompleteAutomatedAverage.write(elapsed);
+        } else {
+            // manual approval
+            timeToCompleteManualAverage.write(elapsed);
+        }
+
+    }
+
+    private void onExpired(ApplicationExpired event) {
+        long id = event.getLoanId();
+        ApplicationStatistics statistics = findStatistics(id);
+        statistics.setExpiredTimestamp(System.currentTimeMillis());
+        em.persist(statistics);
+    }
+
+    private ApplicationStatistics findStatistics(long loanId) {
+        Query query = em.createQuery("SELECT l FROM ApplicationStatistics l WHERE l.loanId = :loanId");
+        query.setParameter("loanId", loanId);
+        ApplicationStatistics statistics = cast(query.getSingleResult());
+        if (statistics == null) {
+            throw new AssertionError("Statistics was null: " + loanId);
+        }
+        return statistics;
+    }
+
+    /**
+     * Reads moving average and periodically publishes updates to the statistics channel.
+     */
+    private class StatisticsRunnable implements Runnable {
+        private boolean stop;
+
+        public void stop() {
+            this.stop = true;
+        }
+
+        public void run() {
+            RunningAveragesUpdateEvent event = new RunningAveragesUpdateEvent();
+            double amount = amountAverage.readAverage();
+            event.setRequestAmount(amount);
+            amount = approvalAverage.readAverage();
+            event.setApprovalAmount(amount);
+            event.setTimeToAutomatedApproval((long) timeToCompleteAutomatedAverage.readAverage());
+            event.setTimeToManualApproval((long) timeToCompleteManualAverage.readAverage());
+
+            statisticsChannel.update(event);
+            if (!stop) {
+                // schedule the next read
+                executorService.schedule(this, wait, TimeUnit.MILLISECONDS);
+            }
+        }
+    }
+
+}
