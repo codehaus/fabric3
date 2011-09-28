bdf32520a62d3ffd9a79b7ef40e72f5b6e59e530
jmarino <jmarino@83866bfc-822f-0410-aa35-bd5043b85eaf>
2011-09-28 20:43:18 +0000

    tag samples

---
+++ samples/tags/fabric3-samples-parent-pom-1.9/apps/bigbank/bigbank-loan/src/main/java/org/fabric3/samples/bigbank/api/message/LoanApplication.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2010 Metaform Systems
+ *
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  This file is licensed
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.fabric3.samples.bigbank.api.message;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Represents a loan application submitted by a borrower.
+ *
+ * @version $Rev$ $Date$
+ */
+@XmlRootElement
+public class LoanApplication implements Serializable {
+    private static final long serialVersionUID = 8045590944866727036L;
+    private long id;
+    private int status;
+    private double amount;
+
+    private LoanOption[] options;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LoanOption[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(LoanOption[] options) {
+        this.options = options;
+    }
+
+}
