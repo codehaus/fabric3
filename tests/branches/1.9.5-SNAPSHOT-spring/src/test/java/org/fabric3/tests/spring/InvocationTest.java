25f820384dda618fd3496b8773c743f40ea201ad
palmalcheg <palmalcheg@83866bfc-822f-0410-aa35-bd5043b85eaf>
2012-08-03 15:50:09 +0000

    FABRICTHREE-660 , branching integration tests

---
+++ tests/branches/1.9.5-SNAPSHOT-spring/src/test/java/org/fabric3/tests/spring/InvocationTest.java
@@ -0,0 +1,46 @@
+package org.fabric3.tests.spring;
+
+import junit.framework.TestCase;
+import org.oasisopen.sca.annotation.Reference;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class InvocationTest extends TestCase {
+
+    @Reference
+    protected TestService service;
+
+    @Reference
+    protected TestService nonTypedService;
+    
+    @Reference
+    protected TestService xbeanService;
+
+    public void testInvoke() throws Exception {
+        assertEquals("test", service.invoke("test"));
+    }
+
+    public void testUnCheckedExceptionInvoke() throws Exception {
+        try {
+            service.invokeUnCheckedException();
+            fail();
+        } catch (TestRuntimeException e) {
+            // expected
+        }
+    }
+
+    public void testCheckedExceptionInvoke() throws Exception {
+        try {
+            service.invokeCheckedException();
+            fail();
+        } catch (TestException e) {
+            // expected
+        }
+    }
+
+    public void testNonTypedService() throws Exception {
+         assertEquals("test", nonTypedService.invoke("test"));
+    }
+
+}
